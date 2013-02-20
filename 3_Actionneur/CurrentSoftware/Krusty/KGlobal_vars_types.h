6e539f1a1e7103244f158ad97739f6f9aec614a3
amurzeau <amurzeau@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2013-02-20 23:07:23 +0000

    Déplacement de l'enum QUEUE_act_e dans KGlobal_vars_types.h et mise à jour du code de changement de vitesse par rapport au nouvelle unités (plus d'asm maintenant)

--- 3_Actionneur/CurrentSoftware/Krusty/KGlobal_vars_types.h
+++ 3_Actionneur/CurrentSoftware/Krusty/KGlobal_vars_types.h
@@ -17,5 +17,15 @@
 		#error "Global_vars_types est inclu par la QS, ne l'incluez pas vous meme"
 	#endif
 
+	typedef enum {
+		BALL_GRABBER=0,
+		HAMMER,
+		QUEUE_ACT_BallLauncher,	//avec QUEUE devant, n'importe ou dans le code on sait que ça fait référence à un actionneur lié à queue.h/c
+		QUEUE_ACT_Plate_Rotation,
+		QUEUE_ACT_Plate_AX12_Plier,
+		NB_ACT
+
+	} QUEUE_act_e;
+
 
 #endif /* ndef KRUSTY_GLOBAL_VARS_TYPES_H && def I_AM_ROBOT_KRUSTY */
