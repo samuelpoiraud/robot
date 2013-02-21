de4cf79ecc7788a4ab4b3aaa39aa4e97214ef6e9
amurzeau <amurzeau@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2013-02-21 13:36:35 +0000

    Ajout des fichiers pour créer des jonctions (liens pour la QS)

---
+++ create-junctions.bat
@@ -0,0 +1,8 @@
+junction.exe 1_Propulsion\CurrentSoftware\project_2012\QS QS
+junction.exe 2_Strategie\CurrentSoftware\QS QS
+junction.exe 3_Actionneur\CurrentSoftware\QS QS
+junction.exe 4_Balises\Software_Emetteur_US\QS QS
+junction.exe 4_Balises\Software_Mere\QS QS
+junction.exe 4_Balises\Software_Recepteur_IR\Project\QS QS
+junction.exe 4_Balises\Software_Recepteur_US\QS QS
+junction.exe 5_Supervision\Current_Software\QS QS
