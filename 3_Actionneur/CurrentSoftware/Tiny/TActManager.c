0c035072c04f0afc2c3b4e73459fb4c4038b7577
amurzeau <amurzeau@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2013-03-11 22:33:41 +0000

    Mise a jour du projet pour rendre le code compilable avec les nouveautés Ajout de l'actionneur Hammer (bras pour les bougies du bas) Mise en commentaire du code faisant référence au BallInflater car les fichiers n'ont pas encore été ajouté au SVN.

--- 3_Actionneur/CurrentSoftware/Tiny/TActManager.c
+++ 3_Actionneur/CurrentSoftware/Tiny/TActManager.c
@@ -12,21 +12,25 @@
 #include "TActManager.h"
 #ifdef I_AM_ROBOT_TINY
 
+#include "THammer.h"
 #include "TLong_hammer.h"
-#include "TBall_inflater.h"
+//#include "TBall_inflater.h"
 
 //Initialise les actionneurs
 void ACTMGR_init() {
+	HAMMER_init();
 	LONGHAMMER_init();
-	BALLINFLATER_init();
+	//BALLINFLATER_init();
 }
 
 //Gère les messages CAN des actionneurs. Si le message à été géré, cette fonction renvoie TRUE, sinon FALSE.
 bool_e ACTMGR_process_msg(CAN_msg_t* msg) {
+	if(HAMMER_CAN_process_msg(msg))
+		return TRUE;
 	if(LONGHAMMER_CAN_process_msg(msg))
 		return TRUE;
-	if(BALLINFLATER_CAN_process_msg(msg))
-		return TRUE;
+	//if(BALLINFLATER_CAN_process_msg(msg))
+	//	return TRUE;
 
 	return FALSE;
 }
