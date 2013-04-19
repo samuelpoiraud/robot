361af4a647699f9db9b3f9f24c014c97e36ea221
jroy <jroy@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2013-04-19 20:49:17 +0000

    Rajout du cooking du gato ! et modification de masse des strats pour tiny

--- 2_Strategie/CurrentSoftware/actions_tests_tiny_micro.h
+++ 2_Strategie/CurrentSoftware/actions_tests_tiny_micro.h
@@ -22,7 +22,7 @@
 void T_BALLINFLATER_start(void);
 
 error_e TINY_open_all_gifts(void);
-error_e TINY_white_candles(void);
+error_e TINY_white_candles(bool_e tobase);
 error_e TINY_BLUESIDE_candles(void);
 error_e TINY_REDSIDE_candles(void);
 error_e TINY_open_all_gifts_homolog(void);
