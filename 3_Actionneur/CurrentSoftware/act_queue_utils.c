7c3875475ddd38e8678c50b7832ffdb0ab45adc0
amurzeau <amurzeau@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2013-04-21 01:07:01 +0000

    Ajout des définitions des ports des canaux du tritronics (capteur couleur pour bougie) Ajout  des fichiers act_queue_utils.h/c au svn ...

---
+++ 3_Actionneur/CurrentSoftware/act_queue_utils.c
@@ -0,0 +1,278 @@
+/*
+ *	Club Robot ESEO 2012 - 2013
+ *	Krusty & Tiny
+ *
+ *	Fichier : act_queue_utils.c
+ *	Package : Carte Actionneur
+ *	Description : Propose des fonctions pour gérer les actions avec la pile.
+ *	Auteur : Alexis
+ *	Version 20130420
+ */
+
+#include "act_queue_utils.h"
+#include "output_log.h"
+#include "QS/QS_CANmsgList.h"
+#include "QS/QS_can.h"
+#include "QS/QS_ax12.h"
+#include "QS/QS_DCMotor2.h"
+
+#define LOG_PREFIX "ActUtils: "
+#define COMPONENT_log(log_level, format, ...) OUTPUTLOG_printf(OUTPUT_LOG_COMPONENT_ACTQUEUEUTILS, log_level, LOG_PREFIX format, ## __VA_ARGS__)
+
+
+typedef enum {
+	CAN_TPT_NoParam, //Pas de paramètre
+	CAN_TPT_Line,    //Le paramètre est une ligne
+	CAN_TPT_Normal   //Le paramètre est un nombre normal sans signification particulière pour nous (ce code)
+} CAN_result_param_type_t;
+static void ACTQ_printResult(Uint11 originalSid, Uint8 originalCommand, Uint8 result, Uint8 errorCode, CAN_result_param_type_t paramType, Uint16 param);
+
+
+//Met sur la pile une action qui sera gérée par act_function_ptr avec en paramètre param. L'action est protégée par semaphore avec act_id
+//Cette fonction est appelée par les fonctions de traitement des messages CAN de chaque actionneur.
+void ACTQ_push_operation_from_msg(CAN_msg_t* msg, QUEUE_act_e act_id, action_t act_function_ptr, Uint16 param) {
+	queue_id_t queueId = QUEUE_create();
+	assert(queueId != QUEUE_CREATE_FAILED);
+	if(queueId != QUEUE_CREATE_FAILED) {	//Si on est pas en verbose_mode, l'assert sera ignoré et la suite risque de vraiment planter ...
+		QUEUE_add(queueId, &QUEUE_take_sem, (QUEUE_arg_t){0, 0, NULL}, act_id);
+		QUEUE_add(queueId, act_function_ptr, (QUEUE_arg_t){msg->data[0], param, &ACTQ_finish_SendResult}, act_id);
+		QUEUE_add(queueId, &QUEUE_give_sem, (QUEUE_arg_t){0, 0, NULL}, act_id);
+	} else {	//on indique qu'on a pas géré la commande
+		//CAN_msg_t resultMsg = {ACT_RESULT, {msg->sid & 0xFF, msg->data[0], ACT_RESULT_NOT_HANDLED, ACT_RESULT_ERROR_NO_RESOURCES}, 4};
+		//CAN_send(&resultMsg);
+		ACTQ_sendResult(msg->sid, msg->data[0], ACT_RESULT_NOT_HANDLED, ACT_RESULT_ERROR_NO_RESOURCES);
+	}
+}
+
+//Envoie le message CAN de retour à la strat (et affiche des infos de debuggage si activé)
+void ACTQ_sendResult(Uint11 originalSid, Uint8 originalCommand, Uint8 result, Uint8 errorCode) {
+	CAN_msg_t resultMsg;
+
+	if(global.match_started == TRUE) {
+		resultMsg.sid = ACT_RESULT;
+		resultMsg.data[0] = originalSid & 0xFF;
+		resultMsg.data[1] = originalCommand;
+		resultMsg.data[2] = result;
+		resultMsg.data[3] = errorCode;
+		resultMsg.size = 4;
+
+		CAN_send(&resultMsg);
+	}
+
+	ACTQ_printResult(originalSid, originalCommand, result, errorCode, CAN_TPT_NoParam, 0);
+}
+
+//Comme CAN_sendResult mais ajoute un paramètre au message. Peut servir pour debuggage.
+void ACTQ_sendResultWithParam(Uint11 originalSid, Uint8 originalCommand, Uint8 result, Uint8 errorCode, Uint16 param) {
+	CAN_msg_t resultMsg;
+
+	resultMsg.sid = ACT_RESULT;
+	resultMsg.data[0] = originalSid & 0xFF;
+	resultMsg.data[1] = originalCommand;
+	resultMsg.data[2] = result;
+	resultMsg.data[3] = errorCode;
+	resultMsg.data[4] = LOWINT(param);
+	resultMsg.data[5] = HIGHINT(param);
+	resultMsg.size = 6;
+
+	CAN_send(&resultMsg);
+
+	ACTQ_printResult(originalSid, originalCommand, result, errorCode, CAN_TPT_Normal, param);
+}
+
+//Comme CAN_sendResultWithParam mais le paramètre est considéré comme étant un numéro de ligne.
+void ACTQ_sendResultWitExplicitLine(Uint11 originalSid, Uint8 originalCommand, Uint8 result, Uint8 errorCode, Uint16 line) {
+	CAN_msg_t resultMsg;
+
+	resultMsg.sid = ACT_RESULT;
+	resultMsg.data[0] = originalSid & 0xFF;
+	resultMsg.data[1] = originalCommand;
+	resultMsg.data[2] = result;
+	resultMsg.data[3] = errorCode;
+	resultMsg.data[4] = LOWINT(line);
+	resultMsg.data[5] = HIGHINT(line);
+	resultMsg.size = 6;
+
+	CAN_send(&resultMsg);
+
+	ACTQ_printResult(originalSid, originalCommand, result, errorCode, CAN_TPT_Line, line);
+}
+
+bool_e ACTQ_check_status_ax12(queue_id_t queueId, Uint8 ax12Id, Uint16 wantedPosition, Uint16 pos_epsilon, Uint16 timeout_ms_x100, Uint16 large_epsilon, Uint8* result, Uint8* error_code, Uint16* line) {
+	AX12_reset_last_error(ax12Id);
+
+	Uint16 current_pos = AX12_get_position(ax12Id);
+	Uint8 error = AX12_get_last_error(ax12Id).error;
+	Uint16 dummy;
+
+	//Avec ça, on ne se soucie pas du contenu de result et error_code par la suite, on sait qu'ils ne sont pas NULL
+	if(!result) result = (Uint8*)&dummy;
+	if(!error_code) error_code = (Uint8*)&dummy;
+	if(!line) line = &dummy;
+
+	if(abs((Sint16)current_pos - (Sint16)(wantedPosition)) <= pos_epsilon) {
+		*result = ACT_RESULT_DONE;
+		*error_code = ACT_RESULT_ERROR_OK;
+		*line = 0x0100;
+	} else if((error & AX12_ERROR_TIMEOUT) && (error & AX12_ERROR_RANGE)) {
+		//Si le driver a attendu trop longtemps, c'est a cause d'un deadlock plutot qu'un manque de ressources (il attend suffisament longtemps pour que les commandes soit bien envoyées)
+		AX12_set_torque_enabled(ax12Id, FALSE);
+		*result = ACT_RESULT_NOT_HANDLED;
+		*error_code = ACT_RESULT_ERROR_LOGIC;
+		*line = 0x0200;
+	} else if(error & AX12_ERROR_TIMEOUT) {
+		//L'ax12 n'a pas répondu à la commande
+		if(abs((Sint16)current_pos - (Sint16)(wantedPosition)) <= large_epsilon) {
+			*result = ACT_RESULT_DONE;
+			*error_code = ACT_RESULT_ERROR_OK;
+			*line = 0x0300;
+		} else {
+			AX12_set_torque_enabled(ax12Id, FALSE);
+			*result = ACT_RESULT_FAILED;
+			*error_code = ACT_RESULT_ERROR_NOT_HERE;
+			*line = 0x0400;
+		}
+	} else if(CLOCK_get_time() >= QUEUE_get_initial_time(queueId) + timeout_ms_x100) {
+		//Timeout, l'ax12 n'a pas bouger à la bonne position a temps
+		AX12_set_torque_enabled(ax12Id, FALSE);
+		*result = ACT_RESULT_FAILED;
+		*error_code = ACT_RESULT_ERROR_UNKNOWN;
+		*line = 0x0500;
+	} else if(error & AX12_ERROR_OVERHEATING) {
+		//autres erreurs fiable, les autres on les teste pas car si elle arrive, c'est plus probablement un problème de transmission ou code ...
+		AX12_set_torque_enabled(ax12Id, FALSE);
+		*result = ACT_RESULT_FAILED;
+		*error_code = ACT_RESULT_ERROR_UNKNOWN;
+		*line = error; //0x00xx avec xx = error
+	} else if(error) {
+		COMPONENT_log(LOG_LEVEL_Error, "Error AX12 %d\n", error);
+		return FALSE;
+	} else return FALSE;
+
+	return TRUE;
+}
+
+bool_e ACTQ_check_status_dcmotor(Uint8 dcmotor_id, bool_e timeout_is_ok, Uint8* result, Uint8* error_code, Uint16* line) {
+	DCM_working_state_e asserState = DCM_get_state(dcmotor_id);
+	Uint16 dummy;
+
+	if(!result) result = (Uint8*)&dummy;
+	if(!error_code) error_code = (Uint8*)&dummy;
+	if(!line) line = &dummy;
+
+	if(asserState == DCM_IDLE) {
+		*result =    ACT_RESULT_DONE;
+		*error_code = ACT_RESULT_ERROR_OK;
+		*line = __LINE__;
+	} else if(asserState == DCM_TIMEOUT) {
+		if(timeout_is_ok) {
+			*result =    ACT_RESULT_DONE;
+			*error_code = ACT_RESULT_ERROR_OK;
+			*line = __LINE__;
+		} else {
+			*result =    ACT_RESULT_FAILED;
+			*error_code = ACT_RESULT_ERROR_TIMEOUT;
+			*line = __LINE__;
+		}
+	} else return FALSE;
+
+	return TRUE;
+}
+
+//Renvoie un retour à la strat dans tous les cas
+bool_e ACTQ_finish_SendResult(queue_id_t queue_id, Uint11 act_sid, Uint8 result, Uint8 error_code, Uint16 param) {
+	ACTQ_sendResultWithParam(act_sid, QUEUE_get_arg(queue_id)->canCommand, result, error_code, param);
+
+	if(result != ACT_RESULT_DONE)
+		return FALSE;
+
+	return TRUE;
+}
+
+//Retour à la strat seulement si l'opération à fail
+bool_e ACTQ_finish_SendResultIfFail(queue_id_t queue_id, Uint11 act_sid, Uint8 result, Uint8 error_code, Uint16 param) {
+	if(result != ACT_RESULT_DONE && error_code != ACT_RESULT_ERROR_OTHER) {
+		ACTQ_sendResultWithParam(act_sid, QUEUE_get_arg(queue_id)->canCommand, result, error_code, param);
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+//Retour à la strat seulement si l'opération à reussi
+bool_e ACTQ_finish_SendResultIfSuccess(queue_id_t queue_id, Uint11 act_sid, Uint8 result, Uint8 error_code, Uint16 param) {
+	if(result == ACT_RESULT_DONE) {
+		ACTQ_sendResultWithParam(act_sid, QUEUE_get_arg(queue_id)->canCommand, result, error_code, param);
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+//Ne fait aucun retour
+bool_e ACTQ_finish_SendNothing(queue_id_t queue_id, Uint11 act_sid, Uint8 result, Uint8 error_code, Uint16 param) {
+	if(result == ACT_RESULT_DONE)
+		return TRUE;
+
+	return FALSE;
+}
+
+static void ACTQ_printResult(Uint11 originalSid, Uint8 originalCommand, Uint8 result, Uint8 errorCode, CAN_result_param_type_t paramType, Uint16 param) {
+#ifdef OUTPUT_LOG
+	const char* originalSidStr = "Unknown";
+	const char* resultStr = "Unknown";
+	const char* errorCodeStr ="Unknown error";
+
+	switch(originalSid) {
+		case ACT_BALLINFLATER:       originalSidStr = "BallInflater"; break;
+		case ACT_BALLLAUNCHER:       originalSidStr = "BallLauncher"; break;
+		case ACT_BALLSORTER:         originalSidStr = "BallSorter";   break;
+		case ACT_CANDLECOLOR:        originalSidStr = "CandleColor";  break;
+		case ACT_HAMMER:             originalSidStr = "Hammer";       break;
+		case ACT_LIFT_LEFT:          originalSidStr = "LiftLeft";     break;
+		case ACT_LIFT_RIGHT:         originalSidStr = "LiftRight";    break;
+		case ACT_PLATE:              originalSidStr = "Plate";        break;
+		default:                     originalSidStr = "Unknown";      break;
+	}
+	switch(result) {
+		case ACT_RESULT_DONE:        resultStr = "Done";       break;
+		case ACT_RESULT_FAILED:      resultStr = "Failed";     break;
+		case ACT_RESULT_NOT_HANDLED: resultStr = "NotHandled"; break;
+		default:                     resultStr = "Unknown";    break;
+	}
+	switch(errorCode) {
+		case ACT_RESULT_ERROR_LOGIC:        errorCodeStr = "Logic";         break;
+		case ACT_RESULT_ERROR_NOT_HERE:     errorCodeStr = "NotHere";       break;
+		case ACT_RESULT_ERROR_NO_RESOURCES: errorCodeStr = "NoResources";   break;
+		case ACT_RESULT_ERROR_OK:           errorCodeStr = "Ok";            break;
+		case ACT_RESULT_ERROR_OTHER:        errorCodeStr = "Other";         break;
+		case ACT_RESULT_ERROR_TIMEOUT:      errorCodeStr = "Timeout";       break;
+		case ACT_RESULT_ERROR_UNKNOWN:      errorCodeStr = "Unknown";       break;
+		default:                            errorCodeStr = "Unknown error"; break;
+	}
+	log_level_e level = LOG_LEVEL_Debug;
+	if(result != ACT_RESULT_DONE)
+		level = LOG_LEVEL_Error;
+	if(paramType == CAN_TPT_Normal) {
+		COMPONENT_log(level, "Result msg: Act: %s(0x%x), cmd: 0x%x(%u), result: %s(%u), error: %s(%u), param: 0x%x(%u)\n",
+			originalSidStr, originalSid & 0xFF,
+			originalCommand, originalCommand,
+			resultStr, result,
+			errorCodeStr, errorCode,
+			param, param);
+	} else if(paramType == CAN_TPT_Line) {
+		COMPONENT_log(level, "Result msg: Act: %s(0x%x), cmd: 0x%x(%u), result: %s(%u), error: %s(%u), line: %u\n",
+			originalSidStr, originalSid & 0xFF,
+			originalCommand, originalCommand,
+			resultStr, result,
+			errorCodeStr, errorCode,
+			param);
+	} else {
+		COMPONENT_log(level, "Result msg: Act: %s(0x%x), cmd: 0x%x(%u), result: %s(%u), error: %s(%u)\n",
+			originalSidStr, originalSid & 0xFF,
+			originalCommand, originalCommand,
+			resultStr, result,
+			errorCodeStr, errorCode);
+	}
+#endif
+}
---
+++ 3_Actionneur/CurrentSoftware/act_queue_utils.h
@@ -0,0 +1,76 @@
+/*
+ *	Club Robot ESEO 2012 - 2013
+ *	Krusty & Tiny
+ *
+ *	Fichier : act_queue_utils.h
+ *	Package : Carte Actionneur
+ *	Description : Propose des fonctions pour gérer les actions avec la pile.
+ *	Auteur : Alexis
+ *	Version 20130420
+ */
+
+#ifndef ACT_QUEUE_UTILS_H
+#define	ACT_QUEUE_UTILS_H
+	#include "queue.h"
+
+	//Met sur la pile une action qui sera gérée par act_function_ptr avec en paramètre param. L'action est protégée par semaphore avec act_id
+	//Cette fonction est appelée par les fonctions de traitement des messages CAN de chaque actionneur.
+	void ACTQ_push_operation_from_msg(CAN_msg_t* msg, QUEUE_act_e act_id, action_t act_function_ptr, Uint16 param);
+
+	//Envoie le message CAN de retour à la strat (et affiche des infos de debuggage si activé)
+	void ACTQ_sendResult(Uint11 originalSid, Uint8 originalCommand, Uint8 result, Uint8 errorCode);
+
+	//Comme CAN_sendResult mais ajoute un paramètre au message. Peut servir pour debuggage.
+	void ACTQ_sendResultWithParam(Uint11 originalSid, Uint8 originalCommand, Uint8 result, Uint8 errorCode, Uint16 param);
+
+	//Comme CAN_sendResultWithParam mais le paramètre est considéré comme étant un numéro de ligne.
+	void ACTQ_sendResultWitExplicitLine(Uint11 originalSid, Uint8 originalCommand, Uint8 result, Uint8 errorCode, Uint16 lineNumber);
+
+	//Macro pour avoir la ligne a laquelle cette macro est utilisé comme paramètre à CAN_sendResultWithParam
+	#define ACTQ_sendResultWithLine(originalSid, originalCommand, result, errorCode) ACTQ_sendResultWitExplicitLine(originalSid, originalCommand, result, errorCode, __LINE__)
+
+
+	//Gestion des états des actionneurs (ax12 / DCMotor)
+	//Retourne TRUE si l'action en cours n'est pas terminée, sinon FALSE
+
+	/** Verifie l'état de l'ax12 et gère la file en conséquence.
+	 *
+     * @param queueId l'identifiant de la file
+     * @param ax12Id l'identifiant de l'ax12 à gérer
+     * @param wantedPosition position voulue en degré
+     * @param pos_epsilon précision de la position voulue, tant qu'on à pas atteint wantedPosition_degre +/- pos_epsilon on considère qu'on est pas rendu à la bonne position
+     * @param timeout_ms_x100 timeout en centaine de msec (10 = 1 sec)
+     * @param large_epsilon comme pos_epsilon mais utilisé après un timeout. Après un timeout, on vérifie si on est proche de la position voulue (utile pour des pinces par ex), si oui il n'y a pas d'erreur
+	 * @param result pointeur vers une variable qui contiendra le résultat de l'opération si la fonction retourne TRUE (ACT_RESULT*)
+	 * @param error_code pointeur vers une variable qui contiendra le l'erreur lié a l'opération si la fonction retourne TRUE (ACT_RESULT_ERROR*)
+	 * @param line pointeur vers une variable qui contiendra la ligne qui affecte l'erreur dans les variables pointeurs
+     * @return TRUE si l'ax12 a fini sa commande, FALSE sinon
+     */
+	bool_e ACTQ_check_status_ax12(queue_id_t queueId, Uint8 ax12Id, Uint16 wantedPosition, Uint16 pos_epsilon, Uint16 timeout_ms_x100, Uint16 large_epsilon, Uint8* result, Uint8* error_code, Uint16* line);
+
+	/** Verifie l'état d'un moteur DC et gère la file en conséquence.
+	 *
+     * @param dcmotor_id identifiant du moteur (passé en parametre des fonctions de DCMotor2.h)
+     * @param timeout_is_ok TRUE si lors d'un timeout, le resultat doit être Ok, FALSE si on doit indiquer une erreur. (Mettre à TRUE pour des moteur qui devrons forcer en continu sur qquechose et qui n'atteindront jamais la position voulue)
+     * @param result pointeur vers une variable qui contiendra le résultat de l'opération si la fonction retourne TRUE (ACT_RESULT*)
+     * @param error_code pointeur vers une variable qui contiendra le l'erreur lié a l'opération si la fonction retourne TRUE (ACT_RESULT_ERROR*)
+     * @param line pointeur vers une variable qui contiendra la ligne qui affecte l'erreur dans les variables pointeurs
+     * @return TRUE si le moteur a fini sa commande, FALSE sinon
+     */
+	bool_e ACTQ_check_status_dcmotor(Uint8 dcmotor_id, bool_e timeout_is_ok, Uint8* result, Uint8* error_code, Uint16* line);
+
+	//Callback
+	//Renvoie un retour à la strat dans tous les cas
+	bool_e ACTQ_finish_SendResult(queue_id_t queue_id, Uint11 act_sid, Uint8 result, Uint8 error_code, Uint16 param);
+
+	//Retour à la strat seulement si l'opération à fail
+	bool_e ACTQ_finish_SendResultIfFail(queue_id_t queue_id, Uint11 act_sid, Uint8 result, Uint8 error_code, Uint16 param);
+
+	//Retour à la strat seuelement si l'opération à reussi
+	bool_e ACTQ_finish_SendResultIfSuccess(queue_id_t queue_id, Uint11 act_sid, Uint8 result, Uint8 error_code, Uint16 param);
+
+	//Ne fait aucun retour
+	bool_e ACTQ_finish_SendNothing(queue_id_t queue_id, Uint11 act_sid, Uint8 result, Uint8 error_code, Uint16 param);
+
+
+#endif	/* ACT_QUEUE_UTILS_H */
