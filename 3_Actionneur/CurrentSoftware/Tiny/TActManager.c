f230d35159eae50e316e6143de78bcadfde9d401
amurzeau <amurzeau@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2013-04-24 20:53:27 +0000

    Ajout effectif des 3 nouveaux fichier au svn ... Ajout des nouvelles fonctionnalité des bras à verre dans TActManager

--- 3_Actionneur/CurrentSoftware/Tiny/TActManager.c
+++ 3_Actionneur/CurrentSoftware/Tiny/TActManager.c
@@ -15,6 +15,7 @@
 #include "THammer.h"
 #include "TBall_inflater.h"
 #include "TCandle_color_sensor.h"
+#include "TPlier.h"
 #include "../QS/QS_CANmsgList.h"
 #include "../QS/QS_ax12.h"
 #include "../act_queue_utils.h"
@@ -26,6 +27,7 @@ void ACTMGR_init() {
 	HAMMER_init();
 	BALLINFLATER_init();
 	CANDLECOLOR_init();
+	PLIER_init();
 	ACTMGR_reset_act();
 }
 
@@ -45,6 +47,8 @@ bool_e ACTMGR_process_msg(CAN_msg_t* msg) {
 		return TRUE;
 	if(CANDLECOLOR_CAN_process_msg(msg))
 		return TRUE;
+	if(PLIER_CAN_process_msg(msg))
+		return TRUE;
 
 	return FALSE;
 }
@@ -54,18 +58,32 @@ void ACTMGR_stop() {
 	HAMMER_stop();
 	BALLINFLATER_stop();
 	CANDLECOLOR_stop();
+	PLIER_stop();
 }
 
 static void ACTMGR_run_reset_act(queue_id_t queueId, bool_e init) {
 	if(init) {
 		//Init des actionneurs
 	} else {
-		if(AX12_is_ready(2)) {
+		if(AX12_is_ready(PLIER_LEFT_AX12_ID)) {
+			queue_id_t subQueue;
+
 			//Hammer
-			QUEUE_add(queueId, &QUEUE_take_sem, (QUEUE_arg_t){0, 0, NULL}, QUEUE_ACT_Hammer);
-			QUEUE_add(queueId, &HAMMER_run_command, (QUEUE_arg_t){ACT_HAMMER_MOVE_TO, HAMMER_ACT_MOVE_TO_INIT_POS, &ACTQ_finish_SendNothing}, QUEUE_ACT_Hammer);
-			QUEUE_add(queueId, &QUEUE_give_sem, (QUEUE_arg_t){0, 0, NULL}, QUEUE_ACT_Hammer);
+			subQueue = QUEUE_create();
+			QUEUE_add(subQueue, &QUEUE_take_sem, (QUEUE_arg_t){0, 0, NULL}, QUEUE_ACT_Hammer);
+			QUEUE_add(subQueue, &HAMMER_run_command, (QUEUE_arg_t){ACT_HAMMER_MOVE_TO, HAMMER_ACT_MOVE_TO_INIT_POS, &ACTQ_finish_SendNothing}, QUEUE_ACT_Hammer);
+			QUEUE_add(subQueue, &QUEUE_give_sem, (QUEUE_arg_t){0, 0, NULL}, QUEUE_ACT_Hammer);
+
+			//Plier
+			//TODO: ajouter l'init de Plier quand il sera fonctionnel
+//			subQueue = QUEUE_create();
+//			QUEUE_add(subQueue, &QUEUE_take_sem, (QUEUE_arg_t){0, 0, NULL}, QUEUE_ACT_Plier);
+//			QUEUE_add(subQueue, &PLIER_run_command, (QUEUE_arg_t){ACT_PLIER_CLOSE, PLIER_CS_CloseRightAX12, &ACTQ_finish_SendNothing}, QUEUE_ACT_Plier);
+//			QUEUE_add(subQueue, &PLIER_run_command, (QUEUE_arg_t){ACT_PLIER_CLOSE, PLIER_CS_CloseLeftAX12,  &ACTQ_finish_SendNothing}, QUEUE_ACT_Plier);
+//			QUEUE_add(subQueue, &QUEUE_give_sem, (QUEUE_arg_t){0, 0, NULL}, QUEUE_ACT_Plier);
+
 			QUEUE_behead(queueId);
+
 		} else if(global.match_started == TRUE) {
 			//Le match a démarré, on arrete d'essayer de bouger les actionneurs
 			QUEUE_behead(queueId);
