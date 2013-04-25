3542b7dd920af598bf290060e8bedb18547b1b03
Alexis Murzeau <amubtdx@outlook.fr>
2013-04-25 15:53:13 +0000

    Changement ordre des actions ball_sorter *+ maj commentaire

--- 3_Actionneur/CurrentSoftware/Krusty/KBall_sorter_config.h
+++ 3_Actionneur/CurrentSoftware/Krusty/KBall_sorter_config.h
@@ -18,9 +18,8 @@
 	#define BALLSORTER_AX12_ASSER_TIMEOUT           10     //unité: 0.1s  (une valeur de 20 correspond à 0.2s soit 200ms)
 	#define BALLSORTER_AX12_EJECT_CHERRY_POS        132
 	#define BALLSORTER_AX12_NEXT_CHERRY_POS         250
-	#define BALLSORTER_AX12_DETECT_CHERRY_POS       165
+	#define BALLSORTER_AX12_DETECT_CHERRY_POS       168
 	#define BALLSORTER_DETECT_TIME                  2      //Attend x * 0.1 s après que l'AX12 soit rendu à sa position avant de capter la couleur de la cerise (unité: 0.1s  (une valeur de 20 correspond à 0.2s soit 200ms))
 	#define BALLSORTER_USE_SICK_SENSOR
 
 #endif	/* KBALL_SORTER_CONFIG_H */
-
