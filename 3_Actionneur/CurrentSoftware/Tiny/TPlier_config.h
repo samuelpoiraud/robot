78c75ea985e2025fac102ce7e2a43dd97eb9f000
amurzeau <amurzeau@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2013-05-07 21:48:08 +0000

    Config plier + ouverture pince plate plus grande

--- 3_Actionneur/CurrentSoftware/Tiny/TPlier_config.h
+++ 3_Actionneur/CurrentSoftware/Tiny/TPlier_config.h
@@ -13,17 +13,17 @@
 #define	TPLIER_CONFIG_H
 
 	#define PLIER_LEFT_AX12_MAX_TORQUE_PERCENT   50
-	#define PLIER_LEFT_AX12_OPEN_POS             0  //TODO: à regler !!
-	#define PLIER_LEFT_AX12_CLOSED_POS           0  //TODO: à regler !!
+	#define PLIER_LEFT_AX12_OPEN_POS             40  //TODO: à regler !!
+	#define PLIER_LEFT_AX12_CLOSED_POS           223  //TODO: à regler !!
 
 	#define PLIER_RIGHT_AX12_MAX_TORQUE_PERCENT  50
-	#define PLIER_RIGHT_AX12_OPEN_POS            0  //TODO: à regler !!
-	#define PLIER_RIGHT_AX12_CLOSED_POS          0  //TODO: à regler !!
+	#define PLIER_RIGHT_AX12_OPEN_POS            49  //TODO: à regler !!
+	#define PLIER_RIGHT_AX12_CLOSED_POS          237  //TODO: à regler !!
 
 
-	#define PLIER_AX12_ASSER_POS_EPSILON         3
-	#define PLIER_AX12_ASSER_TIMEOUT             5  //en ms*100, 20 == 2 sec
-	#define PLIER_AX12_POS_LARGE_EPSILON         20
+	#define PLIER_AX12_ASSER_POS_EPSILON         5
+	#define PLIER_AX12_ASSER_TIMEOUT             20  //en ms*100, 20 == 2 sec
+	#define PLIER_AX12_POS_LARGE_EPSILON         50
 
 
 
