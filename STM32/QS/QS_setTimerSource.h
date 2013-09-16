02b1888af484132e436e4990d4df149256f1d091
amurzeau <amurzeau@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2013-09-16 22:51:32 +0000

    Ajout AX12 stm32 (+ modif TIMER_SRC_TIMER_DisableIT/TIMER_SRC_TIMER_EnableIT changé en macro-fonction) Pas testé (mais compile)

--- STM32/QS/QS_setTimerSource.h
+++ STM32/QS/QS_setTimerSource.h
@@ -42,8 +42,8 @@
 	#define TIMER_SRC_TIMER_start_ms(period_ms) TIMER_SRC_watchdog_timeout_id = WATCHDOG_create(period_ms, &TIMER_SRC_watchdog_timeout_recv, TRUE)
 //	#define TIMER_SRC_TIMER_start_us(period_us) TIMER_SRC_watchdog_timeout_id = WATCHDOG_create(period_us/1000, &TIMER_SRC_watchdog_timeout_recv, TRUE)
 	#define TIMER_SRC_TIMER_stop() WATCHDOG_stop(TIMER_SRC_watchdog_timeout_id)
-	#define TIMER_SRC_TIMER_DisableIT WATCHDOG_disable_timeout(TIMER_SRC_watchdog_timeout_id)
-	#define TIMER_SRC_TIMER_EnableIT WATCHDOG_enable_timeout(TIMER_SRC_watchdog_timeout_id)
+	#define TIMER_SRC_TIMER_DisableIT() WATCHDOG_disable_timeout(TIMER_SRC_watchdog_timeout_id)
+	#define TIMER_SRC_TIMER_EnableIT() WATCHDOG_enable_timeout(TIMER_SRC_watchdog_timeout_id)
 #else
 	#include "QS_timer.h"
 
