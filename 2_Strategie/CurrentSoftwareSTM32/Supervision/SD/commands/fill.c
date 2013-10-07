e77672d6e39b15301122035c1597808fbce8bed6
Alexis Murzeau <amubtdx@outlook.fr>
2013-10-07 19:41:55 +0000

    Ajout commande fill vide (oublié de commit ces fichiers du club ..., avec ce fichier vide, au moins ça compile)

---
+++ 2_Strategie/CurrentSoftwareSTM32/Supervision/SD/commands/fill.c
@@ -0,0 +1,8 @@
+#include "fill.h"
+
+const char term_cmd_fill_brief[] = "";
+const char term_cmd_fill_help[] = "";
+
+int term_cmd_fill(int argc, const char *argv[]) {
+	return 0;
+}
---
+++ 2_Strategie/CurrentSoftwareSTM32/Supervision/SD/commands/fill.h
@@ -0,0 +1,8 @@
+#ifndef FILL_H
+#define FILL_H
+
+int term_cmd_fill(int argc, const char *argv[]);
+extern const char term_cmd_fill_brief[];
+extern const char term_cmd_fill_help[];
+
+#endif // FILL_H
