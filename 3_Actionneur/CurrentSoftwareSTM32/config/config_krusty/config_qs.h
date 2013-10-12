cef9b531cbdb3e82c6de1fe9378bc2e94577e73c
Alexis Murzeau <amubtdx@outlook.fr>
2013-10-12 21:01:28 +0000

    Ajout keyword id au fichier de config actionneur

--- 3_Actionneur/CurrentSoftwareSTM32/config/config_krusty/config_qs.h
+++ 3_Actionneur/CurrentSoftwareSTM32/config/config_krusty/config_qs.h
@@ -1,7 +1,7 @@
 /*
  *  Club Robot ESEO 2013 - 2014
  *
- *  $Id: config_qs.h 904 2013-10-12 12:04:27Z amurzeau $
+ *  $Id$
  *
  *  Package : Carte Actionneur
  *  Description : Configuration des modules de QS
--- 3_Actionneur/CurrentSoftwareSTM32/config/config_use.h
+++ 3_Actionneur/CurrentSoftwareSTM32/config/config_use.h
@@ -1,7 +1,7 @@
 /*
  *  Club Robot ESEO 2013 - 2014
  *
- *  $Id: config_use.h 902 2013-10-11 23:01:25Z amurzeau $
+ *  $Id$
  *
  *  Package : Carte Actionneur
  *  Description : Activation de modules et fonctionnalités
