2a7eebf062013219babd3e3ebda50688add1be89
Alexis Murzeau <amubtdx@outlook.fr>
2013-10-14 17:31:13 +0000

    Ajout UART3

--- STM32/QS/QS_uart.h
+++ STM32/QS/QS_uart.h
@@ -54,5 +54,18 @@
 			Uint8 UART2_get_next_msg();
 		#endif /* def USE_UART2RXINTERRUPT */
 	#endif
+	#ifdef USE_UART3
+		/*	fonction envoyant un octet sur l'uart3 */
+		void UART3_putc(Uint8 mes);
+		#ifdef USE_UART3RXINTERRUPT
+			/* fonction indiquant si il y a un octet à lire */
+			bool_e UART3_data_ready();
+
+			/*	fonction lisant le prochain octet à lire dans le
+			 *	buffer de reception uart3
+			 */
+			Uint8 UART3_get_next_msg();
+		#endif /* def USE_UART3RXINTERRUPT */
+	#endif
 
 #endif /* ndef QS_UART_H */
