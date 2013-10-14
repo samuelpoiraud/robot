01e1156b776daadd316b7fe1897c0dfc786f4964
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2013-10-14 18:51:26 +0000

    Ajout des fonctionnalitées goangle et gohome sur le terminal et optimisation de la fonction goto

---
+++ 2_Strategie/CurrentSoftwareSTM32/Supervision/SD/commands/gohome.h
@@ -0,0 +1,15 @@
+/*
+ * gohome.h
+ *
+ *  Created on: 12 oct. 2013
+ *      Author: Arnaud
+ */
+
+#ifndef GOHOME_H_
+#define GOHOME_H_
+
+int term_cmd_gohome(int argc, const char *argv[]);
+extern const char term_cmd_gohome_brief[];
+extern const char term_cmd_gohome_help[];
+
+#endif /* GOHOME_H_ */
--- 2_Strategie/CurrentSoftwareSTM32/Supervision/SD/commands/goto.c
+++ 2_Strategie/CurrentSoftwareSTM32/Supervision/SD/commands/goto.c
@@ -24,9 +24,20 @@ int term_cmd_goto(int argc, const char *argv[]) {
 	if(!argtolong(argv[1], 0, &y))
 		return EINVAL;
 
-
 	if(x >= 200 && x <= 1800 && y >= 200 && y <= 2800)
-		ASSER_push_goto(x, y, FAST, ANY_WAY, 0, END_AT_BREAK, TRUE);
+			ASSER_push_goto(x, y, FAST, ANY_WAY, 0, END_AT_BREAK, FALSE);
+
+	if(global.env.pos.x < 250)
+		ASSER_push_goto(250, global.env.pos.y, FAST, ANY_WAY, 0, END_AT_BREAK, TRUE);
+	else if(global.env.pos.x > 1750)
+		ASSER_push_goto(1750, global.env.pos.y, FAST, ANY_WAY, 0, END_AT_BREAK, TRUE);
+
+	if(global.env.pos.y < 250)
+		ASSER_push_goto(global.env.pos.x, 250, FAST, ANY_WAY, 0, END_AT_BREAK, TRUE);
+	else if(global.env.pos.y > 2750)
+		ASSER_push_goto(global.env.pos.x, 2750, FAST, ANY_WAY, 0, END_AT_BREAK, TRUE);
+
+
 	else
 		return EINVAL;
 
