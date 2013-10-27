b8c45f330f51d18b16fffeeccd27457b560f0286
amurzeau <amurzeau@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2013-10-27 11:32:38 +0000

    Renommage toolchain-gcc-arm-embedded.cmake -> CMakeToolchain.cmake (indépendant du toolchain, si c'est la QS stm32 c'est le toolchain arm, si c'est QS dspic, c'est toolchain c30)

---
+++ DSPIC30F/QS/CMakeToolchain.cmake
@@ -0,0 +1,63 @@
+# CMake Toolchain file for the gcc-arm-embedded toolchain.
+# https://launchpad.net/gcc-arm-embedded
+#
+# Copyright (c) 2013 Swift Navigation Inc.
+# Contact: Fergus Noble <fergus@swift-nav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+
+include(CMakeForceCompiler)
+
+# Targeting an embedded system, no OS.
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR 30F6010A)
+
+CMAKE_FORCE_C_COMPILER(pic30-gcc GNU)
+CMAKE_FORCE_CXX_COMPILER(pic30-g++ GNU)
+
+# Find the target environment prefix..
+# First see where gcc is keeping libc-elf.a
+execute_process(
+  COMMAND ${CMAKE_C_COMPILER} -print-file-name=libc-elf.a
+  OUTPUT_VARIABLE CMAKE_INSTALL_PREFIX
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+# Strip the filename off
+get_filename_component( CMAKE_INSTALL_PREFIX
+  "${CMAKE_INSTALL_PREFIX}" PATH
+)
+# Then find the canonical path to the directory one up from there
+get_filename_component(CMAKE_INSTALL_PREFIX
+  "${CMAKE_INSTALL_PREFIX}/.." REALPATH
+)
+set(CMAKE_INSTALL_PREFIX  ${CMAKE_INSTALL_PREFIX} CACHE FILEPATH
+    "Install path prefix, prepended onto install directories.")
+
+message(STATUS "Toolchain prefix: ${CMAKE_INSTALL_PREFIX}")
+
+set(CMAKE_FIND_ROOT_PATH  ${CMAKE_INSTALL_PREFIX})
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+SET(CMAKE_C_FLAGS "-omf=elf -mcpu=30F6010A -Wall" CACHE INTERNAL "c compiler flags")
+SET(CMAKE_CXX_FLAGS "-omf=elf -mcpu=30F6010A -Wall" CACHE INTERNAL "cxx compiler flags")
+SET(CMAKE_ASM_FLAGS "-omf=elf -mcpu=30F6010A -Wall -x assembler-with-cpp" CACHE INTERNAL "asm compiler flags")
+SET(CMAKE_EXE_LINKER_FLAGS "-omf=elf -Wall -Wl,--defsym=__MPLAB_BUILD=1" CACHE INTERNAL "exe link flags")
+
+# When we break up long strings in CMake we get semicolon
+# separated lists, undo this here...
+string(REGEX REPLACE ";" " " CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "")
+
+set(BUILD_SHARED_LIBS OFF)
+
+find_program(CMAKE_OBJDUMP pic30-objdump)
+find_program(CMAKE_BIN2HEX pic30-bin2hex)
