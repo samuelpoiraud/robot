fe5bfbdb9647213c396620a28d36da7494106b03
agarry <agarry@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2013-11-14 16:19:51 +0000

    strat_reglage_odo_rotation(void) fini reste à la tester régler nb de tour à faire

--- 2_Strategie/CurrentSoftwareSTM32/strats_2014/actions_both_2014.c
+++ 2_Strategie/CurrentSoftwareSTM32/strats_2014/actions_both_2014.c
@@ -17,8 +17,9 @@
 
 
 #define DEFAULT_SPEED	(SLOW)
-
+#define ODOMETRIE_PLAGE 10 //La variation de la plage sur PI4096(12868) pour savoir si on doit modifier l'angle. Théoriquement avec 10, on devrait avoir au maximum de 2mm de décalage sur un 1m
 #define USE_CURVE	0
+#define NB_TOUR_ODO_ROTATION 1
 
 
 
@@ -35,16 +36,30 @@ void strat_reglage_odo_rotation(void){
 		PROCESS,
 		COMPARE_N_CORRECT,
 		REPORT,
+		PUSH_MOVE,
+		WAIT_END_OF_MOVE,
+		AVANCER,
+		TOUR1,
+		TOUR2,
 		ERROR = -1
 	}state_e;
 
 	static state_e state = IDLE;
+
+	/******TEST******/
+	//state = COMPARE_N_CORRECT;
+
 	static state_e from = IDLE;
+	static state_e inProcess = IDLE;
+	static bool_e timeout=FALSE;
+	static Sint16 i=0;
+	static Uint16 coefOdoRotation = 0x0000C5A2; //Mofifier la valeur KRUSTY_ODOMETRY_COEF_ROTATION_DEFAULT dans _Propulsion_config.h
+	CAN_msg_t msg;
 
 	switch(state){
 	case IDLE: //Cas d'attente et de réinitialisation
 		from = IDLE;
-		state = SORTIR;
+		state = AVANCER;
 		break;
 
 	case SORTIR: // Sert à s'écarter du mur au début et au cours de la procédure, revient dans tous les cas (sauf erreur) au calage
@@ -52,16 +67,23 @@ void strat_reglage_odo_rotation(void){
 		break;
 
 	case CALAGE: // Cale le robot pour regler le zero et comparer les valeurs: L'échapement dépend d'ou vient la machine
-		// Rush in the wall
+		//  in the wall
 		switch(from){
 		case IDLE:	// On vient du début de la procédure on doit donc initialiser le robot
 			state = REINIT;
 			break;
 		case PROCESS:	//On vient de finir le process on va donc traiter les valeurs
-			state = COMPARE_N_CORRECT;
+			state = PUSH_MOVE;
 			break;
 		case REINIT:	//On à remis le robot à zéro après un process, on relance la procédure
-			state = PROCESS; // Ha merde jai oublié davancer
+			state = AVANCER; // Ha merde jai oublié davancer
+			break;
+		case AVANCER:
+			state = PROCESS;
+			break;
+		case WAIT_END_OF_MOVE:
+			state = COMPARE_N_CORRECT;
+			break;
 		default:
 			state = ERROR;
 			break;
@@ -69,22 +91,95 @@ void strat_reglage_odo_rotation(void){
 		break;
 
 	case REINIT:	// Envoie le message can pour réinitialiser la position ou a minima l'angle du robot
-		//send message can set(0,0,0);
+		//send message can set(0,0,0);  le robot doit se trouver a peut pres a set(2000,500,PI4096)
 		from = REINIT;
+
+		msg.sid=ASSER_SET_POSITION;
+		msg.data[0]=2000 >> 8;
+		msg.data[1]=2000 & 0xFF;
+		msg.data[2]=500 >> 8;
+		msg.data[3]=500 & 0xFF;
+		msg.data[4]=PI4096 >> 8;
+		msg.data[5]=PI4096 & 0xFF;
+		CAN_send(&msg);
 		break;
 
+
+	case AVANCER://pour le faire avancer du bord
+		state = try_going(1000,COLOR_Y(500),AVANCER,CALAGE,ERROR,SLOW,FORWARD,NO_AVOIDANCE);
+		from = AVANCER;
+		break;
 	case PROCESS:	// Tourne 10 fois pour comparer l'angle
 		// Tourner 10 fois
-		if(TRUE)//Fini?
-			state = CALAGE;
+
+		if(i < NB_TOUR_ODO_ROTATION){
+			switch(inProcess){
+			case IDLE:
+				inProcess=try_go_angle(0,IDLE,TOUR1,ERROR,SLOW);
+				break;
+			case TOUR1:
+				inProcess=try_go_angle(4*PI4096/3,TOUR1,TOUR2,ERROR,SLOW);
+				break;
+			case TOUR2:
+				inProcess=try_go_angle(2*PI4096/3,TOUR2,IDLE,ERROR,SLOW);
+				if(inProcess == IDLE)
+					i++; // On incremente le i apres avoir fait un tour complet
+
+				break;
+			default:
+				state=ERROR;
+				break;
+			}
+		}
+		else{
+			state=CALAGE;
+		}
+
+
 		from = PROCESS;
 		break;
-
+	case PUSH_MOVE://Le fait forcer contre le mur si mal réglé
+		ASSER_push_rush_in_the_wall(BACKWARD,TRUE,PI4096,TRUE);//Le fait forcer en marche arriére pour protéger les cartes à l'avant
+		state = WAIT_END_OF_MOVE;
+		break;
+	case WAIT_END_OF_MOVE:
+		if(STACKS_wait_end_auto_pull(ASSER, &timeout)){
+			state = CALAGE;
+			from = WAIT_END_OF_MOVE;
+		}
+		break;
 	case COMPARE_N_CORRECT:
 		// compare l'angle reçu après calage par rapport au ZERO
 		//correction de la nouvelle odométrie
 		// Envoi du nouveau coefficient à la propulsion
 		//Renouvel le process autant de fois que nécessaire
+
+		//Si il est bien réglé l'angle interne du robot devrait etre de zero, sinon l'angle interne du robot s'est décalé quand le robot a approché le mur
+
+		debug_printf("L'ancgle globale est de %d\n",global.env.pos.angle);
+
+		if(global.env.pos.angle > (PI4096-ODOMETRIE_PLAGE) || global.env.pos.angle < (-PI4096-ODOMETRIE_PLAGE)){//Robot bien regle, on fait avec PI4096 car le robot recule au lieu d'avancer vers le mur
+			debug_printf("Ne pas modifier\n");
+
+		}else{//Recommencer procédure en modifiant les valeurs
+			debug_printf("Modifier valeur\n");
+			i=0;
+			state = REINIT;
+
+			//Modifier KRUSTY_ODOMETRY_COEF_ROTATION_DEFAULT selon l'angle obtenu
+			if(global.env.pos.angle > 0)//Si l'angle est entre 0 et PI4096
+				coefOdoRotation++;						//A voir peut modifier -- ici et ++ dans l'autre
+			else//Si l'angle est compris entre 0 et -PI4096
+				coefOdoRotation--;
+
+			//Envoie du message CAN
+			msg.sid=DEBUG_PROPULSION_REGLAGE_COEF_ODOMETRIE_ROTATION;
+			msg.data[0] = coefOdoRotation;
+			CAN_send(&msg);
+
+			debug_printf("Nouvelle valeur du coef odométrie rotation %x\n",coefOdoRotation);
+		}
+
 		break;
 
 	case REPORT:	// Correspond à la partie IHM du processus: Le robot affiche le coeff calculé sur le LCD pour modifications en programme
@@ -111,4 +206,3 @@ void strat_reglage_odo_rotation(void){
 /* ----------------------------------------------------------------------------- */
 /* 							Autre strats de test             			 */
 /* ----------------------------------------------------------------------------- */
-
