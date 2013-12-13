d0514fca05df07a7f11cf5e796674e26655c3af3
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2013-12-13 17:20:28 +0000

    Update scan triangle

--- 1_Propulsion/CurrentSoftware/scan_triangle.c
+++ 1_Propulsion/CurrentSoftware/scan_triangle.c
@@ -6,13 +6,16 @@
  */
 
 #include "scan_triangle.h"
+#include <math.h>
 
 #define NB_POINTS 90
 
 volatile struct{Sint16 dist[3]; Sint16 x[3]; Sint16 y[3]; position_t pos;} scan[NB_POINTS];
 
 typedef struct{
-	struct{Sint16 teta[3]; position_t pos;} data[90];
+	struct{Sint16 dist; Sint16 x; Sint16 y; position_t pos;} data[NB_POINTS];
+	Uint8 nbData;
+	Uint8 active;
 }object;
 
 typedef enum{
@@ -32,14 +35,15 @@ typedef enum{
 Uint8 moveCompleted, runCalcul;
 bool_e MESSAGE_CAN_SCAN = FALSE;
 
+
 static Uint8 EstDansLeCarre(Sint16 lx, Sint16 hx, Sint16 ly, Sint16 hy);
 static void Acknowledge_Intern(void);
 static Sint32 abs(Sint32 val);
 static void SCAN_TRIANGLE_in_process(Uint8 *n_mesure);
 
-//void SUPERVISOR_config_intern_acknowledge(void (*prt)(void));
 void SCAN_TRIANGLE_init(void){
 	ADC_init();
+	runCalcul = 0;
 	// Initialisation des convertisseurs analogiques numériques pour les DT10 de détection triangle
 }
 
@@ -158,7 +162,7 @@ void SCAN_TRIANGLE_process_it(void){
 
 		case DISPLAY :
 			if(y < NB_POINTS){
-				debug_printf("%d,%d\n", scan[y].x[0], scan[y].y[0]);
+				debug_printf("%d,%d;\n", scan[y].x[0], scan[y].y[0]);//, scan[y].x[1], scan[y].y[1], scan[y].x[2], scan[y].y[2]);
 				y++;
 			}else{
 				y = 0;
@@ -167,9 +171,9 @@ void SCAN_TRIANGLE_process_it(void){
 		break;
 
 		case WAIT_END :
-			if(temp > 5000){
+			if(temp > 50){
 					temp = 0;
-					state = PLACEMENT_TETA;
+					debug_printf("%ld,%ld,%ld\n", ((Sint32)(39970UL * ADC_getValue(0)) >> 16) - 56,((Sint32)(39970UL * ADC_getValue(1)) >> 16) - 56,((Sint32)(39970UL * ADC_getValue(2)) >> 16) - 56);
 				}
 				else
 					temp++;
@@ -180,7 +184,7 @@ void SCAN_TRIANGLE_process_it(void){
 static void SCAN_TRIANGLE_in_process(Uint8 *n_mesure){
 	Uint8 i;
 	if((*n_mesure) < NB_POINTS){
-		debug_printf("%ld\n", ((Sint32)(39970UL * ADC_getValue(0)) >> 16) - 56);
+		debug_printf("%ld,%ld,%ld\n", ((Sint32)(39970UL * ADC_getValue(0)) >> 16) - 56,((Sint32)(39970UL * ADC_getValue(1)) >> 16) - 56,((Sint32)(39970UL * ADC_getValue(2)) >> 16) - 56);
 		for(i=0;i<3;i++){
 			scan[(*n_mesure)].dist[i] = ((Sint32)(39970UL * ADC_getValue(i)) >> 16) - 56;
 		}
@@ -192,15 +196,100 @@ static void SCAN_TRIANGLE_in_process(Uint8 *n_mesure){
 
 void SCAN_TRIANGLE_calculate(void){
 	if(runCalcul){
-		Uint8 j, i;
+		Uint8 j, i, nb_objet = 0;
+		Uint16 nb_point = NB_POINTS;
 		Sint16 cos, sin;
-		for(j=0;j<NB_POINTS;j++){
-			for(i=0;i<3;i++){
-				COS_SIN_4096_get(scan[j].pos.teta, &cos, &sin);
-				scan[j].x[i] = scan[j].pos.x + 90*cos/4096 - (130 + scan[j].dist[i])*sin/4096;
-				scan[j].y[i] = scan[j].pos.y + 90*sin/4096 + (130 + scan[j].dist[i])*cos/4096;
+		Sint16 coef[3][NB_POINTS-1];
+		static object objet[3][20];
+
+
+		// Initialisation
+		for(i=0;i<20;i++)
+			for(j=0;j<3;j++){
+				objet[j][i].nbData = 0;
+				objet[j][i].active = 1;
+			}
+
+		// Calcul point x et y
+		for(i=0;i<NB_POINTS;i++){
+				COS_SIN_4096_get(scan[i].pos.teta, &cos, &sin);
+				scan[i].x[0] = scan[i].pos.x - 45*cos/4096 - (100 + scan[i].dist[0])*sin/4096;
+				scan[i].y[0] = scan[i].pos.y - 45*sin/4096 + (100 + scan[i].dist[0])*cos/4096;
+				scan[i].x[1] = scan[i].pos.x + 25*cos/4096 - (100 + scan[i].dist[1])*sin/4096;
+				scan[i].y[1] = scan[i].pos.y + 25*sin/4096 + (100 + scan[i].dist[1])*cos/4096;
+				scan[i].x[2] = scan[i].pos.x + 80*cos/4096 - (100 + scan[i].dist[2])*sin/4096;
+				scan[i].y[2] = scan[i].pos.y + 80*sin/4096 + (100 + scan[i].dist[2])*cos/4096;
+		}
+
+		// Calcul coefficient directeur
+		for(i=0;i<NB_POINTS-1;i++)
+			for(j=0;j<3;j++){
+				if((scan[i+1].x[j]-scan[i].x[j])
+				  != 0)
+					coef[j][i] = (10*(10*scan[i+1].y[j]-10*scan[i].y[j]))/(10*scan[i+1].x[j]-10*scan[i].x[j]);
+				else if(i-1 >= 0){
+					debug_printf("\nici");
+					coef[j][i] = coef[j][i-1];
+				}
+				else{
+					debug_printf("connard dégage d'ici");
+					coef[j][i] = 0;
+				}
+			}
+		debug_printf("\nCoefficient directeur : \n");
+		for(i=0;i<NB_POINTS-1;i++)
+			debug_printf("%d\n", coef[0][i]);
+
+		// Détection des différents objets
+		for(i=0;i<NB_POINTS-1;i++){
+			for(j=0;j<3;j++){
+				if(nb_objet <20){
+					if(abs(100*coef[j][i+1] - 100*coef[j][i]) > 1)
+						nb_objet++;
+					objet[j][nb_objet].data[objet[j][nb_objet].nbData].dist = scan[i].dist[j];
+					objet[j][nb_objet].data[objet[j][nb_objet].nbData].x = scan[i].x[j];
+					objet[j][nb_objet].data[objet[j][nb_objet].nbData].y = scan[i].y[j];
+					objet[j][nb_objet].data[objet[j][nb_objet].nbData].pos = scan[i].pos;
+					objet[j][nb_objet].nbData++;
+				}
 			}
 		}
+
+		debug_printf("\nNombre d'objet : %d\n", nb_objet);
+
+		// Calcul de la longueur de chaques objets
+		// Et test des objets probablement des triangles
+		Sint16 longueur[3][nb_objet];
+		for(i=0;i<nb_objet;i++){
+			for(j=0;j<3;j++){
+				longueur[j][i] = sqrt(((objet[j][i].data[0].y - objet[j][i].data[objet[j][i].nbData].y)
+						*(objet[j][i].data[0].y - objet[j][i].data[objet[j][i].nbData].y))
+						+((objet[j][i].data[0].y - objet[j][i].data[objet[j][i].nbData].x)
+						*(objet[j][i].data[0].y - objet[j][i].data[objet[j][i].nbData].x)));
+				if(longueur[j][i] > 100 && longueur[j][i] < 130)
+					objet[j][i].active = 0;
+
+			}
+		}
+
+		debug_printf("Longueur : \n");
+		for(i=0;i<nb_objet+1;i++)
+			debug_printf("%d\n",longueur[0][i]);
+
+
+		// Calcul du point milieu de chaques objets
+		position_t point_milieu[3][nb_objet];
+		for(i=0;i<nb_objet;i++){
+			for(j=0;j<3;j++){
+				point_milieu[j][i].x = (objet[j][i].data[0].x + objet[j][i].data[objet[j][i].nbData].x)/2;
+				point_milieu[j][i].y = (objet[j][i].data[0].y + objet[j][i].data[objet[j][i].nbData].y)/2;
+			}
+		}
+
+		debug_printf("Point milieu : \n");
+		for(i=0;i<nb_objet;i++)
+			debug_printf("%d %d\n",point_milieu[0][i].x,point_milieu[0][i].y);
+
 		runCalcul = 0;
 	}
 }
