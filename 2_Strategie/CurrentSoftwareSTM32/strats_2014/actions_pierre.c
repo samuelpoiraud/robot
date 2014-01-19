3969901dc7c560b61a4244d4c73be16ee6ee5543
agarry <agarry@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2014-01-19 15:23:17 +0000

    Ajout de la strat_manage_fresco

--- 2_Strategie/CurrentSoftwareSTM32/strats_2014/actions_pierre.c
+++ 2_Strategie/CurrentSoftwareSTM32/strats_2014/actions_pierre.c
@@ -26,6 +26,28 @@
 
 #define DECALAGE_LARGEUR 200
 #define ELOIGNEMENT_ARBRE 350
+#define POS_MIN_FRESCO 1300
+#define POS_MAX_FRESCO 1700
+
+
+//Pour Activer le mode manual de pose de fresque
+#define MODE_MANUAL_FRESCO FALSE
+#define POS_Y_MANUAL_FRESCO 1300 // Mettre une valeur entre 1300 et 1700
+
+
+//Les differente's actions que pierre devra faire lors d'un match
+bool_e action_fresco_filed = FALSE;
+
+
+
+//Provisoire pour le moment juste pour test
+#define ADVERSARY_DETECTED_HOKUYO FALSE
+#define FRESQUE_ENLEVER_APRS_1_COUP TRUE
+#define NB_MAX_ADVERSARY_FRESCO_POSITION   2
+volatile Uint16 adversary_fresco_positions[NB_MAX_ADVERSARY_FRESCO_POSITION]={1370,1360};
+volatile Uint8 adversary_fresco_index = 1;
+
+
 
 void strat_test_triangle_cote_rouge(void){
 	CREATE_MAE_WITH_VERBOSE(0,
@@ -152,6 +174,94 @@ void strat_test_triangle_cote_rouge(void){
 
 }
 
+void strat_JPO(){
+	CREATE_MAE_WITH_VERBOSE(0,
+		IDLE,
+		POS_DEPART,
+		POINT_A1, // Point arbre1A cote couloir
+		POINT_B3, // Point arbre1B rouge cote armoire
+		POINT_W3, // Point arbre2b cote jaune armoire
+		POINT_CO, // Pour deposser fruit
+		POINT_M0,
+		DEPOSER_FRESQUE,
+		RAMASSER_FRUIT_ARBRE1,
+		RAMASSER_FRUIT_ARBRE2,
+		RAMASSER_FRUIT_ARBRE1A,
+		RAMASSER_FRUIT_ARBRE1B,
+		DEPOSER_FRUIT_ROUGE,
+		DEPOSER_FRUIT_JAUNE,
+		POS_FIN,
+		DONE,
+		ERROR
+	);
+
+
+	CAN_msg_t msg;
+
+	switch(state){
+	case IDLE:
+		msg.sid=ASSER_SET_POSITION;
+		msg.data[0]=500 >> 8;
+		msg.data[1]=500 & 0xFF;
+		msg.data[2]=120 >> 8;
+		msg.data[3]=120 & 0xFF;
+		msg.data[4]=PI4096/2 >> 8;
+		msg.data[5]=PI4096/2 & 0xFF;
+		msg.size = 6;
+		CAN_send(&msg);
+
+		state = POS_DEPART;
+		break;
+	case POS_DEPART:
+		state = try_going(500,300,POS_DEPART,POINT_A1,ERROR,FAST,FORWARD,NO_AVOIDANCE);
+		break;
+	case POINT_A1:
+		state = try_going(740,400,POINT_A1,RAMASSER_FRUIT_ARBRE1,ERROR,FAST,FORWARD,NO_AVOIDANCE);
+		break;
+	case RAMASSER_FRUIT_ARBRE1:
+		state = check_sub_action_result(strat_test_ramasser_fruit_arbre1(),RAMASSER_FRUIT_ARBRE1,POINT_W3,ERROR);
+		break;
+	case POINT_W3:
+		state = try_going(1670,1900,POINT_W3,RAMASSER_FRUIT_ARBRE2,ERROR,SLOW,FORWARD,NO_AVOIDANCE);
+		break;
+	case RAMASSER_FRUIT_ARBRE2:
+		state = check_sub_action_result(strat_test_ramasser_fruit_arbre2(),RAMASSER_FRUIT_ARBRE2,DEPOSER_FRUIT_JAUNE,ERROR);
+		break;
+	/*case RAMASSER_FRUIT_ARBRE1A:
+		state = check_sub_action_result(strat_test_ramasser_fruit_arbre1A(),RAMASSER_FRUIT_ARBRE1A,RAMASSER_FRUIT_ARBRE1B,ERROR);
+		break;
+	case POINT_B3:
+		state = try_going(1600,650,POINT_B3,RAMASSER_FRUIT_ARBRE1B,ERROR,SLOW,FORWARD,NO_AVOIDANCE);
+		break;
+	case RAMASSER_FRUIT_ARBRE1B:
+		state = check_sub_action_result(strat_test_ramasser_fruit_arbre1B(),RAMASSER_FRUIT_ARBRE1B,POINT_CO,ERROR);
+		break;
+	case POINT_CO:
+		state = try_going(500,1100,POINT_CO,DEPOSER_FRUIT,ERROR,SLOW,FORWARD,NO_AVOIDANCE);
+		break;*/
+	case DEPOSER_FRUIT_ROUGE:
+		state = check_sub_action_result(strat_test_deposser_fruit_rouge(),DEPOSER_FRUIT_ROUGE,DONE,ERROR);
+		break;
+	case DEPOSER_FRUIT_JAUNE:
+		state = check_sub_action_result(strat_test_deposser_fruit_jaune(),DEPOSER_FRUIT_JAUNE,POINT_M0,ERROR);
+		break;
+	case POINT_M0:
+		state = try_going(400,1500,POINT_M0,DEPOSER_FRESQUE,ERROR,FAST,FORWARD,NO_AVOIDANCE);
+		break;
+	case DEPOSER_FRESQUE:
+		state = check_sub_action_result(strat_file_fresco(1500),DEPOSER_FRESQUE,DONE,ERROR);
+		break;
+	case POS_FIN:
+		state = try_going(500,300,POS_FIN,DONE,ERROR,SLOW,FORWARD,NO_AVOIDANCE);
+		break;
+	case DONE:
+		break;
+	case ERROR:
+		break;
+	default:
+		break;
+	}
+}
 
 void strat_test_point(){
 	CREATE_MAE_WITH_VERBOSE(0,
@@ -269,10 +379,10 @@ void strat_test_fresque(){
 		state = try_going_until_break(800,1000,POINT_C1,POINT_M0,ERROR,FAST,FORWARD,NO_AVOIDANCE);
 		break;
 	case POINT_M0:
-		state = try_going(400,1500,POINT_M0,DEPOSER_FRESQUE,ERROR,FAST,FORWARD,NO_AVOIDANCE);
+		state = try_going(500,1500,POINT_M0,DEPOSER_FRESQUE,ERROR,FAST,FORWARD,NO_AVOIDANCE);
 		break;
 	case DEPOSER_FRESQUE:
-		state = check_sub_action_result(strat_file_fresco_center(),DEPOSER_FRESQUE,DONE,ERROR);
+		state = check_sub_action_result(strat_manage_fresco(),DEPOSER_FRESQUE,DONE,ERROR);
 		break;
 	case DONE:
 		break;
@@ -283,39 +393,84 @@ void strat_test_fresque(){
 	}
 }
 
-error_e strat_file_fresco_right(){
+error_e strat_manage_fresco(){
 	CREATE_MAE_WITH_VERBOSE(0,
 		IDLE,
-		WALL,
-		PUSH_MOVE,
-		WAIT_END_OF_MOVE,
-		END,
+		ADVERSARY_DETECTED,
+		FILE_FRESCO,
+		VERIFICATION,
+		LAST_CHANCE_FILE_FRESCO,
 		DONE,
 		ERROR
 	);
 
-	static bool_e timeout=FALSE;
+	static Sint16 posY = 1500;
 
 	switch(state){
 	case IDLE:
-		state = WALL;
-		break;
-	case WALL:
-		state = try_going(200,1700,WALL,PUSH_MOVE,ERROR,FAST,FORWARD,NO_AVOIDANCE);
-		break;
-	case PUSH_MOVE://Le fait forcer contre le mur pour poser la fresque
-		ASSER_push_rush_in_the_wall(BACKWARD,TRUE,0,TRUE);//Le fait forcer en marche arriere
-		state = WAIT_END_OF_MOVE;
-		break;
-	case WAIT_END_OF_MOVE:
-		if(STACKS_wait_end_auto_pull(ASSER, &timeout)){
-			state = END;
+		if(action_fresco_filed){
+			debug_printf("ERREUR, la fresque a déja été posé\n");
+			return END_OK;
 		}
+
+		if(MODE_MANUAL_FRESCO){
+			posY = POS_Y_MANUAL_FRESCO;
+			state = FILE_FRESCO;
+		}else if(ADVERSARY_DETECTED_HOKUYO == FALSE){//Pose les fresques au milieu si on a pas vu l'adversaire poser les siennes
+			posY = 1500;
+			state = FILE_FRESCO;
+		}else
+			state = ADVERSARY_DETECTED;
+
 		break;
-	case END:
-		state = try_going_until_break(400,1700,END,DONE,ERROR,FAST,FORWARD,NO_AVOIDANCE);
+	case ADVERSARY_DETECTED:
+		switch(adversary_fresco_index){
+		case 1:
+			if(adversary_fresco_positions[0] > 1500)
+				posY = POS_MIN_FRESCO;
+			else
+				posY = POS_MAX_FRESCO;
+
+			break;
+		case 2:
+			if(adversary_fresco_positions[0] > 1500 && adversary_fresco_positions[1] > 1500)
+				posY = POS_MIN_FRESCO;
+			else if(adversary_fresco_positions[0] < 1500 && adversary_fresco_positions[1] < 1500)
+				posY = POS_MAX_FRESCO;
+			else    //Pas fine
+				posY = 1500;
+
+			break;
+		default: // On ne sait jamais si aucun adversaire a été detecte
+			posY = 1500;
+			break;
+		}
+
+		state = FILE_FRESCO;
+
+		break;
+	case FILE_FRESCO:
+		state = check_sub_action_result(strat_file_fresco(posY),FILE_FRESCO,VERIFICATION,ERROR);
+		break;
+	case VERIFICATION:
+		if(FRESQUE_ENLEVER_APRS_1_COUP)//fresque plus presente sur le support grace au capteur
+			state = DONE;
+		else
+			state = LAST_CHANCE_FILE_FRESCO;
+
+		break;
+	case LAST_CHANCE_FILE_FRESCO:
+			if(entrance){
+				if(posY > 1500) //Ne prend plus en compte les positions des adversaires eu precedement (Elles sont fausses sinon ne serait pas dans cet état)
+					posY = POS_MIN_FRESCO;
+				else
+					posY = POS_MAX_FRESCO;
+			}
+
+			state = check_sub_action_result(strat_file_fresco(posY),LAST_CHANCE_FILE_FRESCO,DONE,ERROR);
 		break;
 	case DONE:
+		action_fresco_filed = TRUE;
 		return END_OK;
 		break;
 	case ERROR:
@@ -328,7 +483,7 @@ error_e strat_file_fresco_right(){
 	return IN_PROGRESS;
 }
 
-error_e strat_file_fresco_center(){
+error_e strat_file_fresco(Sint16 posY){
 	CREATE_MAE_WITH_VERBOSE(0,
 		IDLE,
 		WALL,
@@ -343,10 +498,18 @@ error_e strat_file_fresco_center(){
 
 	switch(state){
 	case IDLE:
+		if(posY > POS_MAX_FRESCO || posY < POS_MIN_FRESCO){
+			debug_printf("ERREUR Mauvaise position envoyer en fresque\n");
+			return NOT_HANDLED;
+		}
+
+		timeout=FALSE;
+
 		state = WALL;
 		break;
 	case WALL:
-		state = try_going(200,1500,WALL,PUSH_MOVE,ERROR,FAST,FORWARD,NO_AVOIDANCE);
+
+		state = try_going(200,posY,WALL,PUSH_MOVE,ERROR,FAST,BACKWARD,NO_AVOIDANCE);
 		break;
 	case PUSH_MOVE://Le fait forcer contre le mur pour poser la fresque
 		ASSER_push_rush_in_the_wall(BACKWARD,TRUE,0,TRUE);//Le fait forcer en marche arriere
@@ -358,54 +521,10 @@ error_e strat_file_fresco_center(){
 		}
 		break;
 	case END:
-		state = try_going_until_break(400,1500,END,DONE,ERROR,FAST,FORWARD,NO_AVOIDANCE);
+		state = try_going_until_break(400,posY,END,DONE,ERROR,FAST,FORWARD,NO_AVOIDANCE);
 		break;
 	case DONE:
-		return END_OK;
-		break;
-	case ERROR:
-		return NOT_HANDLED;
-		break;
-	default:
-		break;
-	}
-
-	return IN_PROGRESS;
-}
-
-error_e strat_file_fresco_left(){
-	CREATE_MAE_WITH_VERBOSE(0,
-		IDLE,
-		WALL,
-		PUSH_MOVE,
-		WAIT_END_OF_MOVE,
-		END,
-		DONE,
-		ERROR
-	);
-
-	static bool_e timeout=FALSE;
-
-	switch(state){
-	case IDLE:
-		state = WALL;
-		break;
-	case WALL:
-		state = try_going(200,1300,WALL,PUSH_MOVE,ERROR,FAST,FORWARD,NO_AVOIDANCE);
-		break;
-	case PUSH_MOVE://Le fait forcer contre le mur pour poser la fresque
-		ASSER_push_rush_in_the_wall(BACKWARD,TRUE,0,TRUE);//Le fait forcer en marche arriere
-		state = WAIT_END_OF_MOVE;
-		break;
-	case WAIT_END_OF_MOVE:
-		if(STACKS_wait_end_auto_pull(ASSER, &timeout)){
-			state = END;
-		}
-		break;
-	case END:
-		state = try_going_until_break(400,1300,END,DONE,ERROR,FAST,FORWARD,NO_AVOIDANCE);
-		break;
-	case DONE:
+		state = IDLE;
 		return END_OK;
 		break;
 	case ERROR:
