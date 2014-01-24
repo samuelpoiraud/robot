e7a50bd21426e0f3638ef02fd72d8f35b5cf02bf
Alexis Murzeau <amubtdx@outlook.fr>
2014-01-24 16:26:33 +0000

    Finition selftest.c actionneur + selftest du fruit_mouth

--- 3_Actionneur/CurrentSoftwareSTM32/selftest.c
+++ 3_Actionneur/CurrentSoftwareSTM32/selftest.c
@@ -4,25 +4,22 @@
 #include "QS/QS_CANmsgList.h"
 #include "QS/QS_can.h"
 #include "clock.h"
-#include "queue.h"
+#include "act_queue_utils.h"
 
 #include "config_debug.h"
 #define LOG_PREFIX "Selftest: "
 #define LOG_COMPONENT OUTPUT_LOG_COMPONENT_SELFTEST
 #include "QS/QS_outputlog.h"
 
-#define MAX_NB_ACT 10
+#define MAX_NB_ACT 8
 #define SELFTEST_TIMEOUT 100 // 10s
 
-typedef struct {
-	Uint11 sid;
-	SELFTEST_state_e state;
-} SELFTEST_data_t;
-
-static SELFTEST_data_t act_test_states[MAX_NB_ACT];
+static SELFTEST_error_code_e failed_act_tests[MAX_NB_ACT];
 static Uint8 expected_act_num = 0;
+static Uint8 act_test_done_num = 0;
 static bool_e test_finished = TRUE;
 
+static void SELFTEST_done_test(Uint11 act_sid, bool_e test_ok);
 static bool_e SELFTEST_check_end(queue_id_t queueId);
 static void SELFTEST_run(queue_id_t queueId, bool_e init);
 
@@ -35,10 +32,10 @@ void SELFTEST_new_selftest(Uint8 nb_actionneurs) {
 	}
 
 	for(i = 0; i < MAX_NB_ACT; i++) {
-		act_test_states[i].state = SELFTEST_UNUSED;
-		act_test_states[i].sid = 0;
+		failed_act_tests[i] = SELFTEST_NOT_DONE;
 	}
 
+	act_test_done_num = 0;
 	expected_act_num = nb_actionneurs;
 	test_finished = FALSE;
 
@@ -48,23 +45,51 @@ void SELFTEST_new_selftest(Uint8 nb_actionneurs) {
 	QUEUE_add(queueId, &SELFTEST_run, (QUEUE_arg_t){0, 0}, 0);
 }
 
-void SELFTEST_set_state(Uint11 act_sid, SELFTEST_state_e test_status) {
+bool_e SELFTEST_finish(queue_id_t queue_id, Uint11 act_sid, Uint8 result, Uint8 error_code, Uint16 param) {
+	if(result == ACT_RESULT_DONE) {
+		SELFTEST_done_test(act_sid, TRUE);
+	}
+
+	SELFTEST_done_test(act_sid, FALSE);
+
+	return ACTQ_finish_SendNothing(queue_id, act_sid, result, error_code, param);
+}
+
+static void SELFTEST_done_test(Uint11 act_sid, bool_e test_ok) {
 	Uint8 i;
-	assert(test_ok == SELFTEST_UNUSED);
 
 	if(test_finished) {
-		error_printf("SELFTEST_set_state appelé sans selftest en cours !, sid = 0x%x, test state = %d\n", act_sid, test_status);
+		error_printf("SELFTEST_done_test appelé sans selftest en cours !, sid = 0x%x, test state = %d\n", act_sid, test_ok);
 		return;
 	}
 
-	debug_printf("Test de l\'actionneur sid 0x%x terminé\n", act_sid);
+	info_printf("Test de l\'actionneur sid 0x%x terminé avec l\'état %s\n", act_sid, test_ok ? "SELFTEST_STATE_OK" : "SELFTEST_STATE_ERROR");
 
-	for(i = 0; i < MAX_NB_ACT; i++) {
-		if(act_test_states[i].state == SELFTEST_UNUSED) {
-			act_test_states[i].state = test_status;
-			act_test_states[i].sid = act_sid;
+	if(test_ok == FALSE) {
+		for(i = 0; i < MAX_NB_ACT; i++) {
+			if(failed_act_tests[i] == SELFTEST_NOT_DONE) {
+				switch(act_sid) {
+					case ACT_FRUIT_MOUTH:
+						failed_act_tests[i] = SELFTEST_ACT_FRUIT_MOUTH;
+						break;
+
+					case ACT_LANCELAUNCHER:
+						failed_act_tests[i] = SELFTEST_ACT_LANCELAUNCHER;
+						break;
+
+					case ACT_ARM:
+						failed_act_tests[i] = SELFTEST_ACT_ARM;
+						break;
+
+					default:
+						warn_printf("L\'actionneur sid 0x%x n\'a pas d\'enum SELFTEST_ACT_xxx associé, utilisation de SELFTEST_ACT_UNKNOWN_ACT\n", act_sid);
+						failed_act_tests[i] = SELFTEST_ACT_UNKNOWN_ACT;
+						break;
+				}
+			}
 		}
 	}
+	act_test_done_num++;
 }
 
 static void SELFTEST_run(queue_id_t queueId, bool_e init) {
@@ -76,14 +101,9 @@ static void SELFTEST_run(queue_id_t queueId, bool_e init) {
 
 //Verifie si tous les actionneurs on répondu
 static bool_e SELFTEST_check_end(queue_id_t queueId) {
-	Uint8 i, nb_act;
+	Uint8 i;
 
-	for(nb_act = i = 0; i < MAX_NB_ACT; i++) {
-		if(act_test_states[i].state == SELFTEST_OK || act_test_states[i].state == SELFTEST_ERROR)
-			nb_act++;
-	}
-
-	if(nb_act == expected_act_num || CLOCK_get_time() >= QUEUE_get_initial_time(queueId) + SELFTEST_TIMEOUT) {
+	if(act_test_done_num == expected_act_num || CLOCK_get_time() >= QUEUE_get_initial_time(queueId) + SELFTEST_TIMEOUT) {
 		if(CLOCK_get_time() >= QUEUE_get_initial_time(queueId) + SELFTEST_TIMEOUT) {
 			warn_printf("Timeout du selftest\n");
 		}
@@ -93,41 +113,21 @@ static bool_e SELFTEST_check_end(queue_id_t queueId) {
 		msg.sid = STRAT_ACT_SELFTEST_DONE;
 		msg.size = 0;
 
-		for(i = 0; i < MAX_NB_ACT; i++) {
-			if(act_test_states[i].state == SELFTEST_ERROR || act_test_states[i].state == SELFTEST_IN_PROGRESS) {
-				//si on trouve un actionneur qui n'a pas terminé son test alors qu'on a déjà assez d'actionneur qui ont terminé le test, alors le in_progress est en trop
-				if(act_test_states[i].state == SELFTEST_IN_PROGRESS) {
-					warn_printf("Test non prévu en in progress pour l\'actionneur de sid 0x%x\n", act_test_states[i].sid);
-				}
-				if(msg.size < 8) {
-					switch(act_test_states[i].sid) <%
-						case ACT_FRUIT_MOUTH:
-							msg.data[msg.size] = SELFTEST_ACT_FRUIT_MOUTH;
-							break;
+		if(act_test_done_num != expected_act_num) {
+			msg.data[msg.size] = SELFTEST_ACT_MISSING_TEST;
+			msg.size++;
+		}
 
-						case ACT_LANCELAUNCHER:
-							msg.data[msg.size] = SELFTEST_ACT_LANCELAUNCHER;
-							break;
-
-						case ACT_ARM:
-							msg.data[msg.size] = SELFTEST_ACT_ARM;
-							break;
-
-						default:
-							msg.data[msg.size] = SELFTEST_ACT_UNKNOWN;
-							break;
-					%>
-
-					msg.size++;
-				}
-			}
+		for(i = 0; i < MAX_NB_ACT && i < msg.size; i++) {
+			msg.data[msg.size] = failed_act_tests[i];
+			msg.size++;
 		}
 #ifdef USE_CAN
 		CAN_send(&msg);
 #endif
 		return TRUE;
-	} else if(nb_act > expected_act_num) {
-		error_printf("Recu plus de réponse que prévu, nb actioneurs = %d, reçu %d réponses\n", expected_act_num, nb_act);
+	} else if(act_test_done_num > expected_act_num) {
+		error_printf("Recu plus de réponse que prévu, nb actioneurs = %d, reçu %d réponses\n", expected_act_num, act_test_done_num);
 		return TRUE;
 	}
 
--- 3_Actionneur/CurrentSoftwareSTM32/selftest.h
+++ 3_Actionneur/CurrentSoftwareSTM32/selftest.h
@@ -13,19 +13,18 @@
 #define SELFTEST_H
 
 #include "../QS/QS_all.h"
+#include "queue.h"
 
 typedef enum {
-	SELFTEST_IN_PROGRESS,
-	SELFTEST_OK,
-	SELFTEST_ERROR
-
 	// Seul selftest.c a le droit d'utiliser cette valeur
 #ifdef SELFTEST_C
-	,SELFTEST_UNUSED
+	SELFTEST_STATE_UNUSED = 0,
 #endif
+	SELFTEST_STATE_OK = 1,
+	SELFTEST_STATE_ERROR = 2
 } SELFTEST_state_e;
 
 void SELFTEST_new_selftest(Uint8 nb_actionneurs);
-void SELFTEST_set_state(Uint11 act_sid, SELFTEST_state_e test_status);
+bool_e SELFTEST_finish(queue_id_t queue_id, Uint11 act_sid, Uint8 result, Uint8 error_code, Uint16 param);
 
 #endif // SELFTEST_H
