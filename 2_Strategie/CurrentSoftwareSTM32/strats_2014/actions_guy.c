d210ebdb1e930ffc1da86f51a43345554dc9dadf
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2014-02-04 08:49:55 +0000

    Ajout stratégie de scan de triangle dans les stratégie de guy

--- 2_Strategie/CurrentSoftwareSTM32/strats_2014/actions_guy.c
+++ 2_Strategie/CurrentSoftwareSTM32/strats_2014/actions_guy.c
@@ -11,6 +11,7 @@
 
 #include "actions_guy.h"
 #include "../QS/QS_outputlog.h"
+#include "../state_machine_helper.h"
 
 
 
@@ -24,3 +25,80 @@
 /* 							Autre strats de test             			 */
 /* ----------------------------------------------------------------------------- */
 
+// Stratégie de test des déctections de triangle
+void Strat_Detection_Triangle(void){
+	// x = 1000
+	// y = 120 led verte / 2880 led rouge
+
+	CREATE_MAE_WITH_VERBOSE(0,
+		INIT,
+		AVANCER,
+		PLACEMENT_FOYER_GAUCHE,
+		SCAN_FOYER_GAUCHE,
+		PLACEMENT_FOYER_MILIEU,
+		SCAN_FOYER_MILIEU,
+		PLACEMENT_FOYER_DROIT,
+		SCAN_FOYER_DROIT,
+		DONE,
+		ERROR
+	);
+
+	CAN_msg_t msg;
+
+	switch (state){
+		case INIT :
+			state = AVANCER;
+			break;
+
+		case AVANCER :
+			state = try_going(1000, 2500, AVANCER, PLACEMENT_FOYER_GAUCHE, PLACEMENT_FOYER_GAUCHE, SLOW, ANY_WAY, NO_AVOIDANCE);
+			break;
+
+		case PLACEMENT_FOYER_GAUCHE :
+			state = try_going(1500, 2500, PLACEMENT_FOYER_GAUCHE, SCAN_FOYER_GAUCHE, SCAN_FOYER_GAUCHE, SLOW, FORWARD, NO_AVOIDANCE);
+			break;
+
+		case SCAN_FOYER_GAUCHE :
+			if(entrance){
+				msg.sid = ASSER_LAUNCH_SCAN_TRIANGLE;
+				msg.size = 0;
+				CAN_send(&msg);
+			}
+
+			state = DONE;
+			break;
+
+		case PLACEMENT_FOYER_MILIEU :
+			state = try_going(1200, 1500, PLACEMENT_FOYER_GAUCHE, SCAN_FOYER_GAUCHE, SCAN_FOYER_GAUCHE, SLOW, FORWARD, NO_AVOIDANCE);
+			break;
+
+		case SCAN_FOYER_MILIEU :
+			if(entrance){
+				msg.sid = ASSER_LAUNCH_SCAN_TRIANGLE;
+				msg.size = 0;
+				CAN_send(&msg);
+			}
+
+			state = DONE;
+			break;
+
+		case PLACEMENT_FOYER_DROIT :
+			state = try_going(1500, 500, PLACEMENT_FOYER_GAUCHE, SCAN_FOYER_GAUCHE, SCAN_FOYER_GAUCHE, SLOW, FORWARD, NO_AVOIDANCE);
+			break;
+
+		case SCAN_FOYER_DROIT :
+			if(entrance){
+				msg.sid = ASSER_LAUNCH_SCAN_TRIANGLE;
+				msg.size = 0;
+				CAN_send(&msg);
+			}
+
+			state = DONE;
+			break;
+
+
+
+		case DONE :
+		break;
+	}
+}
