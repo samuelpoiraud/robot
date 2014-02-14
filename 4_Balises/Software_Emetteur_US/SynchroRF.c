57cb6cf7cbf66e2ce9c11cb74dd8aec045163856
amurzeau <amurzeau@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2014-02-14 02:05:58 +0000

    Ajustement unité base de temps + demande de synchro dans le main pour éviter les problèmes d'IT + maj svn:ignore + ajout fichier cmake

--- 4_Balises/Software_Emetteur_US/SynchroRF.c
+++ 4_Balises/Software_Emetteur_US/SynchroRF.c
@@ -7,19 +7,25 @@
 static CAN_msg_t canmsg_pending;
 static bool_e canmsg_received = FALSE;
 static Sint16 offset;
+static bool_e request_synchro = FALSE;
 
-extern volatile Uint8 step; //dans EmmissionIR.c
+extern volatile Uint8 step; //dans EmissionIR.c
 
 static void rf_packet_received_callback(bool_e for_me, RF_header_t header, Uint8 *data, Uint8 size);
 static void rf_can_received_callback(CAN_msg_t *msg);
 
 void SYNCRF_init() {
-	//RF_init(RF_MODULE, &rf_packet_received_callback, &rf_can_received_callback);
+	RF_init(RF_MODULE, &rf_packet_received_callback, &rf_can_received_callback);
 }
 
 void SYNCRF_process_main()
 {
-	/*static Uint8 compteur_last;
+	static Uint8 compteur_last;
+	if(request_synchro) {
+		request_synchro = FALSE;
+		RF_synchro_request(RF_BROADCAST);
+	}
+
 	if(canmsg_received) {
 		//Traitement message CAN
 		canmsg_received = FALSE;
@@ -34,19 +40,24 @@ void SYNCRF_process_main()
 		compteur_last = step/20;
 
 		debug_printf("Compteur: %u\n", step);
-	}*/
+	}
 }
 
 void SYNCRF_sendRequest() {
-	//RF_synchro_request(RF_BROADCAST);
+	//synchro dans le main pour éviter les trucs bizarre avec le buffering de l'uart dans QS_rf
+	//condition: boucle main rapide (à priori rien de lent, et on synchronise à une précision de 2ms)
+	request_synchro = TRUE;
 	LED_RUN = !LED_RUN;
 }
 
 static void rf_packet_received_callback(bool_e for_me, RF_header_t header, Uint8 *data, Uint8 size) {
 	size = size;
 	if(for_me && header.type == RF_PT_SynchroResponse) {
-		Sint16 fullOffset = data[0] | data[1] << 8;
 		offset = step - TIME_WHEN_SYNCHRO;
+
+		//unités: tick / (us/localtick / us/tick) = tick * us/tick / us/localtick = us/(us/localtick) = localtick
+		Sint16 fullOffset = (data[0] | data[1] << 8) / (LOCAL_TIMEBASE_UNIT / TIME_BASE_UNIT);
+
 		step = step + fullOffset - (offset >> 1);
 	}
 }
