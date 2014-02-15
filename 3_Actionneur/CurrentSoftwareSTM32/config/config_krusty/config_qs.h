f4501f92016700aeb2978a7aa9f31b3ed9f6e548
Alexis Murzeau <amubtdx@outlook.fr>
2014-02-15 20:52:13 +0000

    Maj des uarts des AX12/RX24 suivant pin_usages.xls

--- 3_Actionneur/CurrentSoftwareSTM32/config/config_krusty/config_qs.h
+++ 3_Actionneur/CurrentSoftwareSTM32/config/config_krusty/config_qs.h
@@ -57,8 +57,8 @@
 	#define AX12_TIMER_ID 2
 	#define AX12_STATUS_RETURN_MODE AX12_STATUS_RETURN_ALWAYS	//Permet de savoir quand l'AX12 n'est pas bien connecté ou ne répond pas.
 	#define AX12_STATUS_RETURN_CHECK_CHECKSUM
-	#define AX12_UART_ID 2
-	//#define AX12_RX24_UART_ID 3
+	#define AX12_UART_ID 3
+	#define AX12_RX24_UART_ID 2
 	//#define AX12_DEBUG_PACKETS
 
 	#define USE_WATCHDOG
