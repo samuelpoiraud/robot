71e1a55b819029cd45870ac6026a90723d27c336
Alexis Murzeau <amubtdx@outlook.fr>
2014-02-15 17:13:29 +0000

    Assert de l'uart id dans l'init de UART_IMPL

--- DSPIC30F/QS/impl/QS_uart_dspic30f.c
+++ DSPIC30F/QS/impl/QS_uart_dspic30f.c
@@ -45,8 +45,7 @@ bool_e UART_IMPL_init(Uint8 uart_id, Uint32 baudrate, Sint8 rx_irq_priority, Sin
 }
 
 bool_e UART_IMPL_init_ex(Uint8 uart_id, Uint32 baudrate, Sint8 rx_irq_priority, Sint8 tx_irq_priority, UART_IMPL_stop_bits_e stop_bits, UART_IMPL_parity_e parity_mode) {
-	if(uart_id >= UART_IMPL_NUM)
-		return FALSE;
+	assert(uart_id < UART_IMPL_NUM);
 
 	Sint16 config1;
 	Sint16 config2;
