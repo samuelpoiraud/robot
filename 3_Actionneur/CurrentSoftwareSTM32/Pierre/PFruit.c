4b593f03ac241fab38fa87face0ffff7710de800
agarry <agarry@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2014-02-19 20:16:47 +0000

    Ajout de l'ax12 fruit_labium qui va contrôler la trappe à fruit

--- 3_Actionneur/CurrentSoftwareSTM32/Pierre/PFruit.c
+++ 3_Actionneur/CurrentSoftwareSTM32/Pierre/PFruit.c
@@ -46,6 +46,8 @@ void FRUIT_init() {
 //Initialise l'AX12 de la pince s'il n'était pas allimenté lors d'initialisations précédentes, si déjà initialisé, ne fait rien
 static void FRUIT_initAX12() {
 	static bool_e ax12_is_initialized = FALSE;
+	static bool_e ax12_is_initialized2 = FALSE;
+
 	if(ax12_is_initialized == FALSE && AX12_is_ready(FRUIT_MOUTH_AX12_ID) == TRUE) {
 		ax12_is_initialized = TRUE;
 		AX12_config_set_highest_voltage(FRUIT_MOUTH_AX12_ID, 136);
@@ -60,6 +62,22 @@ static void FRUIT_initAX12() {
 
 		AX12_set_position(FRUIT_MOUTH_AX12_ID, FRUIT_AX12_INIT_POS);
 		info_printf("FRUIT_MOUTH AX12 initialisé\n");
+	}
+
+	if(ax12_is_initialized2 == FALSE && AX12_is_ready(FRUIT_LABIUM_AX12_ID) == TRUE) {
+		ax12_is_initialized2 = TRUE;
+		AX12_config_set_highest_voltage(FRUIT_LABIUM_AX12_ID, 136);
+		AX12_config_set_lowest_voltage(FRUIT_LABIUM_AX12_ID, 70);
+		AX12_config_set_maximum_torque_percentage(FRUIT_LABIUM_AX12_ID, FRUIT_AX12_MAX_TORQUE_PERCENT);
+
+		AX12_config_set_maximal_angle(FRUIT_LABIUM_AX12_ID, 300);
+		AX12_config_set_minimal_angle(FRUIT_LABIUM_AX12_ID, 0);
+
+		AX12_config_set_error_before_led(FRUIT_LABIUM_AX12_ID, AX12_ERROR_ANGLE | AX12_ERROR_CHECKSUM | AX12_ERROR_INSTRUCTION | AX12_ERROR_OVERHEATING | AX12_ERROR_OVERLOAD | AX12_ERROR_RANGE);
+		AX12_config_set_error_before_shutdown(FRUIT_LABIUM_AX12_ID, AX12_ERROR_OVERHEATING); //On ne met pas l'overload comme par defaut, il faut pouvoir tenir l'assiette et sans que l'AX12 ne s'arrête de forcer pour cause de couple resistant trop fort.
+
+		AX12_set_position(FRUIT_LABIUM_AX12_ID, FRUIT_AX12_LABIUM_INIT_POS);
+		debug_printf("FRUIT_LABIUM AX12 initialisé\n");
 	}
 }
 
@@ -106,14 +124,38 @@ bool_e FRUIT_CAN_process_msg(CAN_msg_t* msg) {
 					ACTQ_sendResultWithLine(msg->sid, msg->data[0], ACT_RESULT_NOT_HANDLED, ACT_RESULT_ERROR_NO_RESOURCES);
 				}
 				break;
+			case ACT_FRUIT_LABIUM_CLOSE:
+				queueId1 = QUEUE_create();
+				if(queueId1 != QUEUE_CREATE_FAILED){
+					QUEUE_add(queueId1, &QUEUE_take_sem, (QUEUE_arg_t){0, 0, NULL}, QUEUE_ACT_AX12_Fruit);
+					QUEUE_add(queueId1, &FRUIT_run_command, (QUEUE_arg_t){msg->data[0], FRUIT_LABIUM_CS_CloseAX12,  &ACTQ_finish_SendResult}, QUEUE_ACT_AX12_Fruit);
+					QUEUE_add(queueId1, &QUEUE_give_sem, (QUEUE_arg_t){0, 0, NULL}, QUEUE_ACT_AX12_Fruit);
+				} else {
+					QUEUE_flush(queueId1);
+					ACTQ_sendResultWithLine(msg->sid, msg->data[0], ACT_RESULT_NOT_HANDLED, ACT_RESULT_ERROR_NO_RESOURCES);
+				}
+				break;
+			case ACT_FRUIT_LABIUM_OPEN:
+				queueId1 = QUEUE_create();
+				if(queueId1 != QUEUE_CREATE_FAILED){
+					QUEUE_add(queueId1, &QUEUE_take_sem, (QUEUE_arg_t){0, 0, NULL}, QUEUE_ACT_AX12_Fruit);
+					QUEUE_add(queueId1, &FRUIT_run_command, (QUEUE_arg_t){msg->data[0], FRUIT_LABIUM_CS_OpenAX12,  &ACTQ_finish_SendResult}, QUEUE_ACT_AX12_Fruit);
+					QUEUE_add(queueId1, &QUEUE_give_sem, (QUEUE_arg_t){0, 0, NULL}, QUEUE_ACT_AX12_Fruit);
+				} else {
+					QUEUE_flush(queueId1);
+					ACTQ_sendResultWithLine(msg->sid, msg->data[0], ACT_RESULT_NOT_HANDLED, ACT_RESULT_ERROR_NO_RESOURCES);
+				}
+				break;
 			default:
 				component_printf(LOG_LEVEL_Warning, "invalid CAN msg data[0]=%u !\n", msg->data[0]);
 		}
 		return TRUE;
-	} else if(msg->sid == ACT_DO_SELFTEST) {
-		SELFTEST_set_actions(&FRUIT_run_command, 2, (SELFTEST_action_t[]){
+	}else if(msg->sid == ACT_DO_SELFTEST) {
+		SELFTEST_set_actions(&FRUIT_run_command, 4, (SELFTEST_action_t[]){
 								 {ACT_FRUIT_MOUTH_OPEN,  FRUIT_CS_OpenAX12,  QUEUE_ACT_AX12_Fruit},
-								 {ACT_FRUIT_MOUTH_CLOSE, FRUIT_CS_CloseAX12, QUEUE_ACT_AX12_Fruit}
+								 {ACT_FRUIT_MOUTH_CLOSE, FRUIT_CS_CloseAX12, QUEUE_ACT_AX12_Fruit},
+								 {ACT_FRUIT_LABIUM_OPEN,  FRUIT_LABIUM_CS_OpenAX12,  QUEUE_ACT_AX12_Fruit},
+								 {ACT_FRUIT_LABIUM_CLOSE, FRUIT_LABIUM_CS_CloseAX12, QUEUE_ACT_AX12_Fruit}
 							 });
 	}
 
@@ -127,12 +169,13 @@ void FRUIT_run_command(queue_id_t queueId, bool_e init) {
 		return;
 	}
 
-if(QUEUE_get_act(queueId) == QUEUE_ACT_AX12_Fruit) {    // Gestion des mouvements du fruit
-		if(init)
-			FRUIT_command_init(queueId);
-		else
-			FRUIT_command_run(queueId);
+	if(QUEUE_get_act(queueId) == QUEUE_ACT_AX12_Fruit) {    // Gestion des mouvements du fruit
+			if(init)
+				FRUIT_command_init(queueId);
+			else
+				FRUIT_command_run(queueId);
 	}
+
 }
 
 
@@ -143,12 +186,21 @@ static void FRUIT_command_init(queue_id_t queueId) {
 
 	*ax12_goalPosition = 0xFFFF;
 
+	bool_e forFruitMouth = FALSE;
+
 	switch(command) {
-		case ACT_FRUIT_MOUTH_OPEN:  *ax12_goalPosition = FRUIT_AX12_OPEN_POS; break;
-		case ACT_FRUIT_MOUTH_CLOSE: *ax12_goalPosition = FRUIT_AX12_CLOSED_POS; break;
-		case ACT_FRUIT_MOUTH_MID: *ax12_goalPosition = FRUIT_AX12_MID_POS; break;
+		case ACT_FRUIT_MOUTH_OPEN:  *ax12_goalPosition = FRUIT_AX12_OPEN_POS; forFruitMouth = TRUE; break;
+		case ACT_FRUIT_MOUTH_CLOSE: *ax12_goalPosition = FRUIT_AX12_CLOSED_POS; forFruitMouth = TRUE; break;
+		case ACT_FRUIT_MOUTH_MID: *ax12_goalPosition = FRUIT_AX12_MID_POS; forFruitMouth = TRUE; break;
 		case ACT_FRUIT_MOUTH_STOP:
 			AX12_set_torque_enabled(FRUIT_MOUTH_AX12_ID, FALSE); //Stopper l'asservissement de l'AX12 qui gère la pince
+			QUEUE_next(queueId, ACT_FRUIT_MOUTH, ACT_RESULT_DONE, ACT_RESULT_ERROR_OK, __LINE__);
+			return;
+
+		case ACT_FRUIT_LABIUM_OPEN: *ax12_goalPosition = FRUIT_AX12_LABIUM_OPEN_POS; forFruitMouth = FALSE; break;
+		case ACT_FRUIT_LABIUM_CLOSE: *ax12_goalPosition = FRUIT_AX12_LABIUM_CLOSE_POS; forFruitMouth = FALSE; break;
+		case ACT_FRUIT_LABIUM_STOP:
+			AX12_set_torque_enabled(FRUIT_LABIUM_AX12_ID, FALSE);
 			QUEUE_next(queueId, ACT_FRUIT_MOUTH, ACT_RESULT_DONE, ACT_RESULT_ERROR_OK, __LINE__);
 			return;
 
@@ -158,18 +210,31 @@ static void FRUIT_command_init(queue_id_t queueId) {
 				return;
 			}
 	}
-	if(*ax12_goalPosition == 0xFFFF) {
+	if(*ax12_goalPosition == 0xFFFF && forFruitMouth == TRUE) {//Cas pour l ax12 Mouth
+		error_printf("Invalid plier position: %u, code is broken !\n", command);
+		QUEUE_next(queueId, ACT_FRUIT_MOUTH, ACT_RESULT_NOT_HANDLED, ACT_RESULT_ERROR_LOGIC, __LINE__);
+		return;
+	}else if(*ax12_goalPosition == 0xFFFF){// Pour l'ax12 Labium
 		error_printf("Invalid plier position: %u, code is broken !\n", command);
 		QUEUE_next(queueId, ACT_FRUIT_MOUTH, ACT_RESULT_NOT_HANDLED, ACT_RESULT_ERROR_LOGIC, __LINE__);
 		return;
 	}
 
 	debug_printf("Move fruit ax12 to %d\n", *ax12_goalPosition);
-	AX12_reset_last_error(FRUIT_MOUTH_AX12_ID); //Sécurité anti terroriste. Nous les parano on aime pas voir des erreurs là ou il n'y en a pas.
-	if(!AX12_set_position(FRUIT_MOUTH_AX12_ID, *ax12_goalPosition)) {	//Si la commande n'a pas été envoyée correctement et/ou que l'AX12 ne répond pas a cet envoi, on l'indique à la carte stratégie
-		error_printf("AX12_set_position error: 0x%x\n", AX12_get_last_error(FRUIT_MOUTH_AX12_ID).error);
-		QUEUE_next(queueId, ACT_FRUIT_MOUTH, ACT_RESULT_FAILED, ACT_RESULT_ERROR_NOT_HERE, __LINE__);
-		return;
+
+	if(forFruitMouth == TRUE){
+		AX12_reset_last_error(FRUIT_MOUTH_AX12_ID); //Sécurité anti terroriste. Nous les parano on aime pas voir des erreurs là ou il n'y en a pas.
+		if(!AX12_set_position(FRUIT_MOUTH_AX12_ID, *ax12_goalPosition)) {	//Si la commande n'a pas été envoyée correctement et/ou que l'AX12 ne répond pas a cet envoi, on l'indique à la carte stratégie
+			error_printf("AX12_set_position error: 0x%x\n", AX12_get_last_error(FRUIT_MOUTH_AX12_ID).error);
+			QUEUE_next(queueId, ACT_FRUIT_MOUTH, ACT_RESULT_FAILED, ACT_RESULT_ERROR_NOT_HERE, __LINE__);
+			return;
+		}
+	}else{
+		AX12_reset_last_error(FRUIT_LABIUM_AX12_ID);
+		if(!AX12_set_position(FRUIT_LABIUM_AX12_ID, *ax12_goalPosition)){
+			error_printf("AX12_set_position error: 0x%x\n", AX12_get_last_error(FRUIT_MOUTH_AX12_ID).error);
+			QUEUE_next(queueId, ACT_FRUIT_MOUTH, ACT_RESULT_FAILED, ACT_RESULT_ERROR_NOT_HERE, __LINE__);
+		}
 	}
 	//La commande a été envoyée et l'AX12 l'a bien reçu
 }
@@ -179,9 +244,16 @@ static void FRUIT_command_run(queue_id_t queueId) {
 	Uint8 result, errorCode;
 	Uint16 line;
 
-	//360° of large_epsilon, quand on a un timeout, on a forcément un resultat Ok (et pas d'erreur, on considère qu'on serre l'assiette)
-	if(ACTQ_check_status_ax12(queueId, FRUIT_MOUTH_AX12_ID, QUEUE_get_arg(queueId)->param, FRUIT_AX12_ASSER_POS_EPSILON, FRUIT_AX12_ASSER_TIMEOUT, 360, &result, &errorCode, &line))
-		QUEUE_next(queueId, ACT_FRUIT_MOUTH, result, errorCode, line);
+	//360° of large_epsilon, quand on a un timeout, on a forcément un resultat Ok
+	Uint8 command = QUEUE_get_arg(queueId)->canCommand;
+
+	if(command == ACT_FRUIT_LABIUM_OPEN || command == ACT_FRUIT_LABIUM_CLOSE || command == ACT_FRUIT_LABIUM_STOP){
+		if(ACTQ_check_status_ax12(queueId, FRUIT_LABIUM_AX12_ID, QUEUE_get_arg(queueId)->param, FRUIT_AX12_ASSER_POS_EPSILON, FRUIT_AX12_ASSER_TIMEOUT, 360, &result, &errorCode, &line))
+			QUEUE_next(queueId, ACT_FRUIT_MOUTH, result, errorCode, line);
+	}else{
+		if(ACTQ_check_status_ax12(queueId, FRUIT_MOUTH_AX12_ID, QUEUE_get_arg(queueId)->param, FRUIT_AX12_ASSER_POS_EPSILON, FRUIT_AX12_ASSER_TIMEOUT, 360, &result, &errorCode, &line))
+			QUEUE_next(queueId, ACT_FRUIT_MOUTH, result, errorCode, line);
+	}
 }
 
 
