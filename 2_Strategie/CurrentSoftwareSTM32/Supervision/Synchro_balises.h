e8b7543df6930f51d610fe2f26ea180aec2d0138
amurzeau <amurzeau@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2014-02-22 01:26:01 +0000

    Strat: LED_BEACON: RED s'allume si le robot ne reçoit rien d'un des 3 autres modules (2 balises + l'autre robot) pendant 10sec GREEN clignote avec la réception de la synchro via rf dans le cas de GUY

--- 2_Strategie/CurrentSoftwareSTM32/Supervision/Synchro_balises.h
+++ 2_Strategie/CurrentSoftwareSTM32/Supervision/Synchro_balises.h
@@ -2,9 +2,10 @@
 #define SYNCHRO_BALISES_H
 
 #include "../QS/QS_all.h"
+#include "../QS/QS_rf.h"
 
 //Les balises sont elles là ? Informatif, si elles sont là, implique que la RF fonctionne en reception pour ce robot, et en emission pour les balises
-extern bool_e balise_here[2];
+extern bool_e rfmodule_here[RF_MODULE_COUNT];
 
 void SYNCHRO_init();
 void SYNCHRO_process_main();
