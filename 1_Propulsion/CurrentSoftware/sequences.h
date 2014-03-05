7126140b5dcd1d0b5205bc42eeb98010ec48e436
agarry <agarry@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2014-03-05 16:11:34 +0000

    Nouvelle position d'initialisation et calibration ok pour les deux robots

--- 1_Propulsion/CurrentSoftware/sequences.h
+++ 1_Propulsion/CurrentSoftware/sequences.h
@@ -7,7 +7,7 @@
  *  Auteur : Nirgal (2009)
  *  Version 201203
  */
- 
+
 #ifndef _SEQUENCES_H
 	#define _SEQUENCES_H
 #include "QS/QS_all.h"
@@ -15,15 +15,15 @@
 #include "roadmap.h"
 
 	void SEQUENCES_border_calibration(Sint16 angle, way_e direction, acknowledge_e acknowledge, Sint32 far_point_x, Sint32 far_point_y);
-	
+
 	void SEQUENCES_border_calibration_update_position(void);
 
 	void SEQUENCES_rush_in_the_wall(Sint16 angle, way_e way, acknowledge_e acquittement, Sint32 consigne_point_eloigne_x, Sint32 consigne_point_eloigne_y, border_mode_e border_mode, corrector_e corrector);
 
-    void SEQUENCES_calibrate(way_e way, calibration_square_e calibration_square);
+	void SEQUENCES_calibrate();
 
-    void SEQUENCES_selftest(void);
+	void SEQUENCES_selftest(void);
 
-    void SEQUENCES_trajectory_for_test_coefs(void);
+	void SEQUENCES_trajectory_for_test_coefs(void);
 
 #endif /*def _SEQUENCES_H*/
