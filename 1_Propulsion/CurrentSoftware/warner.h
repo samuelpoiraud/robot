3b739cfeeb052e0f8b258b8eec5c342a49814685
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2014-03-08 11:02:57 +0000

    Correction warning

--- 1_Propulsion/CurrentSoftware/warner.h
+++ 1_Propulsion/CurrentSoftware/warner.h
@@ -3,37 +3,37 @@
  *
  *  Fichier : warner.h
  *  Package : Propulsion
- *  Description : Le "warner" avertit de la position actuelle 
- *					une carte qui l'a demandé, lorsque que l'on est proche d'un angle où d'une position, 
+ *  Description : Le "warner" avertit de la position actuelle
+ *					une carte qui l'a demandé, lorsque que l'on est proche d'un angle où d'une position,
  *  				ou qu'un délai périodique est écoulé.
  *  Auteur : Nirgal (2009)
  *  Version 201203
  */
- 
+
 
 #ifndef WARNER_H
 	#define WARNER_H
-	
+
 	#include "QS/QS_all.h"
 	#include "supervisor.h"
 
 	//Cette fonction DOIT être appelée à l'initialisation.
 	void WARNER_init(void);
-	
+
 	void WARNER_process_main(void);
 	/////////////////////////////////////////////////////////////////////////////
 
 	//Ces fonctions sont appelées pour activer l'avertisseur, aux positions indiquées...
 	void WARNER_arm_teta(Sint16 teta);
-	
+
 	void WARNER_arm_x(Sint16 x);			//Position [mm]
-	
+
 	void WARNER_arm_y(Sint16 y);			//Position [mm]
-	
+
 	void WARNER_arm_timer(Uint16 duree); 	//Duree en [ms]
 
 	void WARNER_arm_translation(Uint32 translation);	//distance en mm
-	
+
 	void WARNER_arm_rotation(Sint16 rotation);	//en rad4096
 
 	void WARNER_brake(bool_e last_order);
@@ -41,7 +41,7 @@
 
 	/////////////////////////////////////////////////////////////////////////////
 	//cette fonction doit être appelée en IT
-	//elle est le coeur de l'avertisseur, et lève les flags nécessaires	
+	//elle est le coeur de l'avertisseur, et lève les flags nécessaires
 	void WARNER_process_it(void);
 
 
@@ -58,8 +58,9 @@
 
 	//cette fonction sert à avertir, en envoyant des messages CAN d'asser position si nécessaire
 	WARNER_state_t WARNER_get_warning_state(void);
-	
+
 	void WARNER_inform(WARNER_state_t new_warnings, SUPERVISOR_error_source_e new_error_source);
+	void WARNER_enable_counter_trajectory_for_test_coefs_finished(void);
 
 
 #endif //ndef WARNER_H
