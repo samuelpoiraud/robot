f243b1d5e420fa43ac4ca43ea4b9387f888ab5ed
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2014-03-09 22:31:29 +0000

    Ajout de fonctionnalité à la stratégie principale de Pierre et quelques légères corrections

--- 2_Strategie/CurrentSoftwareSTM32/strats_2014/strat_pierre.h
+++ 2_Strategie/CurrentSoftwareSTM32/strats_2014/strat_pierre.h
@@ -19,9 +19,13 @@
 #include "../act_functions.h"
 #include "../QS/QS_types.h"
 #include "../config/config_pin.h"
+#include "../state_machine_helper.h"
 
-typedef enum{
-	SUB_INIT = 0,
+// Cette variable est mise à TRUE quand une sub action prioritaire urgente doit être faite
+bool_e stop_request;
+
+TYPEDEF_ENUM_WITH_STRING(subaction_id_e, subaction_name,
+	SUB_INIT,
 	SUB_GETOUT,
 	SUB_LANCE,
 	SUB_LANCE_ADV,
@@ -37,14 +41,15 @@ typedef enum{
 	// Autre états
 	GET_OUT_IF_NO_CALIBRATION,
 	TURN_IF_NO_CALIBRATION
-}subaction_id_e;
+);
 
-void set_sub_act(subaction_id_e sub_action, Uint8 priority, bool_e enable, time32_t t_begin, time32_t t_end, char * chaine);
+void set_sub_act(subaction_id_e sub_action, Uint8 priority, bool_e enable, time32_t t_begin, time32_t t_end, bool_e ask_stop_request, char * chaine);
 void set_sub_act_priority(subaction_id_e sub_action, Uint8 priority);
 void set_sub_act_enable(subaction_id_e sub_action, bool_e enable);
 void set_sub_act_t_begin(subaction_id_e sub_action, time32_t t_begin);
 void set_sub_act_t_end(subaction_id_e sub_action, time32_t t_end);
 void set_sub_act_chaine(subaction_id_e sub_action, char* chaine);
+void set_sub_act_ask_stop_request(subaction_id_e sub_action, bool_e ask_stop_request);
 void inc_sub_act_priority(subaction_id_e sub_action);
 void dec_sub_act_priority(subaction_id_e sub_action);
 void inc_sub_act_t_begin(subaction_id_e sub_action);
@@ -57,6 +62,7 @@ bool_e get_sub_act_enable(subaction_id_e sub_action);
 Uint32 get_sub_act_t_begin(subaction_id_e sub_action);
 Uint32 get_sub_act_t_end(subaction_id_e sub_action);
 char * get_sub_act_chaine(subaction_id_e sub_action);
+bool_e get_sub_act_ask_stop_request(subaction_id_e sub_action);
 
 
 #endif /* STRAT_PIERRE_H_ */
