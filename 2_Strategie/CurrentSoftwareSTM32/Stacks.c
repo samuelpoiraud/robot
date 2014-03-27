a5635e2ee59ac62c19535328e349c48131685c14
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2014-03-27 23:22:25 +0000

    Remise du BUZZER_play lors d'un timeout du Stacks

--- 2_Strategie/CurrentSoftwareSTM32/Stacks.c
+++ 2_Strategie/CurrentSoftwareSTM32/Stacks.c
@@ -12,6 +12,7 @@
 #define STACKS_C
 #include "Stacks.h"
 #include "QS/QS_outputlog.h"
+#include "Supervision/Buzzer.h"
 
 typedef struct
 {
@@ -136,6 +137,7 @@ bool_e STACKS_wait_end_auto_pull (stack_id_e stack_id, bool_e* got_timeout)
 		#ifdef VERBOSE_MODE
 			debug_printf("TIMEOUT (stack_id : %d)\n", stack_id);
 		#endif
+		BUZZER_play(5,NOTE_LA,10);
 		*got_timeout=TRUE;	//En fait, il y a un timeout.
 		if(STACKS_get_action(stack_id,STACKS_get_top(stack_id))!=&wait_forever)
 			STACKS_pull(stack_id);
