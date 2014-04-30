a910a6761a8f3ed9b3f1dfe3c8fb04cfe80f902c
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2014-04-30 18:10:21 +0000

    Correction goto_pos_curve_with_avoidance évitement

--- 2_Strategie/CurrentSoftwareSTM32/avoidance.c
+++ 2_Strategie/CurrentSoftwareSTM32/avoidance.c
@@ -512,7 +512,7 @@ error_e goto_pos_curve_with_avoidance(const displacement_t displacements[], cons
 	switch(state)
 	{
 		case CHECK_SCAN_FOE :
-			if(foe_in_zone(TRUE, displacements[0].point.x, displacements[0].point.y)){
+			if(avoidance_type != NO_AVOIDANCE && foe_in_zone(TRUE, displacements[0].point.x, displacements[0].point.y)){
 				avoidance_printf("goto_pos_with_scan_foe NOT HANDLED because foe in target zone");
 				state = CHECK_SCAN_FOE;
 				return NOT_HANDLED;
