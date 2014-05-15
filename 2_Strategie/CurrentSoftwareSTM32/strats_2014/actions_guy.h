7564d26b2144008816bbae52be8d66a92aad0b8c
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2014-05-15 22:44:40 +0000

    Gestion d'erreur complete lors d'un dépilage de torche non testé

--- 2_Strategie/CurrentSoftwareSTM32/strats_2014/actions_guy.h
+++ 2_Strategie/CurrentSoftwareSTM32/strats_2014/actions_guy.h
@@ -51,12 +51,8 @@ typedef struct{
 
 
 error_e sub_action_initiale_guy();
-error_e ACT_arm_move(ARM_state_e state_arm, Sint16 x, Sint16 y, Uint8 in_progress, Uint8 success_state, Uint8 fail_state);
 error_e ACT_arm_deploy_torche(torch_choice_e choiceTorch, torch_dispose_zone_e filed);
 error_e do_torch(torch_choice_e torch_choice, torch_dispose_zone_e dispose_zone, torch_dispose_zone_e if_fail_dispose_zone);
-error_e ACT_small_arm_move(Uint8 state_arm, Uint8 in_progress, Uint8 success_state, Uint8 fail_state);
-error_e ACT_elevator_arm_move(Uint8 state_arm, Uint8 in_progress, Uint8 success_state, Uint8 fail_state);
-
 /* Fait tomber le premier triangle par une rotation,
  * puis prend le 2éme et l'emmène soit sur le foyer
  * ou bien au pied d'un arbre
