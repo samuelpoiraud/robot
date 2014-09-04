01b01a7ed2a8707211e57a687e38446bef01069c
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2014-09-04 16:46:57 +0000

    Ajout config ARM (pour test) Ajout QS_measure

---
+++ STM32/QS/QS_measure.h
@@ -0,0 +1,15 @@
+/*
+ *  Club Robot ESEO 2014-2015
+ *
+ *  Package : Qualité Soft
+ *  Description : Ensemble des mesures des robots et du terrain
+ *  Auteur : aguilmet
+ */
+
+#ifndef QS_MEASURE_H
+#define QS_MEASURE_H
+
+#define LARGEUR_ROBOT 246
+#define LARGEUR_LABIUM	235
+
+#endif // QS_MEASURE_H
