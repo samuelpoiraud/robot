1aaee2ddfe77421dd6133dd51289653a1777b4e2
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2014-09-23 19:29:13 +0000

    Début évitement en deux temps

--- STM32/QS/QS_measure.h
+++ STM32/QS/QS_measure.h
@@ -12,15 +12,35 @@
 
 #define SMALL_ROBOT_WIDTH				200		//Largeur du petit robot [mm]
 #define BIG_ROBOT_WIDTH					300		//Largeur du gros robot [mm]
-#define	FOE_SIZE						400		//taille supposée de l'adversaire
-#define MARGE_COULOIR_EVITEMENT_STATIC_BIG_ROBOT	(300 + 100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Variable commune à la stratégie et la propulsion pour la gestion de l'évitement
+
+#define	FOE_SIZE										400		//taille supposée de l'adversaire
+#define MARGE_COULOIR_EVITEMENT_STATIC_BIG_ROBOT		(300 + 100)
 #define MARGE_COULOIR_EVITEMENT_STATIC_SMALL_ROBOT		(240 + 100)
-#define DISTANCE_EVITEMENT_STATIC		500
+#define DISTANCE_EVITEMENT_STATIC						500
 
-#define SMALL_ROBOT_ACCELERATION_NORMAL	468*2	//Réglage d'accélération de la propulsion : 625 	mm/sec = 64 	[mm/4096/5ms/5ms]
-#define BIG_ROBOT_ACCELERATION_NORMAL	937*2	//Réglage d'accélération de la propulsion : 1094 	mm/sec = 112 	[mm/4096/5ms/5ms]
-#define SMALL_ROBOT_RESPECT_DIST_MIN	400		//Distance à laquelle on se tient d'un adversaire [mm]
-#define BIG_ROBOT_RESPECT_DIST_MIN		700		//Distance à laquelle on se tient d'un adversaire [mm]
-
+#define SMALL_ROBOT_ACCELERATION_NORMAL					468*2	//Réglage d'accélération de la propulsion : 625 	mm/sec = 64 	[mm/4096/5ms/5ms]
+#define BIG_ROBOT_ACCELERATION_NORMAL					937*2	//Réglage d'accélération de la propulsion : 1094 	mm/sec = 112 	[mm/4096/5ms/5ms]
+#define SMALL_ROBOT_RESPECT_DIST_MIN					400		//Distance à laquelle on se tient d'un adversaire [mm]
+#define BIG_ROBOT_RESPECT_DIST_MIN						550		//Distance à laquelle on se tient d'un adversaire [mm]
+#define SMALL_ROBOT_DIST_MIN_SPEED_SLOW					550		//Distance à laquelle on ralentie s'il ya un adversaire [mm]
+#define BIG_ROBOT_DIST_MIN_SPEED_SLOW					750		//Distance à laquelle on ralentie s'il ya un adversaire [mm]
 
 #endif /* ndef QS_MEASURE_H */
