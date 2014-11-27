79f50ec13f98baba700a67d0ba5a510281bfabd4
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2014-11-27 13:59:57 +0000

    position de l'ax12 CLAP

--- 3_Actionneur/CurrentSoftwareSTM32/Wood/Clap_config.h
+++ 3_Actionneur/CurrentSoftwareSTM32/Wood/Clap_config.h
@@ -21,11 +21,11 @@
 /** Si le déplacement de l'ax12 timeout mais que sa position est à +/- CLAP_AX12_ASSER_POS_LARGE_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré. */
 	#define CLAP_AX12_ASSER_POS_LARGE_EPSILON   10	//en degré
 /** Position en degré de l'ax12 lorsque EXEMPLE est rentré  */
-	#define CLAP_AX12_IDLE_POS            230		//en degré
+	#define CLAP_AX12_IDLE_POS            53		//en degré
 /** Position en degré de l'ax12 lorsque EXEMPLE est mi sortie */
 	#define CLAP_AX12_MID_POS			   180		//en degré
 /** Position en degré de l'ax12 lorsque EXEMPLE est sortie */
-	#define CLAP_AX12_DEPLOYED_POS        156		//en degré
+	#define CLAP_AX12_DEPLOYED_POS        234		//en degré
 /** Tension maximale en pourcentage de l'ax12. Utilisé pour limiter le courant dans le moteur  */
 	#define CLAP_AX12_MAX_TORQUE_PERCENT  100		//A mettre a une valeur correcte pour pincer assez fort sans risquer d'endommager l'AX12.
 /** Position initiale de l'AX12. Doit être un define du type CLAP_AX12_* */
