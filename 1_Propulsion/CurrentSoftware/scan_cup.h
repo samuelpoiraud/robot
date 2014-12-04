9d7c5869852002ab0b3e405a7a59284f6961f497
gberland <gberland@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2014-12-04 13:10:51 +0000

    Ajout code scan_cup

---
+++ 1_Propulsion/CurrentSoftware/scan_cup.h
@@ -0,0 +1,11 @@
+#include "QS/QS_all.h"
+
+#ifdef SCAN_CUP
+
+
+void SCAN_CUP_init(void);
+void SCAN_CUP_process_it();
+void SCAN_CUP_canMsg(CAN_msg_t *msg);
+void SCAN_TRIANGLE_calculate(void);
+
+#endif
