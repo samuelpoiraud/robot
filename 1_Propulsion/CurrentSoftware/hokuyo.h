26590392b42015a2f0035daf0d8e39058ccbad70
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2015-01-01 20:56:25 +0000

    Triangulation suite (adaption au coté vert)

--- 1_Propulsion/CurrentSoftware/hokuyo.h
+++ 1_Propulsion/CurrentSoftware/hokuyo.h
@@ -47,17 +47,12 @@
 	void tri_points();
 	void Hokuyo_validPointsAndBeacons();
 
-	//Fonction de tri des tableaux de mesures (tri à bulles)
-	void tri_tableau(HOKUYO_adversary_position points_beacons[3][NB_MESURES_HOKUYO*20], Uint16 nb_points, Uint8 numero_beacon);
 
 	//Fonction permettant d'éliminer les points bizarres
-	void tri_mesures(HOKUYO_adversary_position points_beacons[3][NB_MESURES_HOKUYO*20], Uint16 nb_points, Uint8 numero_beacon);
-
-	//Fonction prenant la moyenne des points mesurés pour chaque angle donné (ie tout les 0,25°)
-	void moyenne_mesures(HOKUYO_adversary_position points_beacons[3][NB_MESURES_HOKUYO*20], Uint16 nb_points, Uint8 numero_beacon);
+	void tri_mesures(HOKUYO_point_position points_beacons[3][NB_MESURES_HOKUYO*20], Uint16 *nb_points, Uint8 numero_beacon);
 
 	//Fonction réalisant la régression circulaire d'un nuage de points
-	HOKUYO_adversary_position regression_circulaire(HOKUYO_adversary_position points_beacons[3][NB_MESURES_HOKUYO*20], Uint16 nb_points, Uint8 numero_beacon);
+	HOKUYO_adversary_position regression_circulaire(HOKUYO_point_position points_beacons[3][NB_MESURES_HOKUYO*20], Uint16 nb_points, Uint8 numero_beacon);
 
 	//Fonction trouvant le centre des balises
 	void find_beacons_centres();
