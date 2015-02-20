68222362742af47c4cfc32c2bbecd93e9c921dee
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2015-02-20 15:41:26 +0000

    Modification du selftest actionneur en prévision de la gestion de multiple actionneur par act_sid

--- 3_Actionneur/CurrentSoftwareSTM32/Holly/Carpet/carpet_launcher_right.c
+++ 3_Actionneur/CurrentSoftwareSTM32/Holly/Carpet/carpet_launcher_right.c
@@ -130,7 +130,7 @@ bool_e CARPET_LAUNCHER_RIGHT_CAN_process_msg(CAN_msg_t* msg) {
 		// Lister les différents états que l'actionneur doit réaliser pour réussir le selftest
 		SELFTEST_set_actions(&CARPET_LAUNCHER_RIGHT_run_command, 3, (SELFTEST_action_t[]){
 								 {ACT_CARPET_LAUNCHER_RIGHT_IDLE,		0,  QUEUE_ACT_AX12_CARPET_LAUNCHER_RIGHT},
-								 {ACT_CARPET_LAUNCHER_RIGHT_LAUNCH,			0,  QUEUE_ACT_AX12_CARPET_LAUNCHER_RIGHT},
+								 {ACT_CARPET_LAUNCHER_RIGHT_LAUNCH,		0,  QUEUE_ACT_AX12_CARPET_LAUNCHER_RIGHT},
 								 {ACT_CARPET_LAUNCHER_RIGHT_IDLE,		0,  QUEUE_ACT_AX12_CARPET_LAUNCHER_RIGHT}
 							 });
 	}
