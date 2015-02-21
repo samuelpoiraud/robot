6881e34bf17aed11680e4c367412777ad9b3ae94
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2015-02-21 02:23:00 +0000

    Ajout de l'actionneur pincemi (les deux pinces du bas de l'actionneur à spot) Retour sur commit pour la gestion selftest actionneur qui en fait été pas utile --"

---
+++ 3_Actionneur/CurrentSoftwareSTM32/Holly/Ascenseur/pincemi_RX24_config.h
@@ -0,0 +1,65 @@
+/*  Club Robot ESEO 2014 - 2015
+ *	HOLLY - WOOD
+ *
+ *	Fichier : pincemi_RX24_config.h
+ *	Package : Carte actionneur
+ *	Description : Gestion des pinces de l'actionneur spot
+ *  Auteur : Arnaud
+ *  Version 20130219
+ *  Robot : HOLLY
+ */
+
+#ifndef PINCEMI_RX24_CONFIG_H
+#define	PINCEMI_RX24_CONFIG_H
+
+//Config
+	#define PINCEMI_RX24_ASSER_TIMEOUT								1000			// Si l'ax12 n'atteint pas la position demandée avant ce temps, on considère qu'il y a un problème. Temps en ms.
+	#define PINCEMI_RX24_ASSER_POS_EPSILON							2				// Lorsque l'ax12 atteint sa position avec +/- POS_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
+	#define PINCEMI_RX24_ASSER_POS_LARGE_EPSILON					10				// Si le déplacement de l'ax12 timeout mais que sa position est à +/- POS_LARGE_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
+	#define PINCEMI_RX24_MAX_TORQUE_PERCENT							100				// Couple maximum en pourcentage de l'ax12. Utilisé pour limiter le courant dans le moteur. A mettre a une valeur correcte pour pincer assez fort sans risquer d'endommager l'AX12.
+
+//Position
+
+	// Pince droite, mord droit
+	#define PINCEMIR_RIGHT_CLOSE									50
+	#define PINCEMIR_RIGHT_CLOSE_INNER								150
+	#define PINCEMIR_RIGHT_LOCK										150
+	#define PINCEMIR_RIGHT_OPEN										150
+	#define PINCEMIR_RIGHT_OPEN_GREAT								150
+
+	// Pince droite, mord gauche
+	#define PINCEMIR_LEFT_CLOSE										50
+	#define PINCEMIR_LEFT_CLOSE_INNER								150
+	#define PINCEMIR_LEFT_LOCK										150
+	#define PINCEMIR_LEFT_OPEN										150
+	#define PINCEMIR_LEFT_OPEN_GREAT								150
+
+	// Pince gauche, mord droit
+	#define PINCEMIL_RIGHT_CLOSE									50
+	#define PINCEMIL_RIGHT_CLOSE_INNER								150
+	#define PINCEMIL_RIGHT_LOCK										150
+	#define PINCEMIL_RIGHT_OPEN										150
+	#define PINCEMIL_RIGHT_OPEN_GREAT								150
+
+	// Pince gauche, mord gauche
+	#define PINCEMIL_LEFT_CLOSE										50
+	#define PINCEMIL_LEFT_CLOSE_INNER								150
+	#define PINCEMIL_LEFT_LOCK										150
+	#define PINCEMIL_LEFT_OPEN										150
+	#define PINCEMIL_LEFT_OPEN_GREAT								150
+
+//Position initiale du RX24
+	#define PINCEMIR_RIGHT_INIT_POS									PINCEMIR_RIGHT_CLOSE
+	#define PINCEMIR_LEFT_INIT_POS									PINCEMIR_LEFT_CLOSE
+	#define PINCEMIL_RIGHT_INIT_POS									PINCEMIL_RIGHT_CLOSE
+	#define PINCEMIL_LEFT_INIT_POS									PINCEMIL_LEFT_CLOSE
+
+
+//Extremum valeur
+	#define PINCEMI_RX24_MIN_VALUE									0
+	#define PINCEMI_RX24_MAX_VALUE									300
+
+
+#endif	/* PINCEMI_RX24_CONFIG_H */
+
+
