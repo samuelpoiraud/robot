89878b99f950da20b7ae249a43850dd037155cee
robot_pc_droit <robot_pc_droit@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2015-04-03 11:06:50 +0000

    Passage de 300 vers 1024 pour QS_AX12

--- 3_Actionneur/CurrentSoftwareSTM32/Wood/Pince_droite_config.h
+++ 3_Actionneur/CurrentSoftwareSTM32/Wood/Pince_droite_config.h
@@ -14,21 +14,21 @@
 
 //Config
 	#define PINCE_DROITE_AX12_ASSER_TIMEOUT				1000	// Si l'ax12 n'atteint pas la position demandée avant ce temps, on considère qu'il y a un problème. Temps en ms.
-	#define PINCE_DROITE_AX12_ASSER_POS_EPSILON			2		// Lorsque l'ax12 atteint sa position avec +/- POS_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
-	#define PINCE_DROITE_AX12_ASSER_POS_LARGE_EPSILON	10		// Si le déplacement de l'ax12 timeout mais que sa position est à +/- POS_LARGE_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
+	#define PINCE_DROITE_AX12_ASSER_POS_EPSILON			6		// Lorsque l'ax12 atteint sa position avec +/- POS_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
+	#define PINCE_DROITE_AX12_ASSER_POS_LARGE_EPSILON	28		// Si le déplacement de l'ax12 timeout mais que sa position est à +/- POS_LARGE_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
 	#define PINCE_DROITE_AX12_MAX_TORQUE_PERCENT		100		// Couple maximum en pourcentage de l'ax12. Utilisé pour limiter le courant dans le moteur. A mettre a une valeur correcte pour pincer assez fort sans risquer d'endommager l'AX12.
 
 //Position
-	#define PINCE_DROITE_AX12_CLOSE_POS					225		//en degré
-	#define PINCE_DROITE_AX12_MID_POS					200  	//en degré  25° de diff avec IDLE POS
-	#define PINCE_DROITE_AX12_OPEN_POS					150		//en degré
+	#define PINCE_DROITE_AX12_CLOSE_POS					768		//en degré
+	#define PINCE_DROITE_AX12_MID_POS					682  	//en degré  25° de diff avec IDLE POS
+	#define PINCE_DROITE_AX12_OPEN_POS					512		//en degré
 
 //Position initiale de l'AX12
 	#define PINCE_DROITE_AX12_INIT_POS					PINCE_DROITE_AX12_CLOSE_POS
 
 //Extremum valeur
-	#define PINCE_DROITE_AX12_MAX_VALUE					300
 	#define PINCE_DROITE_AX12_MIN_VALUE					0
+	#define PINCE_DROITE_AX12_MAX_VALUE					1024
 
 #endif	/* PINCE_DROITE_CONFIG_H */
 
