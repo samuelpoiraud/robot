af1d88e57f1b8ce042628c3f3fcfc0275e5644cf
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2015-04-14 17:24:03 +0000

    Correction couple servo tapis

--- 3_Actionneur/CurrentSoftwareSTM32/Holly/Carpet/carpet_launcher_right_config.h
+++ 3_Actionneur/CurrentSoftwareSTM32/Holly/Carpet/carpet_launcher_right_config.h
@@ -16,7 +16,7 @@
 	#define CARPET_LAUNCHER_RIGHT_AX12_ASSER_TIMEOUT				1000			// Si l'ax12 n'atteint pas la position demandée avant ce temps, on considère qu'il y a un problème. Temps en ms.
 	#define CARPET_LAUNCHER_RIGHT_AX12_ASSER_POS_EPSILON			6				// Lorsque l'ax12 atteint sa position avec +/- POS_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
 	#define CARPET_LAUNCHER_RIGHT_AX12_ASSER_POS_LARGE_EPSILON		28				// Si le déplacement de l'ax12 timeout mais que sa position est à +/- POS_LARGE_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
-	#define CARPET_LAUNCHER_RIGHT_AX12_MAX_TORQUE_PERCENT			70				// Couple maximum en pourcentage de l'ax12. Utilisé pour limiter le courant dans le moteur. A mettre a une valeur correcte pour pincer assez fort sans risquer d'endommager l'AX12.
+	#define CARPET_LAUNCHER_RIGHT_AX12_MAX_TORQUE_PERCENT			30				// Couple maximum en pourcentage de l'ax12. Utilisé pour limiter le courant dans le moteur. A mettre a une valeur correcte pour pincer assez fort sans risquer d'endommager l'AX12.
 
 //Position
 	#define CARPET_LAUNCHER_RIGHT_AX12_IDLE_POS						557				//en degré
