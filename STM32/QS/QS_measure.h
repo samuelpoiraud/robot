9c59ac6f74906db288d07e07649434f0cc521c6a
spoiraud <spoiraud@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2015-04-16 11:56:05 +0000

    front holly

--- STM32/QS/QS_measure.h
+++ STM32/QS/QS_measure.h
@@ -13,7 +13,7 @@
 // BIG
 #define BIG_ROBOT_WIDTH					292		//Largeur du gros robot [mm]
 #define BIG_ROBOT_CALIB_BACK			146		//Distance entre le 'centre' du robot et l'arrière en calage [mm]
-#define BIG_ROBOT_CALIB_FRONT			111		//Distance entre le 'centre' du robot et l'avant en calage [mm]
+#define BIG_ROBOT_CALIB_FRONT			100		//Distance entre le 'centre' du robot et l'avant en calage [mm]
 
 // SMALL
 #define SMALL_ROBOT_WIDTH				190		//Largeur du petit robot [mm]
