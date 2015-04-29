bd2c80358777cf14bade10538eccea34122bb266
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2015-04-29 18:42:21 +0000

    Position des actionneurs popcorn

--- 3_Actionneur/CurrentSoftwareSTM32/Holly/Popcorn/Pop_collect_right_config.h
+++ 3_Actionneur/CurrentSoftwareSTM32/Holly/Popcorn/Pop_collect_right_config.h
@@ -16,13 +16,13 @@
 	#define POP_COLLECT_RIGHT_AX12_ASSER_TIMEOUT			1000			// Si l'ax12 n'atteint pas la position demandée avant ce temps, on considère qu'il y a un problème. Temps en ms.
 	#define POP_COLLECT_RIGHT_AX12_ASSER_POS_EPSILON		6				// Lorsque l'ax12 atteint sa position avec +/- POS_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
 	#define POP_COLLECT_RIGHT_AX12_ASSER_POS_LARGE_EPSILON	28				// Si le déplacement de l'ax12 timeout mais que sa position est à +/- POS_LARGE_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
-	#define POP_COLLECT_RIGHT_AX12_MAX_TORQUE_PERCENT		100				// Couple maximum en pourcentage de l'ax12. Utilisé pour limiter le courant dans le moteur. A mettre a une valeur correcte pour pincer assez fort sans risquer d'endommager l'AX12.
-	#define POP_COLLECT_RIGHT_AX12_MAX_TEMPERATURE           60				// Température maximale en degré
+	#define POP_COLLECT_RIGHT_AX12_MAX_TORQUE_PERCENT		60				// Couple maximum en pourcentage de l'ax12. Utilisé pour limiter le courant dans le moteur. A mettre a une valeur correcte pour pincer assez fort sans risquer d'endommager l'AX12.
+	#define POP_COLLECT_RIGHT_AX12_MAX_TEMPERATURE          60				// Température maximale en degré
 
 //Position
-	#define POP_COLLECT_RIGHT_AX12_IDLE_POS					122				//en degré
-	#define POP_COLLECT_RIGHT_AX12_MID_POS					316				//en degré
-	#define POP_COLLECT_RIGHT_AX12_OPEN_POS				390				//en degré
+	#define POP_COLLECT_RIGHT_AX12_IDLE_POS					32				//en degré
+	#define POP_COLLECT_RIGHT_AX12_MID_POS					264				//en degré
+	#define POP_COLLECT_RIGHT_AX12_OPEN_POS				    426				//en degré
 
 //Position initiale de l'AX12
 	#define POP_COLLECT_RIGHT_AX12_INIT_POS					POP_COLLECT_RIGHT_AX12_IDLE_POS
