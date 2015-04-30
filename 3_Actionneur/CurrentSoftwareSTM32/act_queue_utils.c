e9ff631b575cf3a51cb1822ce68e0b7438c843f3
agarry <agarry@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2015-04-30 20:19:32 +0000

    correction diverse

--- 3_Actionneur/CurrentSoftwareSTM32/act_queue_utils.c
+++ 3_Actionneur/CurrentSoftwareSTM32/act_queue_utils.c
@@ -251,8 +251,8 @@ static void ACTQ_internal_printResult(Uint11 originalSid, Uint8 originalCommand,
 		case ACT_POP_DROP_RIGHT & 0xFF:			originalSidStr = "Pop_drop_right";			break;
 		case ACT_BACK_SPOT_RIGHT & 0xFF:		originalSidStr = "Back_spot_right";			break;
 		case ACT_BACK_SPOT_LEFT & 0xFF:			originalSidStr = "Back_spot_left";			break;
-		case ACT_SPOT_POMPE_RIGHT & 0xFF:		originalSidStr = "Spot_pompe_left";			break;
-		case ACT_SPOT_POMPE_LEFT & 0xFF:		originalSidStr = "Spot_pompe_right";		break;
+		case ACT_SPOT_POMPE_RIGHT & 0xFF:		originalSidStr = "Spot_pompe_right";		break;
+		case ACT_SPOT_POMPE_LEFT & 0xFF:		originalSidStr = "Spot_pompe_left";			break;
 		case ACT_CARPET_LAUNCHER_RIGHT & 0xFF:	originalSidStr = "Carpet_launcher_right";	break;
 		case ACT_CARPET_LAUNCHER_LEFT & 0xFF:	originalSidStr = "Carpet_launcher_left";	break;
 		case ACT_PINCEMI_RIGHT & 0xFF:			originalSidStr = "Pincemi_right";			break;
