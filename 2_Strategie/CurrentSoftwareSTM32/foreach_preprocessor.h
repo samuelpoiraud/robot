c7626d3e5624fce0b5581e17dc742110d6772c57
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2015-05-23 16:38:37 +0000

    Augmentation des foreach pour la MAE spotix

--- 2_Strategie/CurrentSoftwareSTM32/foreach_preprocessor.h
+++ 2_Strategie/CurrentSoftwareSTM32/foreach_preprocessor.h
@@ -13,9 +13,11 @@
 		 _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
 		 _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
 		 _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
-		 _61,_62,_63,N,...) N
+		 _61,_62,_63,_64,_65,_66,_67,_68,_69,_70, \
+		 _71,_72,_73,_74,_75,_76,_77,_78,_79,N,...) N
 #define PP_RSEQ_N() \
-		 63,62,61,60,                   \
+		 79,78,77,76,75,74,73,72,71,70, \
+		 69,68,67,66,65,64,63,62,61,60, \
 		 59,58,57,56,55,54,53,52,51,50, \
 		 49,48,47,46,45,44,43,42,41,40, \
 		 39,38,37,36,35,34,33,32,31,30, \
@@ -96,6 +98,12 @@
 #define FOREACH_71(macro, val, ...) macro(val), FOREACH_70(macro, ##__VA_ARGS__)
 #define FOREACH_72(macro, val, ...) macro(val), FOREACH_71(macro, ##__VA_ARGS__)
 #define FOREACH_73(macro, val, ...) macro(val), FOREACH_72(macro, ##__VA_ARGS__)
+#define FOREACH_74(macro, val, ...) macro(val), FOREACH_73(macro, ##__VA_ARGS__)
+#define FOREACH_75(macro, val, ...) macro(val), FOREACH_74(macro, ##__VA_ARGS__)
+#define FOREACH_76(macro, val, ...) macro(val), FOREACH_75(macro, ##__VA_ARGS__)
+#define FOREACH_77(macro, val, ...) macro(val), FOREACH_76(macro, ##__VA_ARGS__)
+#define FOREACH_78(macro, val, ...) macro(val), FOREACH_77(macro, ##__VA_ARGS__)
+#define FOREACH_79(macro, val, ...) macro(val), FOREACH_78(macro, ##__VA_ARGS__)
 
 
 #define CONCATENATE(arg1, arg2)   CONCATENATE1(arg1, arg2)
