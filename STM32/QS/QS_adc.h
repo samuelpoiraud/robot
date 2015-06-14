f084a3f17a12b0b72f67e99215bec7fe8e8cde24
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2015-06-14 18:28:13 +0000

    Avancement dans la complétion de commentaire en QS

--- STM32/QS/QS_adc.h
+++ STM32/QS/QS_adc.h
@@ -8,11 +8,34 @@
  *  Version 20130518
  */
 
+/** ----------------  Defines possibles  --------------------
+ *
+ *	USE_AN0						: Activation du convertisseur analogique 0
+ *	USE_AN1						: Activation du convertisseur analogique 1
+ *	USE_AN2						: Activation du convertisseur analogique 2
+ *	USE_AN3						: Activation du convertisseur analogique 3
+ *	USE_AN4						: Activation du convertisseur analogique 4
+ *	USE_AN5						: Activation du convertisseur analogique 5
+ *	USE_AN6						: Activation du convertisseur analogique 6
+ *	USE_AN7						: Activation du convertisseur analogique 7
+ *	USE_AN8						: Activation du convertisseur analogique 8
+ *	USE_AN9						: Activation du convertisseur analogique 9
+ *	USE_AN10					: Activation du convertisseur analogique 10
+ *	USE_AN11					: Activation du convertisseur analogique 11
+ *	USE_AN12					: Activation du convertisseur analogique 12
+ *	USE_AN13					: Activation du convertisseur analogique 13
+ *	USE_AN14					: Activation du convertisseur analogique 14
+ *	USE_AN15					: Activation du convertisseur analogique 15
+ */
+
 #ifndef QS_ADC_H
 	#define QS_ADC_H
 
-	#include "QS_types.h"
+	#include "QS_all.h"
 
+	/**
+	 * @brief Enumération des convertisseurs analogique numérique sélectionnable
+	 */
 	typedef enum{
 		ADC_0 = 0,
 		ADC_1,
@@ -31,8 +54,6 @@
 		ADC_14,
 		ADC_15
 	}adc_id_e;
-	// Enumération des convertisseurs analogique numérique sélectionnable
-
 
 	/**
 	 * @brief Fonction d'initialisation des ADCs
