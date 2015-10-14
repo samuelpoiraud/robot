1a2c90ffbc9c0ad676a3f585e4c57bc90942d5d1
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2015-10-14 09:49:51 +0000

    structure des fichiers de détections de choc avec accéléromètres

---
+++ 2_Strategie/CurrentSoftwareSTM32/detection_choc.h
@@ -0,0 +1,21 @@
+/*
+ *	Club Robot ESEO 2015 - 2016
+ *	Black & Pearl
+ *
+ *	Fichier : detection_choc.c
+ *	Package : Carte Stratégie
+ *	Description : Detection de choc avec un robot adverse entrainant une perte de notre position
+ *	Auteur :
+ *	Version 2015/10/14
+ */
+
+
+#include "QS/QS_accelero.h"
+
+#ifndef DETECTION_CHOC_H
+	#define  DETECTION_CHOC_H
+
+
+
+
+#endif /* ndef  DETECTION_CHOC_H */
