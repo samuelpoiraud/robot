3f620f3dea1a15b596417f6006acaf30ad612182
jgoum <jgoum@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2015-11-19 20:15:17 +0000

    Rajout de fichiers manquants pour actionneur LEFT_ARM sur Pearl.

---
+++ 3_Actionneur/CurrentSoftwareSTM32/Pearl/Sand/left_arm.h
@@ -0,0 +1,59 @@
+/*  Club Robot ESEO 2015 - 2016
+ *	SMALL
+ *
+ *	Fichier : LEFT_ARM.h
+ *	Package : Carte actionneur
+ *	Description : Gestion de l'actionneur LEFT_ARM
+ *  Auteur : Julien
+ *  Version 2016
+ *  Robot : SMALL
+ */
+
+#ifndef LEFT_ARM_H
+	#define	LEFT_ARM_H
+
+	#include "../QS/QS_all.h"
+	#include "../QS/QS_CANmsgList.h"
+
+	#include "../queue.h"
+
+	/** Initialisation du gestionnaire de LEFT_ARM.
+	 *
+	 * Configure l'asservissement de l'AX12
+	 */
+	void LEFT_ARM_init();
+
+	/** Initialisation en position du gestionnaire de LEFT_ARM.
+	 *
+	 * Initialise la position de l'AX12
+	 */
+	void LEFT_ARM_init_pos();
+
+	/** Stoppe l'actionneur.
+	 *
+	 * Cette fonction est appelée en fin de match
+	 */
+	void LEFT_ARM_stop();
+
+	/** Gère les messages CAN liés à la LEFT_ARM.
+	 *
+	 * Cette fonction s'occupe toute seule de gérer la queue et d'agir en conséquence.
+	 * @param msg le message CAN
+	 * @return TRUE si le message CAN a été géré par cet actionneur, FALSE sinon
+	 */
+	bool_e LEFT_ARM_CAN_process_msg(CAN_msg_t* msg);
+
+	/** Gère les commandes demandées.
+	 *
+	 * Fonction à mettre sur la file pour demander une action.
+	 */
+	void LEFT_ARM_run_command(queue_id_t queueId, bool_e init);
+
+
+	/**
+	 * Réinitialise la configuration de l'AX12
+	 */
+	void LEFT_ARM_reset_config();
+
+#endif	/* LEFT_ARM_H */
+
---
+++ 3_Actionneur/CurrentSoftwareSTM32/Pearl/Sand/left_arm_config.h
@@ -0,0 +1,36 @@
+/*  Club Robot ESEO 2015 - 2016
+ *	SMALL
+ *
+ *	Fichier : LEFT_ARM_config.h
+ *	Package : Carte actionneur
+ *	Description : Gestion de l'actionneur LEFT_ARM
+ *  Auteur : Julien
+ *  Version 2016
+ *  Robot : SMALL
+ */
+
+#ifndef LEFT_ARM_CONFIG_H
+#define	LEFT_ARM_CONFIG_H
+
+//Config
+	#define LEFT_ARM_AX12_ASSER_TIMEOUT				2000	// Si l'ax12 n'atteint pas la position demandée avant ce temps, on considère qu'il y a un problème. Temps en ms.
+	#define LEFT_ARM_AX12_ASSER_POS_EPSILON			6		// Lorsque l'ax12 atteint sa position avec +/- POS_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
+	#define LEFT_ARM_AX12_ASSER_POS_LARGE_EPSILON	28		// Si le déplacement de l'ax12 timeout mais que sa position est à +/- POS_LARGE_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
+	#define LEFT_ARM_AX12_MAX_TORQUE_PERCENT		    50		// Couple maximum en pourcentage de l'ax12. Utilisé pour limiter le courant dans le moteur. A mettre a une valeur correcte pour pincer assez fort sans risquer d'endommager l'AX12.
+	#define LEFT_ARM_AX12_MAX_TEMPERATURE			60		// Température maximale en degré
+
+//Position
+	#define LEFT_ARM_AX12_IDLE_POS					512
+	#define LEFT_ARM_AX12_CLOSE_POS				    512		//en degré
+	#define LEFT_ARM_AX12_OPEN_POS					512		//en degré
+
+//Position initiale de l'AX12
+	#define LEFT_ARM_AX12_INIT_POS					LEFT_ARM_AX12_IDLE_POS
+
+//Extremum valeur
+	#define LEFT_ARM_AX12_MIN_VALUE					0
+	#define LEFT_ARM_AX12_MAX_VALUE					1024
+
+#endif	/* LEFT_ARM_CONFIG_H */
+
+
