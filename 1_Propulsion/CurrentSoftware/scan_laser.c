4625d140dd00eabcb953dfb949c22ba4f7596dae
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-02-27 14:13:27 +0000

    Ajout des fichiers de scan

---
+++ 1_Propulsion/CurrentSoftware/scan_laser.c
@@ -0,0 +1,12 @@
+/*
+ *  Club Robot ESEO 2009 - 2010
+ *
+ *  Fichier : scan_laser.c
+ *  Package : Propulsion
+ *  Description : Scan avec scan_laser
+ *  Auteur :
+ *  Version 201001
+ */
+
+
+#define CONVERSION_LASER_LEFT(x)  ((Sint32)((x)*5385-18695)/10000)
