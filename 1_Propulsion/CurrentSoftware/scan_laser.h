62abfb159a58b9f80a5cc2912412e6407ad90f0a
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-02-27 14:13:27 +0000

    Ajout des fichiers de scan

---
+++ 1_Propulsion/CurrentSoftware/scan_laser.h
@@ -0,0 +1,16 @@
+/*
+ *  Club Robot ESEO 2009 - 2010
+ *
+ *  Fichier : scan_laser.h
+ *  Package : Propulsion
+ *  Description : Scan avec scan_laser
+ *  Auteur :
+ *  Version 201001
+ */
+
+#ifndef SCAN_LASER_H
+#define SCAN_LASER_H
+
+
+
+#endif // SCAN_LASER_H
