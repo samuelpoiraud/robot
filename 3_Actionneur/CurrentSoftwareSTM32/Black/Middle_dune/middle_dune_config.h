731ad9d2b3b6c72bf8d215d0c7a8377f9d5ebd23
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-04-04 11:52:17 +0000

    Ajout des fichiers bottom dune et middle dune synchro

---
+++ 3_Actionneur/CurrentSoftwareSTM32/Black/Middle_dune/middle_dune_config.h
@@ -0,0 +1,43 @@
+/*  Club Robot ESEO 2015 - 2016
+ *	BIG
+ *
+ *	Fichier : middle_dune_config.h
+ *	Package : Carte actionneur
+ *	Description : Gestion de l'actionneur RX24 MIDDLE_DUNE
+ *  Auteur : Julien
+ *  Version 2016
+ *  Robot : BIG
+ */
+
+#ifndef MIDDLE_DUNE_CONFIG_H
+#define	MIDDLE_DUNE_CONFIG_H
+
+//Config
+	#define MIDDLE_DUNE_RX24_ASSER_TIMEOUT					600		// Si le RX24 n'atteint pas la position demandée avant ce temps, on considère qu'il y a un problème. Temps en ms.
+	#define MIDDLE_DUNE_RX24_ASSER_POS_EPSILON				6		// Lorsque le RX24 atteint sa position avec +/- POS_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
+	#define MIDDLE_DUNE_RX24_ASSER_POS_LARGE_EPSILON	    28		// Si le déplacement de le RX24 timeout mais que sa position est à +/- POS_LARGE_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
+	#define MIDDLE_DUNE_RX24_MAX_TORQUE_PERCENT		        100		// Couple maximum en pourcentage de le RX24. Utilisé pour limiter le courant dans le moteur. A mettre a une valeur correcte pour pincer assez fort sans risquer d'endommager le RX24.
+	#define MIDDLE_DUNE_RX24_MAX_TEMPERATURE			    65		// Température maximale en degré
+	#define MIDDLE_DUNE_RX24_MAX_SPEED                      50      // Vitesse maximale en pourcent
+
+
+//Position
+	#define MIDDLE_DUNE_L_RX24_IDLE_POS					    920
+	#define MIDDLE_DUNE_L_RX24_LOCK_POS			     	    596
+	#define MIDDLE_DUNE_L_RX24_UNLOCK_POS				    920
+
+	#define MIDDLE_DUNE_R_RX24_IDLE_POS					    95
+	#define MIDDLE_DUNE_R_RX24_LOCK_POS			     	    422
+	#define MIDDLE_DUNE_R_RX24_UNLOCK_POS				    95
+
+//Position initiale de l'RX24
+	#define MIDDLE_DUNE_L_RX24_INIT_POS					MIDDLE_DUNE_L_RX24_IDLE_POS
+	#define MIDDLE_DUNE_R_RX24_INIT_POS					MIDDLE_DUNE_R_RX24_IDLE_POS
+
+//Extremum valeur
+	#define MIDDLE_DUNE_RX24_MIN_VALUE					0
+	#define MIDDLE_DUNE_RX24_MAX_VALUE					1024
+
+#endif	/* MIDDLE_DUNE_CONFIG_H */
+
+
