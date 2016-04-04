096d07f9a00bcb739e994295d2180143df9799dc
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-04-04 11:52:17 +0000

    Ajout des fichiers bottom dune et middle dune synchro

---
+++ 3_Actionneur/CurrentSoftwareSTM32/Black/Bottom_dune/bottom_dune.h
@@ -0,0 +1,59 @@
+/*  Club Robot ESEO 2015 - 2016
+ *	BIG
+ *
+ *	Fichier : bottom_dune.h
+ *	Package : Carte actionneur
+ *	Description : Gestion de l'actionneur RX24 BOTTOM_DUNE
+ *  Auteur : Julien
+ *  Version 2016
+ *  Robot : BIG
+ */
+
+#ifndef BOTTOM_DUNE_H
+	#define	BOTTOM_DUNE_H
+
+	#include "../QS/QS_all.h"
+	#include "../QS/QS_CANmsgList.h"
+
+	#include "../queue.h"
+
+	/** Initialisation du gestionnaire de BOTTOM_DUNE.
+	 *
+	 * Configure l'asservissement du RX24
+	 */
+	void BOTTOM_DUNE_init();
+
+	/** Initialisation en position du gestionnaire de BOTTOM_DUNE.
+	 *
+	 * Initialise la position du RX24
+	 */
+	void BOTTOM_DUNE_init_pos();
+
+	/** Stoppe l'actionneur.
+	 *
+	 * Cette fonction est appelée en fin de match
+	 */
+	void BOTTOM_DUNE_stop();
+
+	/** Gère les messages CAN liés à la BOTTOM_DUNE.
+	 *
+	 * Cette fonction s'occupe toute seule de gérer la queue et d'agir en conséquence.
+	 * @param msg le message CAN
+	 * @return TRUE si le message CAN a été géré par cet actionneur, FALSE sinon
+	 */
+	bool_e BOTTOM_DUNE_CAN_process_msg(CAN_msg_t* msg);
+
+	/** Gère les commandes demandées.
+	 *
+	 * Fonction à mettre sur la file pour demander une action.
+	 */
+	void BOTTOM_DUNE_run_command(queue_id_t queueId, bool_e init);
+
+
+	/**
+	 * Réinitialise la configuration du RX24
+	 */
+	void BOTTOM_DUNE_reset_config();
+
+#endif	/* BOTTOM_DUNE_H */
+
