d6e03857cf639d120d2729a68db7ac2c56942f99
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-05-02 06:37:15 +0000

    Correction script delete-junction

--- STM32_delete-junctions.bat
+++ STM32_delete-junctions.bat
@@ -14,10 +14,10 @@ junction.exe -d 6_IHM\CurrentSoftware\QS
 junction.exe -d 6_IHM\CurrentSoftware\stm32f4xx
 junction.exe -d R_D\HokuyoUTM30LX\Software\QS
 junction.exe -d R_D\HokuyoUTM30LX\Software\stm32f4xx
-junction.exe -d R_D\projet_matrice_led\CurrentSoftware\QS STM32\QS
-junction.exe -d R_D\projet_matrice_led\CurrentSoftware\stm32f4xx STM32\stm32f4xx
-junction.exe -d R_D\Balise_Emettrice\CurrentSoftware\QS STM32\QS
-junction.exe -d R_D\Balise_Emettrice\CurrentSoftware\stm32f4xx STM32\stm32f4xx
-junction.exe -d R_D\Balise_Receptrice\CurrentSoftware\QS STM32\QS
-junction.exe -d R_D\Balise_Receptrice\CurrentSoftware\stm32f4xx STM32\stm32f4xx
+junction.exe -d R_D\projet_matrice_led\CurrentSoftware\QS
+junction.exe -d R_D\projet_matrice_led\CurrentSoftware\stm32f4xx
+junction.exe -d R_D\Balise_Emettrice\CurrentSoftware\QS
+junction.exe -d R_D\Balise_Emettrice\CurrentSoftware\stm32f4xx
+junction.exe -d R_D\Balise_Receptrice\CurrentSoftware\QS
+junction.exe -d R_D\Balise_Receptrice\CurrentSoftware\stm32f4xx
 pause
