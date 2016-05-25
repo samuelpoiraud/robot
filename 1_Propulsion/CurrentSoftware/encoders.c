a1b6d2907161af89671f145ff9c6a94239e0a44f
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-05-25 09:46:28 +0000

    Fonction inutile, fonction inutilisé....

--- 1_Propulsion/CurrentSoftware/encoders.c
+++ 1_Propulsion/CurrentSoftware/encoders.c
@@ -49,39 +49,3 @@ void ENCODERS_get(Sint32 * left, Sint32 * right)
 	count_left_prec=count_left;
 	count_right_prec=count_right;
 }
-
-void ENCODERS_get2(Sint32 * left, Sint32 * right)
-{
-	//variables locales (car on fait les calculs en Sint16 et le retour est en Sint32...
-	Sint32 delta_left;
-	Sint32 delta_right;
-	Sint16 count_left;
-	Sint16 count_right;
-	static Sint16 count_left_prec = 0;
-	static Sint16 count_right_prec = 0;
-
-
-	//Lire les roues codeuses
-	count_right=QEI1_get_count();
-	count_left=-QEI2_get_count();
-
-	//Mise a jour des deltas des roues...
-	delta_left=count_left-count_left_prec;
-	delta_right=count_right-count_right_prec;
-
-	if(delta_left > 512)
-		delta_left -= 65536;
-	else if(delta_left < -512)
-		delta_left += 65536;
-	if(delta_right > 512)
-		delta_right -= 65536;
-	else if(delta_right < -512)
-		delta_right += 65536;
-
-	*left = (Sint32)delta_left;
-	*right = (Sint32)delta_right;
-
-	//Sauvegarde de l'état actuel pour le prochain appel.
-	count_left_prec=count_left;
-	count_right_prec=count_right;
-}
