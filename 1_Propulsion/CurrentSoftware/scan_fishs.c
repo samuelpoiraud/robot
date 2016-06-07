7b11d2b8fbf6d0770d72ff6f1c650989c0a05400
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-06-07 17:56:11 +0000

    Affichage pour le scan poissons

--- 1_Propulsion/CurrentSoftware/scan_fishs.c
+++ 1_Propulsion/CurrentSoftware/scan_fishs.c
@@ -80,15 +80,18 @@ void SCAN_FISHS_process_it(){
 		GEOMETRY_point_t point;
 		point.x = global.position.x + distance_scan*cos4096(global.position.teta);
 		point.y = global.position.y + distance_scan*sin4096(global.position.teta);
+		debug_printf("%4d ; %4d",point.x, point.y);
 		if(tab_index < NB_POINTS){
 			if(color == MAGENTA && is_in_square(2027 + MARGIN_BAC , 2227 - MARGIN_BAC, 503 + MARGIN_BAC, 903 - MARGIN_BAC, point)){
 				tab_points[tab_index].x = point.x;
 				tab_points[tab_index].y = point.y;
 				tab_index++;
+				debug_printf(" X\n");
 			}else if(color == GREEN && is_in_square(2027 + MARGIN_BAC , 2227 - MARGIN_BAC, 2097 + MARGIN_BAC, 2497 - MARGIN_BAC, point)){
 				tab_points[tab_index].x = point.x;
 				tab_points[tab_index].y = point.y;
 				tab_index++;
+				debug_printf("\n");
 			}
 		}
 	}
@@ -99,6 +102,7 @@ static void SCAN_FISHS_treatment(){
 	Uint16 moyX, moyY;
 	Uint16 index_begin_detection = 0;
 
+	debug_printf("tab_index=%d", tab_index);
 	//PS: Si on ne rentre pas dans le for, alors aucun poisson n'a été détecté
 	for(i = 0; i < tab_index && nb_fishs_detected < NB_FISHS; i++){
 		debug_printf("i=%3d  %4d %4d\n", i, tab_points[i].x, tab_points[i].y);
