27953b73431d57bdec48a5c008ab4c54376719a0
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-06-09 15:09:45 +0000

    Augmentation du nombre d'état dans les states machine

--- 2_Strategie/CurrentSoftwareSTM32/foreach_preprocessor.h
+++ 2_Strategie/CurrentSoftwareSTM32/foreach_preprocessor.h
@@ -7,18 +7,21 @@
 #define PP_NARG_(...) \
 		 PP_ARG_N(__VA_ARGS__)
 #define PP_ARG_N( \
-		  _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
-		 _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
-		 _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
-		 _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
-		 _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
-		 _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
-		 _61,_62,_63,_64,_65,_66,_67,_68,_69,_70, \
-		 _71,_72,_73,_74,_75,_76,_77,_78,_79,_80, \
-		 _81,_82,_83,_84,_85,_86,_87,_88,_89,_90, \
-		 _91,_92,_93,_94,_95,_96,_97,_98,_99,N,...) N
+			_1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
+			_11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
+			_21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
+			_31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
+			_41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
+			_51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
+			_61,_62,_63,_64,_65,_66,_67,_68,_69,_70, \
+			_71,_72,_73,_74,_75,_76,_77,_78,_79,_80, \
+			_81,_82,_83,_84,_85,_86,_87,_88,_89,_90, \
+			_91,_92,_93,_94,_95,_96,_97,_98,_99,_100, \
+			_101,_102,_103,_104,_105,_106,_107,_108,_109,N,...) N
+
 #define PP_RSEQ_N() \
-		 99,98,97,96,95,94,93,92,91,90, \
+		109,108,107,106,105,104,103,102,101,100, \
+		99,98,97,96,95,94,93,92,91,90, \
 		 89,88,87,86,85,84,83,82,81,80, \
 		 79,78,77,76,75,74,73,72,71,70, \
 		 69,68,67,66,65,64,63,62,61,60, \
@@ -128,6 +131,16 @@
 #define FOREACH_97(macro, val, ...) macro(val), FOREACH_96(macro, ##__VA_ARGS__)
 #define FOREACH_98(macro, val, ...) macro(val), FOREACH_97(macro, ##__VA_ARGS__)
 #define FOREACH_99(macro, val, ...) macro(val), FOREACH_98(macro, ##__VA_ARGS__)
+#define FOREACH_100(macro, val, ...) macro(val), FOREACH_99(macro, ##__VA_ARGS__)
+#define FOREACH_101(macro, val, ...) macro(val), FOREACH_100(macro, ##__VA_ARGS__)
+#define FOREACH_102(macro, val, ...) macro(val), FOREACH_101(macro, ##__VA_ARGS__)
+#define FOREACH_103(macro, val, ...) macro(val), FOREACH_102(macro, ##__VA_ARGS__)
+#define FOREACH_104(macro, val, ...) macro(val), FOREACH_103(macro, ##__VA_ARGS__)
+#define FOREACH_105(macro, val, ...) macro(val), FOREACH_104(macro, ##__VA_ARGS__)
+#define FOREACH_106(macro, val, ...) macro(val), FOREACH_105(macro, ##__VA_ARGS__)
+#define FOREACH_107(macro, val, ...) macro(val), FOREACH_106(macro, ##__VA_ARGS__)
+#define FOREACH_108(macro, val, ...) macro(val), FOREACH_107(macro, ##__VA_ARGS__)
+#define FOREACH_109(macro, val, ...) macro(val), FOREACH_108(macro, ##__VA_ARGS__)
 
 
 #define CONCATENATE(arg1, arg2)   CONCATENATE1(arg1, arg2)
