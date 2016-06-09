55deb599c49afc599be89b41121ef4023ec0ca8b
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-06-09 08:44:40 +0000

    Reprise des positions RX24 pour pelle et balayette

--- 3_Actionneur/CurrentSoftwareSTM32/Black/Shovel_dune/shovel_dune_config.h
+++ 3_Actionneur/CurrentSoftwareSTM32/Black/Shovel_dune/shovel_dune_config.h
@@ -33,14 +33,14 @@
 
 //Position
 	#define SHOVEL_DUNE_L_RX24_IDLE_POS					    635
-	#define SHOVEL_DUNE_L_RX24_TAKE_POS			     	    294
-	#define SHOVEL_DUNE_L_RX24_STORE_POS				    326
-	#define SHOVEL_DUNE_L_RX24_RESCUE_POS				    326
+	#define SHOVEL_DUNE_L_RX24_TAKE_POS			     	    291
+	#define SHOVEL_DUNE_L_RX24_STORE_POS				    321
+	#define SHOVEL_DUNE_L_RX24_RESCUE_POS				    324
 
 	#define SHOVEL_DUNE_R_RX24_IDLE_POS					    529
 	#define SHOVEL_DUNE_R_RX24_TAKE_POS			     	    871
-	#define SHOVEL_DUNE_R_RX24_STORE_POS				    835
-	#define SHOVEL_DUNE_R_RX24_RESCUE_POS					835
+	#define SHOVEL_DUNE_R_RX24_STORE_POS				    822
+	#define SHOVEL_DUNE_R_RX24_RESCUE_POS					816
 
 //Position initiale de l'RX24
 	#define SHOVEL_DUNE_L_RX24_INIT_POS						SHOVEL_DUNE_L_RX24_IDLE_POS
