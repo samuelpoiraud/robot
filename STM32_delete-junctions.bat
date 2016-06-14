e8af70e393fe5a23c68633801f882d2681a4ccaa
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-06-14 14:48:16 +0000

    Modification des créateurs de junctions pour les nouveaux projet balise

--- STM32_delete-junctions.bat
+++ STM32_delete-junctions.bat
@@ -10,14 +10,14 @@ junction.exe -d 2_Strategie\CurrentSoftwareSTM32\QS
 junction.exe -d 2_Strategie\CurrentSoftwareSTM32\stm32f4xx
 junction.exe -d 3_Actionneur\CurrentSoftwareSTM32\QS
 junction.exe -d 3_Actionneur\CurrentSoftwareSTM32\stm32f4xx
+junction.exe -d 4_Balises\Balise_Emettrice\CurrentSoftware\QS
+junction.exe -d 4_Balises\Balise_Emettrice\CurrentSoftware\stm32f4xx
+junction.exe -d 4_Balises\Balise_Receptrice\CurrentSoftware\QS
+junction.exe -d 4_Balises\Balise_Receptrice\CurrentSoftware\stm32f4xx
 junction.exe -d 6_IHM\CurrentSoftware\QS
 junction.exe -d 6_IHM\CurrentSoftware\stm32f4xx
 junction.exe -d R_D\HokuyoUTM30LX\Software\QS
 junction.exe -d R_D\HokuyoUTM30LX\Software\stm32f4xx
 junction.exe -d R_D\projet_matrice_led\CurrentSoftware\QS
 junction.exe -d R_D\projet_matrice_led\CurrentSoftware\stm32f4xx
-junction.exe -d R_D\Balise_Emettrice\CurrentSoftware\QS
-junction.exe -d R_D\Balise_Emettrice\CurrentSoftware\stm32f4xx
-junction.exe -d R_D\Balise_Receptrice\CurrentSoftware\QS
-junction.exe -d R_D\Balise_Receptrice\CurrentSoftware\stm32f4xx
 pause
