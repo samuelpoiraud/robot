c8d644d95d9e3f276884ba2f20d344df8a9630c2
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-06-14 14:48:16 +0000

    Modification des créateurs de junctions pour les nouveaux projet balise

--- STM32_create-junctions.bat
+++ STM32_create-junctions.bat
@@ -10,6 +10,10 @@ junction.exe 2_Strategie\CurrentSoftwareSTM32\QS STM32\QS
 junction.exe 2_Strategie\CurrentSoftwareSTM32\stm32f4xx STM32\stm32f4xx
 junction.exe 3_Actionneur\CurrentSoftwareSTM32\QS STM32\QS
 junction.exe 3_Actionneur\CurrentSoftwareSTM32\stm32f4xx STM32\stm32f4xx
+junction.exe 4_Balises\Balise_Emettrice\CurrentSoftware\QS STM32\QS
+junction.exe 4_Balises\Balise_Emettrice\CurrentSoftware\stm32f4xx STM32\stm32f4xx
+junction.exe 4_Balises\Balise_Receptrice\CurrentSoftware\QS STM32\QS
+junction.exe 4_Balises\Balise_Receptrice\CurrentSoftware\stm32f4xx STM32\stm32f4xx
 junction.exe 6_IHM\CurrentSoftware\QS STM32\QS
 junction.exe 6_IHM\CurrentSoftware\stm32f4xx STM32\stm32f4xx
 junction.exe R_D\HokuyoUTM30LX\Software\QS STM32\QS
@@ -18,9 +22,5 @@ junction.exe R_D\HokuyoUTM30LX\SoftwareURBCallbackInIT\QS STM32\QS
 junction.exe R_D\HokuyoUTM30LX\SoftwareURBCallbackInIT\stm32f4xx STM32\stm32f4xx
 junction.exe R_D\projet_matrice_led\CurrentSoftware\QS STM32\QS
 junction.exe R_D\projet_matrice_led\CurrentSoftware\stm32f4xx STM32\stm32f4xx
-junction.exe R_D\Balise_Emettrice\CurrentSoftware\QS STM32\QS
-junction.exe R_D\Balise_Emettrice\CurrentSoftware\stm32f4xx STM32\stm32f4xx
-junction.exe R_D\Balise_Receptrice\CurrentSoftware\QS STM32\QS
-junction.exe R_D\Balise_Receptrice\CurrentSoftware\stm32f4xx STM32\stm32f4xx
 
 pause
