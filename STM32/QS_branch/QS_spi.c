e6738f865d4d8ec88be2e984cd5b9e27a0312090
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-06-26 11:56:02 +0000

    HAL : QS_servo HAL : QS_spi

--- STM32/QS_branch/QS_spi.c
+++ STM32/QS_branch/QS_spi.c
@@ -14,7 +14,7 @@
 #include "QS_spi.h"
 #include "QS_ports.h"
 #include "QS_outputlog.h"
-#include "stm32f4xx_spi.h"
+#include "../stm32f4xx_hal/stm32f4xx_hal_spi.h"
 
 #define SPI1_SPI_HANDLE SPI1
 #define SPI1_SPI_CLOCK  RCC_APB1Periph_SPI1
@@ -25,6 +25,8 @@
 #if defined(USE_SPI1) || defined(USE_SPI2)
 
 static bool_e initialized = FALSE;
+static SPI_HandleTypeDef SPI1_HandleStructure;
+static SPI_HandleTypeDef SPI2_HandleStructure;
 
 void SPI_init(void)
 {
@@ -32,66 +34,75 @@ void SPI_init(void)
 		return;
 	initialized = TRUE;
 
-	SPI_InitTypeDef SPI_InitStructure;
-
 	PORTS_spi_init();
 
-	SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
-	SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
-	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
-	SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
-	SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;//SPI_CPOL_High;
-	SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
-	SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
-	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64;
-	SPI_InitStructure.SPI_CRCPolynomial = 7;
+	SPI1_HandleStructure.Instance = SPI1;
+	SPI1_HandleStructure.Init.Mode = SPI_MODE_MASTER;
+	SPI1_HandleStructure.Init.Direction = SPI_DIRECTION_2LINES;
+	SPI1_HandleStructure.Init.FirstBit = SPI_FIRSTBIT_MSB;
+	SPI1_HandleStructure.Init.DataSize = SPI_DATASIZE_8BIT;
+	SPI1_HandleStructure.Init.CLKPolarity = SPI_POLARITY_LOW;
+	SPI1_HandleStructure.Init.CLKPhase = SPI_PHASE_1EDGE;
+	SPI1_HandleStructure.Init.NSS = SPI_NSS_SOFT;
+	SPI1_HandleStructure.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
+	SPI1_HandleStructure.Init.TIMode = SPI_TIMODE_DISABLE;
+	SPI1_HandleStructure.Init.CRCCalculation = SPI_CRCCALCULATION_ENABLE;
+	SPI1_HandleStructure.Init.CRCPolynomial = 7;
+
+	//Copie de l'init dans SPI2
+	SPI2_HandleStructure.Instance = SPI2;
+	SPI2_HandleStructure.Init = SPI1_HandleStructure.Init;
 
 	#ifdef USE_SPI1
-		RCC_APB1PeriphClockCmd(SPI1_SPI_CLOCK, ENABLE);
-		SPI_Init(SPI1_SPI_HANDLE, &SPI_InitStructure);
-		SPI_Cmd(SPI1_SPI_HANDLE, ENABLE);
+		__HAL_RCC_SPI1_CLK_ENABLE();
+		__HAL_SPI_DISABLE(&SPI1_HandleStructure);
+		HAL_SPI_Init(&SPI1_HandleStructure);
+		__HAL_SPI_ENABLE(&SPI1_HandleStructure);
 	#endif /* def USE_SPI1 */
 
 	#ifdef USE_SPI2
-		RCC_APB1PeriphClockCmd(SPI2_SPI_CLOCK, ENABLE);
-		SPI_Init(SPI2_SPI_HANDLE, &SPI_InitStructure);
-		SPI_Cmd(SPI2_SPI_HANDLE, ENABLE);
+		__HAL_RCC_SPI2_CLK_ENABLE();
+		__HAL_SPI_DISABLE(&SPI2_HandleStructure);
+		HAL_SPI_Init(&SPI2_HandleStructure);
+		__HAL_SPI_ENABLE(&SPI2_HandleStructure);
 	#endif /* def USE_SPI2 */
 
 }
 
 #endif  /* defined(USE_SPI1) || defined(USE_SPI2) */
 
-#ifdef USE_SPI1
+//#ifdef USE_SPI1
 
 Uint8 SPI1_exchange(Uint8 c)
 {
+	Uint8 receiveChar;
 	if(!initialized){
 		error_printf("SPI non initialisé ! Appeller SPI_init\n");
 		return 0;
 	}
 
-	while(SPI_I2S_GetFlagStatus(SPI1_SPI_HANDLE, SPI_I2S_FLAG_TXE) == RESET);
+	while(__HAL_SPI_GET_FLAG(&SPI1_HandleStructure, SPI_FLAG_TXE) == RESET);
 
-	if(SPI_I2S_GetFlagStatus(SPI1_SPI_HANDLE, SPI_I2S_FLAG_RXNE) == SET)
+	if(__HAL_SPI_GET_FLAG(&SPI1_HandleStructure, SPI_FLAG_RXNE) == SET)
 	{
 		debug_printf("SPI1: buffer non vide \n");
-		SPI_I2S_ReceiveData(SPI1_SPI_HANDLE);
+		HAL_SPI_Receive(&SPI1_HandleStructure, &receiveChar, 1, 10);
 	}
 
-	SPI_I2S_SendData(SPI1_SPI_HANDLE, c);
-	while(SPI_I2S_GetFlagStatus(SPI1_SPI_HANDLE, SPI_I2S_FLAG_RXNE) == RESET);
+	HAL_SPI_Transmit(&SPI1_HandleStructure, &c, 1, 10);
+	while(__HAL_SPI_GET_FLAG(&SPI1_HandleStructure, SPI_FLAG_RXNE) == RESET);
 
 
 	//Test si erreur
-	if(SPI_I2S_GetFlagStatus(SPI1_SPI_HANDLE, SPI_I2S_FLAG_OVR) || SPI_I2S_GetFlagStatus(SPI1_SPI_HANDLE, SPI_FLAG_MODF))
+	if(__HAL_SPI_GET_FLAG(&SPI1_HandleStructure, SPI_FLAG_OVR) || __HAL_SPI_GET_FLAG(&SPI1_HandleStructure, SPI_FLAG_MODF))
 	{
 		debug_printf("SPI1: collision\n");
-		SPI_I2S_ClearFlag(SPI1_SPI_HANDLE, SPI_I2S_FLAG_OVR);
-		SPI_I2S_ClearFlag(SPI1_SPI_HANDLE, SPI_FLAG_MODF);
+		__HAL_SPI_CLEAR_OVRFLAG(&SPI1_HandleStructure);
+		__HAL_SPI_CLEAR_MODFFLAG(&SPI1_HandleStructure);
 	}
 
-	return SPI_I2S_ReceiveData(SPI1_SPI_HANDLE);
+	HAL_SPI_Receive(&SPI1_HandleStructure, &receiveChar, 1, 10);
+	return receiveChar;
 }
 
 void SPI1_write(Uint8 msg)
@@ -120,32 +131,34 @@ Uint8 SPI1_read()
 
 Uint8 SPI2_exchange(Uint8 c)
 {
+	Uint8 receiveChar;
 	if(!initialized){
 		error_printf("SPI non initialisé ! Appeller SPI_init\n");
 		return 0;
 	}
 
-	while(SPI_I2S_GetFlagStatus(SPI2_SPI_HANDLE, SPI_I2S_FLAG_TXE) == RESET);
+	while(__HAL_SPI_GET_FLAG(&SPI2_HandleStructure, SPI_FLAG_TXE) == RESET);
 
-	if(SPI_I2S_GetFlagStatus(SPI2_SPI_HANDLE, SPI_I2S_FLAG_RXNE) == SET)
+	if(__HAL_SPI_GET_FLAG(&SPI2_HandleStructure, SPI_FLAG_RXNE) == SET)
 	{
-		debug_printf("SPI2: buffer non vide \n");
-		SPI_I2S_ReceiveData(SPI2_SPI_HANDLE);
+		debug_printf("SPI1: buffer non vide \n");
+		HAL_SPI_Receive(&SPI2_HandleStructure, &receiveChar, 1, 10);
 	}
 
-	SPI_I2S_SendData(SPI2_SPI_HANDLE, c);
-	while(SPI_I2S_GetFlagStatus(SPI2_SPI_HANDLE, SPI_I2S_FLAG_RXNE) == RESET);
+	HAL_SPI_Transmit(&SPI2_HandleStructure, &c, 1, 10);
+	while(__HAL_SPI_GET_FLAG(&SPI2_HandleStructure, SPI_FLAG_RXNE) == RESET);
 
 
 	//Test si erreur
-	if(SPI_I2S_GetFlagStatus(SPI2_SPI_HANDLE, SPI_I2S_FLAG_OVR) || SPI_I2S_GetFlagStatus(SPI2_SPI_HANDLE, SPI_FLAG_MODF))
+	if(__HAL_SPI_GET_FLAG(&SPI2_HandleStructure, SPI_FLAG_OVR) || __HAL_SPI_GET_FLAG(&SPI2_HandleStructure, SPI_FLAG_MODF))
 	{
-		debug_printf("SPI2: collision\n");
-		SPI_I2S_ClearFlag(SPI2_SPI_HANDLE, SPI_I2S_FLAG_OVR);
-		SPI_I2S_ClearFlag(SPI2_SPI_HANDLE, SPI_FLAG_MODF);
+		debug_printf("SPI1: collision\n");
+		__HAL_SPI_CLEAR_OVRFLAG(&SPI2_HandleStructure);
+		__HAL_SPI_CLEAR_MODFFLAG(&SPI2_HandleStructure);
 	}
 
-	return SPI_I2S_ReceiveData(SPI2_SPI_HANDLE);
+	HAL_SPI_Receive(&SPI2_HandleStructure, &receiveChar, 1, 10);
+	return receiveChar;
 }
 
 void SPI2_write(Uint8 msg)
