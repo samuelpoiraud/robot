479215cf8c0a51e09086ad0850ae92457a8621cb
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-10-12 16:42:42 +0000

    QS_objects

---
+++ STM32/QS/QS_objects.c
@@ -0,0 +1,20 @@
+
+
+#include "QS_objects.h"
+
+
+OBJECTS_circles_t OBJECTS_circles;
+OBJECTS_polygons_t OBJECTS_polygons;
+
+
+void OBJECTS_init(){
+	// Définition des données
+
+}
+
+
+
+
+
+
+
---
+++ STM32/QS/QS_objects.h
@@ -0,0 +1,57 @@
+
+#include "QS_all.h"
+#include "QS_maths.h"
+
+#define NB_MAX_POLYGONS (20)
+#define NB_MAX_SUMMITS (10)
+
+#define NB_MAX_CIRCLES (20)
+
+typedef struct{
+	GEOMETRY_point_t summits[NB_MAX_SUMMITS];
+	Uint8 nb_summits;
+}OBJECTS_polygon_t;
+
+typedef struct{
+	OBJECTS_polygon_t polygons[NB_MAX_CIRCLES];
+	Uint8 nb_polygons;
+	bool_e enable[NB_MAX_CIRCLES];
+}OBJECTS_polygons_t;
+
+typedef struct{
+	GEOMETRY_circle_t circles[NB_MAX_POLYGONS];
+	Uint8 nb_circles;
+	bool_e enable[NB_MAX_POLYGONS];
+}OBJECTS_circles_t;
+
+typedef enum{
+	ROCKET_BLUE,
+	ROCKET_YELLOW,
+	ROCKET_POLYC_BS,
+	ROCKET_POLYC_YS,
+	MODULE_POLYC1_BS,
+	MODULE_POLYC2_BS,
+	MODULE_POLYC3_BS,
+	MODULE_POLYC1_YS,
+	MODULE_POLYC2_YS,
+	MODULE_POLYC3_YS,
+	MODULE_BLUE,
+	MODULE_YELLOW,
+	BIG_CRATER_BS,
+	BIG_CRATER_YS,
+	SMALL_CRATER_NORTH_BS,
+	SMALL_CRATER_SOUTH_BS,
+	SMALL_CRATER_NORTH_YS,
+	SMALL_CRATER_SOUTH_YS
+}OBJECTS_circles_name_e;
+
+typedef enum{
+	DEPOSE_BLUE,
+	DEPOSE_YELLOW,
+	DEPOSE_MIDDLE_L,
+	DEPOSE_MIDDLE_M,
+	DEPOSE_MIDDLE_R
+}OBJECTS_polygons_name_e;
+
+void OBJECTS_init();
+
