90a28f57a92448bdabebf100dc76d96be660ee46
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-11-03 21:01:04 +0000

    Ajout de l'image du club robot en RGB 565

---
+++ 6_IHM/CurrentSoftware/LCD/low layer/image.h
@@ -0,0 +1,16 @@
+/*
+ * image.h
+ *
+ *  Created on: 23 oct. 2016
+ *      Author: Arnaud
+ */
+
+#ifndef LCD_LOW_LAYER_IMAGE_H_
+#define LCD_LOW_LAYER_IMAGE_H_
+
+#include "../../QS/QS_all.h"
+
+extern const Uint16 logo[76800];
+
+
+#endif /* LCD_LOW_LAYER_IMAGE_H_ */
