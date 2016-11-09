90f299c94fbbdb57acadc859a8628ba5ceba9d3c
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-11-09 20:27:19 +0000

    Oubli de l'ajout des fichiers de strat_inutile

---
+++ 2_Strategie/CurrentSoftwareSTM32/strats_2017/inutile/guillaumeBe_strat_inutile.c
@@ -0,0 +1,41 @@
+#include "strat_inutile.h"
+#include "../../propulsion/movement.h"
+#include "../../QS/QS_stateMachineHelper.h"
+
+void guillaumeBe_strat_inutile_big(){
+	CREATE_MAE_WITH_VERBOSE(SM_ID_STRAT_HARRY_INUTILE,
+			INIT,
+			ERROR,
+			DONE
+		);
+
+	switch(state){
+		case INIT:
+			break;
+
+		case ERROR:
+			break;
+
+		case DONE:
+			break;
+	}
+}
+
+void guillaumeBe_strat_inutile_small(){
+	CREATE_MAE_WITH_VERBOSE(SM_ID_STRAT_ANNE_INUTILE,
+			INIT,
+			ERROR,
+			DONE
+		);
+
+	switch(state){
+		case INIT:
+			break;
+
+		case ERROR:
+			break;
+
+		case DONE:
+			break;
+	}
+}
