a44eba30d2649e5d451a3f9d7a97b394bda3e9d4
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2016-11-20 20:30:36 +0000

    Correction et complétion middleware LCD

--- 6_IHM/CurrentSoftware/LCD/middleware.h
+++ 6_IHM/CurrentSoftware/LCD/middleware.h
@@ -57,20 +57,25 @@
 	//---------------------Fonction Création------------------------//
 	//////////////////////////////////////////////////////////////////
 
-	objectId_t MIDDLEWARE_addText(Sint16 x, Sint16 y, char * text, Uint16 colorText, Uint32 colorBackground);
+	objectId_t MIDDLEWARE_addText(Sint16 x, Sint16 y, Uint16 colorText, Uint32 colorBackground, const char *text, ...)  __attribute__((format (printf, 5, 6)));
 
 	objectId_t MIDDLEWARE_addButtonImg(Sint16 x, Sint16 y, const imageInfo_s *imageNormal, const imageInfo_s *imageLock, bool_e lockTouch, bool_e * touch);
 
-	objectId_t MIDDLEWARE_addButton(Sint16 x, Sint16 y, Sint16 width, Sint16 height, char * text, bool_e lockTouch, bool_e *touch, Uint16 colorText, Uint16 colorButton, Uint16 colorButtonTouch);
+	objectId_t MIDDLEWARE_addButton(Sint16 x, Sint16 y, Uint16 width, Uint16 height, char * text, bool_e lockTouch, bool_e *touch, Uint16 colorText, Uint16 colorButton, Uint16 colorButtonTouch, Uint32 colorBorder);
 
-	objectId_t MIDDLEWARE_addProgressBar(Sint16 x, Sint16 y, Sint16 width, Sint16 height, objectOrientation_e orientation, Uint8 *value);
+	objectId_t MIDDLEWARE_addProgressBar(Sint16 x, Sint16 y, Uint16 width, Uint16 height, objectOrientation_e orientation, Uint8 *value);
 
-	objectId_t MIDDLEWARE_addSlider(Sint16 x, Sint16 y, Sint16 width, Sint16 height, Sint32 minValue, Sint32 maxValue, objectOrientation_e orientation, Sint32 *value);
+	objectId_t MIDDLEWARE_addSlider(Sint16 x, Sint16 y, Uint16 width, Uint16 height, Sint32 minValue, Sint32 maxValue, objectOrientation_e orientation, Sint32 *value);
 
 	objectId_t MIDDLEWARE_addImage(Sint16 x, Sint16 y, const imageInfo_s *image);
 
 	objectId_t MIDDLEWARE_addAnimatedImage(Sint16 x, Sint16 y, const animatedImageInfo_s *animatedImageInfo);
 
+	objectId_t MIDDLEWARE_addRectangle(Sint16 x, Sint16 y, Uint16 width, Uint16 height, Uint32 colorBorder, Uint32 colorCenter);
+
+	objectId_t MIDDLEWARE_addCircle(Sint16 x, Sint16 y, Uint16 r, Uint32 colorBorder, Uint32 colorCenter);
+
+	objectId_t MIDDLEWARE_addLine(Sint16 x0, Sint16 y0, Sint16 x1, Sint16 y1, Uint16 color);
 
 
 	//////////////////////////////////////////////////////////////////
