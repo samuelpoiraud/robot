c94fa30f34d1fff3696484ef9bd21b431adceaf4
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2017-01-11 18:10:00 +0000

    Optimisation du scan (commit en accord avec Corentin j'ai pas tout cassé sans prévenir :p)

--- 1_Propulsion/CurrentSoftware/scan/borders_scan.c
+++ 1_Propulsion/CurrentSoftware/scan/borders_scan.c
@@ -30,15 +30,16 @@ static volatile GEOMETRY_point_t yellow_south[NB_DATA_POINTS];
 static volatile Uint8 yellow_south_index;
 static volatile bool_e yellow_south_enable;
 
+typedef enum{
+	BORDER_SCAN_MODE_DEN_X = 0,
+	BORDER_SCAN_MODE_DEN_Y,
+}border_scan_mode_den_e;
 
+static void addPointZone(volatile GEOMETRY_point_t zone[], volatile Uint8 *zone_index, volatile bool_e *zone_enable, scan_data_t tab[], Uint32 indexAdd);
+static void calculeZone(volatile GEOMETRY_point_t zone[], volatile Uint8 *zone_index, volatile bool_e zone_enable, Sint16 * angle_zone, Sint16 * moy_border, border_scan_mode_den_e mode_den);
 
 void BORDERS_SCAN_treatment(scan_data_t tab[]){
-    Uint32 index,i,j;
-    Sint64 xmoy=0;
-    Sint64 ymoy=0;
-    Sint64 num=0;
-    Sint64 den=0;
-    double pente=0;
+    Uint32 index;
     Sint16 angle_middle_north=0;
     Sint16 angle_blue_start=0;
     Sint16 angle_blue_corner=0;
@@ -58,302 +59,84 @@ void BORDERS_SCAN_treatment(scan_data_t tab[]){
 
     for(index=0;index<NB_SCAN_DATA;index++){
         if ((tab[index].pos_mesure.x<20)&&(tab[index].pos_mesure.y<1750)&&(tab[index].pos_mesure.y>1250)){
-            middle_north[middle_north_index]=tab[index].pos_mesure;
-            middle_north_index++;
-            if(middle_north_index==NB_DATA_POINTS){
-                middle_north_index=0;
-                middle_north_enable=TRUE;
-            }
-            if((middle_north_index%NB_POINTS_ANALYSYS)==0){
-                if(middle_north_enable){
-                    xmoy=0;
-                    ymoy=0;
-                    num=0;
-                    den=0;
-                    for(i=0;i<NB_DATA_POINTS;i++){
-                        xmoy+=middle_north[i].x;
-                        ymoy+=middle_north[i].y;
-                    }
-                    xmoy=xmoy/NB_DATA_POINTS;
-                    ymoy=ymoy/NB_DATA_POINTS;
-                    for(i=0;i<NB_DATA_POINTS;i++){
-                        num+=(middle_north[i].x-xmoy)*(middle_north[i].y-ymoy);
-                        den+=(middle_north[i].y-ymoy)*(middle_north[i].y-ymoy); //attention on inverse pour ne pas avoir une pente dementielle
-                    }
-                    pente=num/den;
-                    angle_middle_north=atan4096(pente);
-                    xmoy_middle_north=xmoy;
-                }
-                else{
-                    for(i=0;i<middle_north_index;i++){
-                        xmoy+=middle_north[i].x;
-                        ymoy+=middle_north[i].y;
-                    }
-                    xmoy=xmoy/middle_north_index;
-                    ymoy=ymoy/middle_north_index;
-                    for(i=0;i<middle_north_index;i++){
-                        num+=(middle_north[i].x-xmoy)*(middle_north[i].y-ymoy);
-                        den+=(middle_north[i].y-ymoy)*(middle_north[i].y-ymoy);//attention inversion
-                    }
-                    pente=num/den;
-                    angle_middle_north=atan4096(pente);
-                    xmoy_middle_north=xmoy;
-                }
-            }
+        	addPointZone(middle_north, &middle_north_index, &middle_north_enable, tab, index);
+            calculeZone(middle_north, &middle_north_index, middle_north_enable, &angle_middle_north, &xmoy_middle_north, BORDER_SCAN_MODE_DEN_X);
         }
         else if ((tab[index].pos_mesure.y<20)&&(tab[index].pos_mesure.x<650)&&(tab[index].pos_mesure.x>400)){
-            blue_start[blue_start_index]=tab[index].pos_mesure;
-            blue_start_index++;
-            if(blue_start_index==NB_DATA_POINTS){
-                blue_start_index=0;
-                blue_start_enable=TRUE;
-            }
-            if((blue_start_index%NB_POINTS_ANALYSYS)==0){
-                if(blue_start_enable){
-                    xmoy=0;
-                    ymoy=0;
-                    num=0;
-                    den=0;
-                    for(i=0;i<NB_DATA_POINTS;i++){
-                        xmoy+=blue_start[i].x;
-                        ymoy+=blue_start[i].y;
-                    }
-                    xmoy=xmoy/NB_DATA_POINTS;
-                    ymoy=ymoy/NB_DATA_POINTS;
-                    for(i=0;i<NB_DATA_POINTS;i++){
-                        num+=(blue_start[i].x-xmoy)*(blue_start[i].y-ymoy);
-                        den+=(blue_start[i].x-xmoy)*(blue_start[i].x-xmoy);
-                    }
-                    angle_blue_start=atan4096(num/den);
-                    ymoy_blue_start=ymoy;
-                }
-                else{
-                    for(i=0;i<blue_start_index;i++){
-                        xmoy+=blue_start[i].x;
-                        ymoy+=blue_start[i].y;
-                    }
-                    xmoy=xmoy/blue_start_index;
-                    ymoy=ymoy/blue_start_index;
-                    for(i=0;i<blue_start_index;i++){
-                        num+=(blue_start[i].x-xmoy)*(blue_start[i].y-ymoy);
-                        den+=(blue_start[i].x-xmoy)*(blue_start[i].x-xmoy);
-                    }
-                    angle_blue_start=atan4096(num/den);
-                    ymoy_blue_start=ymoy;
-                }
-            }
+        	addPointZone(blue_start, &blue_start_index, &blue_start_enable, tab, index);
+            calculeZone(blue_start, &blue_start_index, blue_start_enable, &angle_blue_start, &ymoy_blue_start, BORDER_SCAN_MODE_DEN_Y);
         }
         else if ((tab[index].pos_mesure.y<20)&&(tab[index].pos_mesure.x<1980)&&(tab[index].pos_mesure.x>1450)){
-            blue_corner[blue_corner_index]=tab[index].pos_mesure;
-            blue_corner_index++;
-            if(blue_corner_index==NB_DATA_POINTS){
-                blue_corner_index=0;
-                blue_corner_enable=TRUE;
-            }
-            if((blue_corner_index%NB_POINTS_ANALYSYS)==0){
-                if(blue_corner_enable){
-                    xmoy=0;
-                    ymoy=0;
-                    num=0;
-                    den=0;
-                    for(i=0;i<NB_DATA_POINTS;i++){
-                        xmoy+=blue_corner[i].x;
-                        ymoy+=blue_corner[i].y;
-                    }
-                    xmoy=xmoy/NB_DATA_POINTS;
-                    ymoy=ymoy/NB_DATA_POINTS;
-                    for(i=0;i<NB_DATA_POINTS;i++){
-                        num+=(blue_corner[i].x-xmoy)*(blue_corner[i].y-ymoy);
-                        den+=(blue_corner[i].x-xmoy)*(blue_corner[i].x-xmoy);
-                    }
-                    angle_blue_corner=atan4096(num/den);
-                    ymoy_blue_corner=ymoy;
-                }
-                else{
-                    for(i=0;i<blue_corner_index;i++){
-                        xmoy+=blue_corner[i].x;
-                        ymoy+=blue_corner[i].y;
-                    }
-                    xmoy=xmoy/blue_corner_index;
-                    ymoy=ymoy/blue_corner_index;
-                    for(i=0;i<blue_corner_index;i++){
-                        num+=(blue_corner[i].x-xmoy)*(blue_corner[i].y-ymoy);
-                        den+=(blue_corner[i].x-xmoy)*(blue_corner[i].x-xmoy);
-                    }
-                    angle_blue_corner=atan4096(num/den);
-                    ymoy_blue_corner=ymoy;
-                }
-            }
+        	addPointZone(blue_corner, &blue_corner_index, &blue_corner_enable, tab, index);
+            calculeZone(blue_corner, &blue_corner_index, blue_corner_enable, &angle_blue_corner, &ymoy_blue_corner, BORDER_SCAN_MODE_DEN_Y);
         }
         else if ((tab[index].pos_mesure.y>2980)&&(tab[index].pos_mesure.x<650)&&(tab[index].pos_mesure.x>400)){
-            yellow_start[yellow_start_index]=tab[index].pos_mesure;
-            yellow_start_index++;
-            if(yellow_start_index==NB_DATA_POINTS){
-                yellow_start_index=0;
-                yellow_start_enable=TRUE;
-            }
-            if((yellow_start_index%NB_POINTS_ANALYSYS)==0){
-                if(yellow_start_enable){
-                    xmoy=0;
-                    ymoy=0;
-                    num=0;
-                    den=0;
-                    for(i=0;i<NB_DATA_POINTS;i++){
-                        xmoy+=yellow_start[i].x;
-                        ymoy+=yellow_start[i].y;
-                    }
-                    xmoy=xmoy/NB_DATA_POINTS;
-                    ymoy=ymoy/NB_DATA_POINTS;
-                    for(i=0;i<NB_DATA_POINTS;i++){
-                        num+=(yellow_start[i].x-xmoy)*(yellow_start[i].y-ymoy);
-                        den+=(yellow_start[i].x-xmoy)*(yellow_start[i].x-xmoy);
-                    }
-                    angle_yellow_start=atan4096(num/den);
-                    ymoy_yellow_start=ymoy;
-                }
-                else{
-                    for(i=0;i<yellow_start_index;i++){
-                        xmoy+=yellow_start[i].x;
-                        ymoy+=yellow_start[i].y;
-                    }
-                    xmoy=xmoy/yellow_start_index;
-                    ymoy=ymoy/yellow_start_index;
-                    for(i=0;i<yellow_start_index;i++){
-                        num+=(yellow_start[i].x-xmoy)*(yellow_start[i].y-ymoy);
-                        den+=(yellow_start[i].x-xmoy)*(yellow_start[i].x-xmoy);
-                    }
-                    angle_yellow_start=atan4096(num/den);
-                    ymoy_yellow_start=ymoy;
-                }
-            }
+        	addPointZone(yellow_start, &yellow_start_index, &yellow_start_enable, tab, index);
+            calculeZone(yellow_start, &yellow_start_index, yellow_start_enable, &angle_yellow_start, &ymoy_yellow_start, BORDER_SCAN_MODE_DEN_Y);
         }
         else if ((tab[index].pos_mesure.y>2980)&&(tab[index].pos_mesure.x<1980)&&(tab[index].pos_mesure.x>1450)){
-            yellow_corner[yellow_corner_index]=tab[index].pos_mesure;
-            yellow_corner_index++;
-            if(yellow_corner_index==NB_DATA_POINTS){
-                yellow_corner_index=0;
-                yellow_corner_enable=TRUE;
-            }
-            if((yellow_corner_index%NB_POINTS_ANALYSYS)==0){
-                if(yellow_corner_enable){
-                    xmoy=0;
-                    ymoy=0;
-                    num=0;
-                    den=0;
-                    for(i=0;i<NB_DATA_POINTS;i++){
-                        xmoy+=yellow_corner[i].x;
-                        ymoy+=yellow_corner[i].y;
-                    }
-                    xmoy=xmoy/NB_DATA_POINTS;
-                    ymoy=ymoy/NB_DATA_POINTS;
-                    for(i=0;i<NB_DATA_POINTS;i++){
-                        num+=(yellow_corner[i].x-xmoy)*(yellow_corner[i].y-ymoy);
-                        den+=(yellow_corner[i].x-xmoy)*(yellow_corner[i].x-xmoy);
-                    }
-                    angle_yellow_corner=atan4096(num/den);
-                    ymoy_yellow_corner=ymoy;
-                }
-                else{
-                    for(i=0;i<yellow_corner_index;i++){
-                        xmoy+=yellow_corner[i].x;
-                        ymoy+=yellow_corner[i].y;
-                    }
-                    xmoy=xmoy/yellow_corner_index;
-                    ymoy=ymoy/yellow_corner_index;
-                    for(i=0;i<yellow_corner_index;i++){
-                        num+=(yellow_corner[i].x-xmoy)*(yellow_corner[i].y-ymoy);
-                        den+=(yellow_corner[i].x-xmoy)*(yellow_corner[i].x-xmoy);
-                    }
-                    angle_yellow_corner=atan4096(num/den);
-                    ymoy_yellow_corner=ymoy;
-                }
-            }
+        	addPointZone(yellow_corner, &yellow_corner_index, &yellow_corner_enable, tab, index);
+            calculeZone(yellow_corner, &yellow_corner_index, yellow_corner_enable, &angle_yellow_corner, &ymoy_yellow_corner, BORDER_SCAN_MODE_DEN_Y);
         }
         else if ((tab[index].pos_mesure.x>1980)&&(tab[index].pos_mesure.y<1250)&&(tab[index].pos_mesure.y>20)){
-            blue_south[blue_south_index]=tab[index].pos_mesure;
-            blue_south_index++;
-            if(blue_south_index==NB_DATA_POINTS){
-                blue_south_index=0;
-                blue_south_enable=TRUE;
-            }
-            if((blue_south_index%NB_POINTS_ANALYSYS)==0){
-                if(blue_south_enable){
-                    xmoy=0;
-                    ymoy=0;
-                    num=0;
-                    den=0;
-                    for(i=0;i<NB_DATA_POINTS;i++){
-                        xmoy+=blue_south[i].x;
-                        ymoy+=blue_south[i].y;
-                    }
-                    xmoy=xmoy/NB_DATA_POINTS;
-                    ymoy=ymoy/NB_DATA_POINTS;
-                    for(i=0;i<NB_DATA_POINTS;i++){
-                        num+=(blue_south[i].x-xmoy)*(blue_south[i].y-ymoy);
-                        den+=(blue_south[i].y-ymoy)*(blue_south[i].y-ymoy); //attention inversion
-                    }
-                    angle_blue_south=atan4096(num/den);
-                    xmoy_blue_south=xmoy;
-                }
-                else{
-                    for(i=0;i<blue_south_index;i++){
-                        xmoy+=blue_south[i].x;
-                        ymoy+=blue_south[i].y;
-                    }
-                    xmoy=xmoy/blue_south_index;
-                    ymoy=ymoy/blue_south_index;
-                    for(i=0;i<blue_south_index;i++){
-                        num+=(blue_south[i].x-xmoy)*(blue_south[i].y-ymoy);
-                        den+=(blue_south[i].y-ymoy)*(blue_south[i].y-ymoy);//attention inversion
-                    }
-                    angle_blue_south=atan4096(num/den);
-                    xmoy_blue_south=xmoy;
-                }
-            }
+        	addPointZone(blue_south, &blue_south_index, &blue_south_enable, tab, index);
+        	calculeZone(blue_south, &blue_south_index, blue_south_enable, &angle_blue_south, &xmoy_blue_south, BORDER_SCAN_MODE_DEN_X);
         }
         else if ((tab[index].pos_mesure.x>1980)&&(tab[index].pos_mesure.y<2980)&&(tab[index].pos_mesure.y>1750)){
-            yellow_south[yellow_south_index]=tab[index].pos_mesure;
-            yellow_south_index=(yellow_south_index+1)%NB_DATA_POINTS;
-            yellow_south_index++;
-            if(yellow_south_index==NB_DATA_POINTS){
-                yellow_south_index=0;
-                yellow_south_enable=TRUE;
-            }
-            if((yellow_south_index%NB_POINTS_ANALYSYS)==0){
-                if(yellow_south_enable){
-                    xmoy=0;
-                    ymoy=0;
-                    num=0;
-                    den=0;
-                    for(i=0;i<NB_DATA_POINTS;i++){
-                        xmoy+=yellow_south[i].x;
-                        ymoy+=yellow_south[i].y;
-                    }
-                    xmoy=xmoy/NB_DATA_POINTS;
-                    ymoy=ymoy/NB_DATA_POINTS;
-                    for(i=0;i<NB_DATA_POINTS;i++){
-                        num+=(yellow_south[i].x-xmoy)*(yellow_south[i].y-ymoy);
-                        den+=(yellow_south[i].y-ymoy)*(yellow_south[i].y-ymoy);//attention inversion
-                    }
-                    angle_yellow_south=atan4096(num/den);
-                    xmoy_yellow_south=xmoy;
-                }
-                else{
-                    for(i=0;i<yellow_south_index;i++){
-                        xmoy+=yellow_south[i].x;
-                        ymoy+=yellow_south[i].y;
-                    }
-                    xmoy=xmoy/yellow_south_index;
-                    ymoy=ymoy/yellow_south_index;
-                    for(i=0;i<yellow_south_index;i++){
-                        num+=(yellow_south[i].x-xmoy)*(yellow_south[i].y-ymoy);
-                        den+=(yellow_south[i].y-ymoy)*(yellow_south[i].y-ymoy);//attention inversion
-                    }
-                    angle_yellow_south=atan4096(num/den);
-                    xmoy_yellow_south=xmoy;
-                }
-            }
+        	addPointZone(yellow_south, &yellow_south_index, &yellow_south_enable, tab, index);
+        	calculeZone(yellow_south, &yellow_south_index, yellow_south_enable, &angle_yellow_south, &xmoy_yellow_south, BORDER_SCAN_MODE_DEN_X);
         }
     }
 }
 
+static void addPointZone(volatile GEOMETRY_point_t zone[], volatile Uint8 *zone_index, volatile bool_e *zone_enable, scan_data_t tab[], Uint32 indexAdd){
+	zone[(*zone_index)]=tab[indexAdd].pos_mesure;
+	(*zone_index)++;
+	if((*zone_index) == NB_DATA_POINTS){
+		*zone_index=0;
+		*zone_enable=TRUE;
+	}
+}
+
+static void calculeZone(volatile GEOMETRY_point_t zone[], volatile Uint8 *zone_index, volatile bool_e zone_enable, Sint16 * angle_zone, Sint16 * moy_border, border_scan_mode_den_e mode_den){
+    Sint64 xmoy=0, ymoy=0, num=0, den=0;
+    Uint32 i;
+
+	if(((*zone_index)%NB_POINTS_ANALYSYS)==0){
+		if(zone_enable){
+			for(i=0;i<NB_DATA_POINTS;i++){
+				xmoy+=zone[i].x;
+				ymoy+=zone[i].y;
+			}
+			xmoy=xmoy/NB_DATA_POINTS;
+			ymoy=ymoy/NB_DATA_POINTS;
+			for(i=0;i<NB_DATA_POINTS;i++){
+				num+=(zone[i].x-xmoy)*(zone[i].y-ymoy);
+				if(mode_den == BORDER_SCAN_MODE_DEN_X)
+					den+=(zone[i].x-xmoy)*(zone[i].x-xmoy);
+				else
+                    den+=(zone[i].y-ymoy)*(zone[i].y-ymoy);
+
+			}
+			*angle_zone=atan4096(num/den);
+		}
+		else{
+			for(i=0;i<(*zone_index);i++){
+				xmoy+=zone[i].x;
+				ymoy+=zone[i].y;
+			}
+			xmoy=xmoy/(*zone_index);
+			ymoy=ymoy/(*zone_index);
+			for(i=0;i<(*zone_index);i++){
+				num+=(zone[i].x-xmoy)*(zone[i].y-ymoy);
+				den+=(zone[i].x-xmoy)*(zone[i].x-xmoy);
+			}
+			*angle_zone=atan4096(((float)num)/((float)den));
+			if(mode_den == BORDER_SCAN_MODE_DEN_X)
+				*moy_border = xmoy;
+			else
+				*moy_border = ymoy;
+
+		}
+	}
+}
