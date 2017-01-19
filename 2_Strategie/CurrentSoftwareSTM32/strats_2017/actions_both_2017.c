bde89a20493d3d0941e45b76e07065c6f2492cf6
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2017-01-19 18:47:53 +0000

    Ajout de la subAction pour traverser la bascule

--- 2_Strategie/CurrentSoftwareSTM32/strats_2017/actions_both_2017.c
+++ 2_Strategie/CurrentSoftwareSTM32/strats_2017/actions_both_2017.c
@@ -11,4 +11,56 @@
 
 
 #include "actions_both_2017.h"
+#include "../QS/QS_stateMachineHelper.h"
+#include "../propulsion/prop_functions.h"
+#include "../propulsion/movement.h"
+#include "../utils/generic_functions.h"
 
+error_e sub_cross_rocker(void){
+	CREATE_MAE_WITH_VERBOSE(SM_ID_SUB_CROSS_ROCKER,
+			INIT,
+			CROSS,
+			CORRECT_ODOMETRY_MATH,
+			CORRECT_ODOMETRY_MEASURE,
+			ERROR,
+			DONE
+		);
+
+	switch (state) {
+		case INIT:
+			if(i_am_in_square_color(0, 350, 0, 400))
+				state = CROSS;
+			else
+				state = ERROR;
+			break;
+
+		case CROSS:
+			state = try_going(175, COLOR_Y(950), state, CORRECT_ODOMETRY_MATH, ERROR, SLOW, ANY_WAY, NO_DODGE_AND_WAIT, END_AT_LAST_POINT);
+			break;
+
+		case CORRECT_ODOMETRY_MATH:{
+			Sint16 diffY = COLOR_EXP(-10, 10);
+
+			PROP_set_position(global.pos.x, global.pos.y + diffY, global.pos.angle);
+
+			state = CORRECT_ODOMETRY_MEASURE;
+			}break;
+
+		case CORRECT_ODOMETRY_MEASURE:
+
+			// COCO ON T'ATTEND !
+
+			state = DONE;
+			break;
+
+		case DONE:
+			RESET_MAE();
+			return END_OK;
+
+		case ERROR:
+			RESET_MAE();
+			return NOT_HANDLED;
+	}
+
+	return IN_PROGRESS;
+}
