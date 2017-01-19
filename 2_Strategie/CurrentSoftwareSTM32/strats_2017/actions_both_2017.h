50331d1ea9b3abf576d5f3a693672c2d1cf9939f
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2017-01-19 18:02:24 +0000

    Ajout stratégie commune 2017

---
+++ 2_Strategie/CurrentSoftwareSTM32/strats_2017/actions_both_2017.h
@@ -0,0 +1,20 @@
+/*
+ *	Club Robot ESEO 2014-2015
+ *	Holly & Wood
+ *
+ *	Fichier : actions_both_2015.h
+ *	Package : Carte S²/strats2015
+ *	Description : Test des actions réalisables par le robot
+ *	Auteur : Arnaud
+ *	Version 2013/10/03
+ */
+
+
+#include "../QS/QS_all.h"
+
+#ifndef ACTIONS_BOTH_2017_H
+#define ACTIONS_BOTH_2017_H
+
+
+#endif
+
