03dd5dbbb984b89caa89696e7182183287b2d73e
aguilmet <aguilmet@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2017-02-15 09:06:12 +0000

    Finition de la QS_ads1118 Correction QS_SPI

--- STM32/QS/QS_ads1118.c
+++ STM32/QS/QS_ads1118.c
@@ -10,8 +10,6 @@
 #include "QS_ads1118.h"
 #include "QS_spi.h"
 
-#if 0
-
 typedef union{
 	Uint16 raw;
 	struct{
@@ -35,14 +33,11 @@ typedef union{
 
 }ADS1118_config_reg_s;
 
-static bool_e ADS1118_writeSPI();
-static bool_e ADS1118_readSPI();
-
 void ADS1118_init(void){
 	SPI_init();
 }
 
-bool_e ADS1118_setConfig(ADS1118_sensorConfig_t sensorConfig,
+bool_e ADS1118_setConfig(ADS1118_sensorConfig_t * sensorConfig,
 								ADS1118_CONFIG_singleShotStart singleShotStart,
 								ADS1118_CONFIG_inputMultiplexer inputMultiplexer,
 								ADS1118_CONFIG_gainAmplifier gainAmplifier,
@@ -51,10 +46,37 @@ bool_e ADS1118_setConfig(ADS1118_sensorConfig_t sensorConfig,
 								ADS1118_CONFIG_temperatureSensorMode temperatureSensorMode,
 								ADS1118_CONFIG_pullup pullup){
 
+	assert(sensorConfig != NULL);
 
+	ADS1118_config_reg_s configReg;
+
+	configReg.singleShotStart = singleShotStart;
+	configReg.inputMultiplexer = inputMultiplexer;
+	configReg.gainAmplifier = gainAmplifier;
+	configReg.operatingMode = operatingMode;
+	configReg.dataRate = dataRate;
+	configReg.temperatureSensorMode = temperatureSensorMode;
+	configReg.pullup;
+
+	GPIO_SetBits(sensorConfig->GPIO_cs.GPIOx, sensorConfig->GPIO_cs.GPIO_Pin);
+
+	if(ADS1118_isAvailable(sensorConfig))
+		return FALSE;
+
+	SPI_setDataSize(sensorConfig->SPI_handle, SPI_DATA_SIZE_16_BIT);
+
+	SPI_read(sensorConfig->SPI_handle);
+
+	SPI_write(sensorConfig->SPI_handle, configReg.raw);
+
+	SPI_setDataSize(sensorConfig->SPI_handle, SPI_DATA_SIZE_8_BIT);
+
+	GPIO_ResetBits(sensorConfig->GPIO_cs.GPIOx, sensorConfig->GPIO_cs.GPIO_Pin);
+
+	return TRUE;
 }
 
-bool_e ADS1118_getConfig(ADS1118_sensorConfig_t sensorConfig,
+bool_e ADS1118_getConfig(ADS1118_sensorConfig_t * sensorConfig,
 						ADS1118_CONFIG_singleShotStart * singleShotStart,
 						ADS1118_CONFIG_inputMultiplexer * inputMultiplexer,
 						ADS1118_CONFIG_gainAmplifier * gainAmplifier,
@@ -63,19 +85,73 @@ bool_e ADS1118_getConfig(ADS1118_sensorConfig_t sensorConfig,
 						ADS1118_CONFIG_temperatureSensorMode * temperatureSensorMode,
 						ADS1118_CONFIG_pullup * pullup){
 
+	assert(sensorConfig != NULL);
 
+	ADS1118_config_reg_s configReg;
+
+	GPIO_SetBits(sensorConfig->GPIO_cs.GPIOx, sensorConfig->GPIO_cs.GPIO_Pin);
+
+	if(ADS1118_isAvailable(sensorConfig))
+		return FALSE;
+
+	SPI_setDataSize(sensorConfig->SPI_handle, SPI_DATA_SIZE_16_BIT);
+
+	SPI_read(sensorConfig->SPI_handle);
+
+	configReg.raw = SPI_read(sensorConfig->SPI_handle);
+
+	SPI_setDataSize(sensorConfig->SPI_handle, SPI_DATA_SIZE_8_BIT);
+
+	GPIO_ResetBits(sensorConfig->GPIO_cs.GPIOx, sensorConfig->GPIO_cs.GPIO_Pin);
+
+	if(singleShotStart != NULL)
+		*singleShotStart = configReg.singleShotStart;
+
+	if(inputMultiplexer != NULL)
+		*inputMultiplexer = configReg.inputMultiplexer;
+
+	if(gainAmplifier != NULL)
+		*gainAmplifier = configReg.gainAmplifier;
+
+	if(operatingMode != NULL)
+		*operatingMode = configReg.operatingMode;
+
+	if(dataRate != NULL)
+		*dataRate = configReg.dataRate;
+
+	if(temperatureSensorMode != NULL)
+		*temperatureSensorMode = configReg.temperatureSensorMode;
+
+	if(pullup != NULL)
+		*pullup = configReg.pullup;
+
+	return TRUE;
 }
 
-bool_e ADS1118_getValue(Uint16 * value){
+bool_e ADS1118_getValue(ADS1118_sensorConfig_t * sensorConfig, Uint16 * value){
+	assert(sensorConfig != NULL);
+	assert(value != NULL);
 
+	GPIO_SetBits(sensorConfig->GPIO_cs.GPIOx, sensorConfig->GPIO_cs.GPIO_Pin);
+
+	if(ADS1118_isAvailable(sensorConfig))
+		return FALSE;
+
+	SPI_setDataSize(sensorConfig->SPI_handle, SPI_DATA_SIZE_16_BIT);
+
+	*value = SPI_read(sensorConfig->SPI_handle);
+
+	SPI_setDataSize(sensorConfig->SPI_handle, SPI_DATA_SIZE_8_BIT);
+
+	GPIO_ResetBits(sensorConfig->GPIO_cs.GPIOx, sensorConfig->GPIO_cs.GPIO_Pin);
+
+	return TRUE;
 }
 
-static bool_e ADS1118_writeSPI(){
-
+bool_e ADS1118_isAvailable(ADS1118_sensorConfig_t * sensorConfig){
+	assert(sensorConfig != NULL);
+	if(GPIO_ReadInputDataBit(sensorConfig->GPIO_din.GPIOx, sensorConfig->GPIO_din.GPIO_Pin))
+		return FALSE;
+	else
+		return TRUE;
 }
-
-static bool_e ADS1118_readSPI(){
-
-}
-
-#endif
