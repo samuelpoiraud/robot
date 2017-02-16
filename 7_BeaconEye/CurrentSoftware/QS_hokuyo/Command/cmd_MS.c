c36ff06f132c29b68455e8dec24d1bea256cb56d
gberland <gberland@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2017-02-16 17:53:55 +0000

    [IN PROGRESS] Vérification du checksum pour les trames MS

--- 7_BeaconEye/CurrentSoftware/QS_hokuyo/Command/cmd_MS.c
+++ 7_BeaconEye/CurrentSoftware/QS_hokuyo/Command/cmd_MS.c
@@ -5,22 +5,35 @@
  *      Author: guill
  */
 
+//TODO : Vérifier si l'on enlève les 5 premiers degrés et derniers
 
 #include "cmd_MS.h"
 #include "../QS_hokuyo.h"
 #include <string.h>
 #include "../../QS/QS_outputlog.h"
 #include "../usb_vcp_cdc.h"
+#include "../../QS/QS_maths.h"
+#include "../../environment.h"
+#ifdef USE_BEACON_EYE
+	#include "../IHM/terminal.h"
+	#define Terminal_debug(x)	 TERMINAL_puts(x)
+#else
+	#define Terminal_debug(x)	 (void)0
+#endif
 
 #define NB_BYTES_FROM_HOKUYO_CMD_MS		2500
 #define ECHO_MS_SIZE_MIN				21
 
-static Uint8 CMD_MS_datas[NB_BYTES_FROM_HOKUYO_CMD_MS];			//Données brutes issues du capteur HOKUYO
-static Uint32 CMD_MS_index = 0;									//Index pour ces données
+static Uint8 CMD_MS_datas[NB_BYTES_FROM_HOKUYO_CMD_MS];			// Données brutes issues du capteur HOKUYO
+static Uint32 CMD_MS_index = 0;									// Index pour ces données
+static HOKUYO_adversary_position detectedPoints[NB_STEP_MAX];	// Points détectés par l'hokuyo
+static Uint16 nbDetectedPoints = 0;
 
 static commandEchoStatus_e CMD_MS_readBufferEcho(hokuyoSendCommand_s *cmd);
+static commandEchoStatus_e CMD_MS_readBufferData(hokuyoSendCommand_s *cmd);
 static void CMD_MS_generateString(hokuyoSendCommand_s *cmd);
 static void CMD_MS_setNumberOfScans(hokuyoSendCommand_s *cmd, Uint8 numberOfScans);
+static void CMD_MS_computeDataBlock2C(hokuyoSendCommand_s *cmd, Uint8 *dataBlock, Uint8 length, Sint16 angle);
 
 hokuyoSendCommand_s CMD_MS_create(Uint16 startingStep,
 		                          Uint16 endStep,
@@ -83,6 +96,7 @@ HOKUYO_commandStatus_e CMD_MS_sendCommand(hokuyoSendCommand_s *cmd) {
 
 		case SEND_COMMAND:
 			HOKUYO_writeCommand(cmd->cmdStr);
+			debug_printf("Commande envoyée\n");
 			state = WAIT_ECHO;
 			break;
 
@@ -90,8 +104,35 @@ HOKUYO_commandStatus_e CMD_MS_sendCommand(hokuyoSendCommand_s *cmd) {
 
 			switch(CMD_MS_readBufferEcho(cmd)) {
 				case CMD_ECHO_DONE:
+					debug_printf("Echo recu\n");
 					state = READ_DATA;
 					break;
+				case CMD_ECHO_TIMEOUT:
+					debug_printf("Timeout echo\n");
+					Terminal_debug("ReadEcho : timeout");
+					state = TIMEOUT;
+					break;
+				case CMD_ECHO_ERROR:
+					debug_printf("Error echo\n");
+					Terminal_debug("ReadEcho : error");
+					state = ERROR;
+					break;
+				case CMD_ECHO_IN_PROGRESS:
+				default:
+					break;
+			}
+
+			break;
+
+		case READ_DATA:
+
+			switch(CMD_MS_readBufferData(cmd)) {
+				case CMD_ECHO_DONE:{
+					char strNb[255];
+					sprintf(strNb, "Points detectes : %4d", nbDetectedPoints);
+					Terminal_debug(strNb);
+					state = DONE;
+				}break;
 				case CMD_ECHO_TIMEOUT:
 					state = TIMEOUT;
 					break;
@@ -105,18 +146,20 @@ HOKUYO_commandStatus_e CMD_MS_sendCommand(hokuyoSendCommand_s *cmd) {
 
 			break;
 
-		case READ_DATA:
-			return HOKUYO_CMD_DONE;
-			break;
-
 		case TIMEOUT:
+			state = INIT;
 			return HOKUYO_CMD_TIMEOUT;
 			break;
 
 		case ERROR:
+			state = INIT;
 			return HOKUYO_CMD_ERROR;
 			break;
 
+		case DONE:
+			state = INIT;
+			return HOKUYO_CMD_DONE;
+			break;
 		default:
 			break;
 
@@ -147,10 +190,12 @@ static commandEchoStatus_e CMD_MS_readBufferEcho(hokuyoSendCommand_s *cmd) {
 	static bool_e isInit = FALSE;
 	Uint8 data;
 
+	static state_e state = CMD_ACK, last_state = CMD_ACK;
+	bool_e entrance;
+
 	while(!VCP_isRxEmpty()) {
 
-		static state_e state = CMD_ACK, last_state = ERROR;
-		bool_e entrance;
+
 
 		if((state == CMD_ACK && last_state == CMD_ACK) || state != last_state)
 			entrance = TRUE;
@@ -160,6 +205,8 @@ static commandEchoStatus_e CMD_MS_readBufferEcho(hokuyoSendCommand_s *cmd) {
 		last_state = state;
 
 		data = VCP_read();
+
+		debug_printf("%c", data);
 
 		CMD_MS_datas[CMD_MS_index++] = data;
 
@@ -267,6 +314,7 @@ static commandEchoStatus_e CMD_MS_readBufferEcho(hokuyoSendCommand_s *cmd) {
 						if(previous_data != HOKUYO_convertChecksum(checksum)) {
 							error_printf("Hokuyo Erreur : checksum timeStamp\n");
 						}
+						state = CMD_ACK;
 						return CMD_ECHO_DONE;
 					}
 				}
@@ -274,6 +322,7 @@ static commandEchoStatus_e CMD_MS_readBufferEcho(hokuyoSendCommand_s *cmd) {
 
 			case ERROR:
 				if(data == LINE_FEED && previous_data == LINE_FEED) {
+					state = CMD_ACK;
 					return CMD_ECHO_ERROR;
 				}
 				break;
@@ -282,6 +331,8 @@ static commandEchoStatus_e CMD_MS_readBufferEcho(hokuyoSendCommand_s *cmd) {
 
 		if(global.absolute_time - buffer_read_time_begin > HOKUYO_ECHO_READ_TIMEOUT) {
 			error_printf("Hokuyo Erreur : TimeOut echo\n");
+			state = CMD_ACK;
+			isInit = FALSE;
 			return CMD_ECHO_TIMEOUT;
 		}
 
@@ -294,31 +345,93 @@ static commandEchoStatus_e CMD_MS_readBufferEcho(hokuyoSendCommand_s *cmd) {
 
 }
 
+static commandEchoStatus_e CMD_MS_readBufferData(hokuyoSendCommand_s *cmd) {
 
-static bool_e CMD_MS_readBuffer(bool_e isEcho) {
+	static Sint32 resolution = ANGLE_RESOLUTION;
+	static Sint32 blockSize = DATA_BLOCK_SIZE;
+	Sint32 cluster = cmd->command.formated.parameters.MD_MS.clusterCount;
 
-	static Uint8 previous_data = 0;
-	Uint8 data;
+	static Uint8 previous_data = 0;						// Octet précédent
+	static Uint8 checksum = 0;							// Checksum courant
+	static Uint8 expectedChecksum = 0;					// Checksum attendu
+	static time32_t buffer_read_time_begin = 0;			// Temps de début de traitement de la trame
+	static bool_e isNewBloc = TRUE;						// Entrance pour le première état de la MAE
+	static bool_e resetTimeout = TRUE;					// Indique la remise à zéro du timeout
+	static Uint8 dataBlock[DATA_BLOCK_SIZE] = "";		// Block de données courant
+	static Uint8 indexDataBlock = 0;					// Index pour le tableau de données
+	static Sint32 angle = 0;							// Angle courant pour le premier point du bloc de donnée
+	static bool_e isInTreatement = FALSE;				// En cours de traitement d'un bloc de données
+	Uint8 data;											// Octet courant
 
 	while(!VCP_isRxEmpty()) {
 		data = VCP_read();
-		CMD_MS_datas[CMD_MS_index] = data;
-		debug_printf("%c", data);
-		if(CMD_MS_index < NB_BYTES_FROM_HOKUYO_CMD_MS) {
-			CMD_MS_index++;
-		} else {
-			fatal_printf("Overflow in hokuyo_read_buffer !\n");
+
+		/* Si nous avons un nouveau bloc de données */
+		if(isNewBloc) {
+			checksum = 0;
+			indexDataBlock = 0;
+			isNewBloc = FALSE;
 		}
 
-		/* On interrompt la réception des octets lorsque l'on reçoit deux LF consécutifs */
 		if(data == LINE_FEED && previous_data == LINE_FEED) {
-			return TRUE;
+			/* Fin de la trame Hokuyo */
+			isNewBloc = TRUE;
+			return CMD_ECHO_DONE;
+
+		} else if(data == LINE_FEED) {
+
+			/* Fin d'un bloc de données */
+			if(indexDataBlock < DATA_BLOCK_SIZE) {
+				/* Pour les blocs de données non complet */
+				checksum -= previous_data;
+				indexDataBlock--;
+			}
+			expectedChecksum = previous_data;
+
+			/* Vérification du checksum */
+			Uint8 calculateChecksum = HOKUYO_convertChecksum(checksum);
+			if(calculateChecksum == expectedChecksum) {
+
+				/* Traitement des données */
+				debug_printf("Compute data : %d\n", angle);
+				CMD_MS_computeDataBlock2C(cmd, dataBlock, indexDataBlock, angle);
+				angle += ANGLE_RESOLUTION * DATA_BLOCK_SIZE * cmd->command.formated.parameters.MD_MS.clusterCount;
+				isNewBloc = TRUE;
+
+			} else {
+
+				/* Le checksum est faux */
+				error_printf("Hokuyo Erreur : checksum data block (angle = %d)\n", angle);
+				angle += ANGLE_RESOLUTION * DATA_BLOCK_SIZE * cmd->command.formated.parameters.MD_MS.clusterCount;
+				isNewBloc = TRUE;
+
+			}
+
+		} else {
+
+			/* Dans le bloc de données */
+			if(indexDataBlock < DATA_BLOCK_SIZE) {
+				/* On ne veut pas enregistrer le checksum dans les données */
+				dataBlock[indexDataBlock++] = data;
+				checksum += data;
+			} else if (indexDataBlock == DATA_BLOCK_SIZE){
+				/* C'est le checksum */
+			} else {
+				/* Le bloc de données est trop long */
+				error_printf("Hokuyo Erreur : data block too long\n");
+				isNewBloc = TRUE;
+				return CMD_ECHO_ERROR;
+			}
 		}
 
 		previous_data = data;
+
 	}
-	return FALSE;
+
+	return CMD_ECHO_IN_PROGRESS;
+
 }
+
 
 static void CMD_MS_generateString(hokuyoSendCommand_s *cmd) {
 	Uint8 index = 0;
@@ -341,4 +454,84 @@ static void CMD_MS_generateString(hokuyoSendCommand_s *cmd) {
 static void CMD_MS_setNumberOfScans(hokuyoSendCommand_s *cmd, Uint8 numberOfScans) {
 	cmd->command.formated.parameters.MD_MS.numberOfScans = numberOfScans;
 	CMD_MS_generateString(cmd);
+}
+
+static void CMD_MS_computeDataBlock2C(hokuyoSendCommand_s *cmd, Uint8 *dataBlock, Uint8 length, Sint16 angle) {
+
+	Sint16 cos;				// Cosinus de l'angle en cours
+	Sint16 sin;				// Sinus de l'angle en cours
+	Sint32 x_absolute;		// Position en x sur le terrain du point en cours
+	Sint32 y_absolute;		// Position en Y sur le terrain du point en cours
+	bool_e point_filtered;	// Le point courant est-il pris en compte
+
+	Sint16 angle_offset_rad = ((Sint32)(FIRST_STEP_DEG)) +
+							  ((Sint32)(cmd->command.formated.parameters.MD_MS.startingStep * ANGLE_RESOLUTION));
+	angle_offset_rad = DEG100_TO_PI4096(angle_offset_rad);
+
+
+	/* Le nombre de données du bloc doit être pair */
+	if((length % 2) == 0) {
+
+		Uint8 i = 0;
+		for(i = 1; i < length; i+=2) {
+			Sint32 distance = (((Uint16)dataBlock[i-1]-0x30)<<6)+(((Uint16)dataBlock[i]-0x30)&0x3f);
+
+			/* On élimine les points trop près ou trop loin */
+			if(distance > TO_CLOSE_DISTANCE && distance < TO_FAR_DISTANCE) {
+
+				/* Angle relatif au robot */
+				Sint16 teta_relative_rad = DEG100_TO_PI4096(angle) - angle_offset_rad;
+				/* Angle absolu par rapport au terrain */
+				Sint16 teta_absolute_rad = GEOMETRY_modulo_angle(teta_relative_rad);
+				/* Calcul du cosinus et du sinus pour l'angle précédent */
+				COS_SIN_4096_get(teta_absolute_rad, &cos, &sin);
+
+				/* Calcul de la position sur la terrain suivant la couleur */
+				if(ENVIRONMENT_getColor() == BOT_COLOR) {
+					x_absolute = (distance*(Sint32)(cos))/4096;
+					y_absolute = (distance*(Sint32)(sin))/4096;
+				}
+				else
+				{
+					x_absolute = -(distance*(Sint32)(cos))/4096;
+					y_absolute = -(distance*(Sint32)(sin))/4096;
+				}
+
+				/* On garde les points qui sont sur le terrain */
+				if(	x_absolute < FIELD_SIZE_X - HOKUYO_MARGIN_FIELD_SIDE_IGNORE &&
+					x_absolute > HOKUYO_MARGIN_FIELD_SIDE_IGNORE &&
+					y_absolute < FIELD_SIZE_Y - HOKUYO_MARGIN_FIELD_SIDE_IGNORE &&
+					y_absolute > HOKUYO_MARGIN_FIELD_SIDE_IGNORE)
+				{
+					point_filtered = FALSE;
+
+					/* Les 4 coins et deux balises fixes */
+					if( x_absolute > FIELD_SIZE_X - MARGIN_BEACON ||
+						x_absolute < MARGIN_BEACON ||
+						absolute(x_absolute - FIELD_SIZE_X/2) < MARGIN_BEACON)
+					{
+						if(y_absolute < MARGIN_BEACON || y_absolute > FIELD_SIZE_Y - MARGIN_BEACON) {
+							point_filtered = TRUE;
+						}
+
+					}
+
+				}
+
+				if(point_filtered == FALSE && nbDetectedPoints < NB_STEP_MAX) {
+					detectedPoints[nbDetectedPoints].teta = teta_relative_rad; // Pour l'évitement
+					detectedPoints[nbDetectedPoints].coordX = x_absolute;
+					detectedPoints[nbDetectedPoints].coordY = y_absolute;
+					nbDetectedPoints++;
+				}
+			}
+
+			/* On passe au prochain point */
+			angle += ANGLE_RESOLUTION * cmd->command.formated.parameters.MD_MS.clusterCount;
+
+		}
+
+	} else {
+		debug_printf("Hokuyo Error : modulo error\n");
+	}
 }
--- 7_BeaconEye/CurrentSoftware/QS_hokuyo/QS_hokuyo.h
+++ 7_BeaconEye/CurrentSoftware/QS_hokuyo/QS_hokuyo.h
@@ -10,9 +10,42 @@
 
 #include "../QS/QS_all.h"
 
-#define LINE_FEED				0x0A							// Caractère de retour à la ligne
-#define HOKUYO_BUFFER_READ_TIMEOUT	500  //ms
-#define HOKUYO_ECHO_READ_TIMEOUT	100  //ms
+
+/* Hokuyo frame */
+#define LINE_FEED						0x0A	// Caractère de retour à la ligne
+#define DATA_BLOCK_SIZE					64
+/* Timeout */
+#define HOKUYO_BUFFER_READ_TIMEOUT		500  	// [ms]
+#define HOKUYO_ECHO_READ_TIMEOUT		100  	// [ms]
+/* Hokuyo specification */
+#define FIRST_MEASUREMENT_POINT			0
+#define SENSOR_FRONT_STEP				540
+#define LAST_MEASUREMENT_POINT			1080
+#define SLIT_DIVISION					1440	// Nombre de pas de mesures pour 360°
+#define NB_STEP_MAX						(LAST_MEASUREMENT_POINT - FIRST_MEASUREMENT_POINT)
+#define ANGLE_RESOLUTION				(360 * 100 / SLIT_DIVISION) // [°*100]
+#define FIRST_STEP_DEG					4500	// [°*100]
+#define FRONT_STEP_DEG					18000   // [°*100]
+#define LAST_POINT_DEG					31525   // [°*100]
+/* Treatement point */
+#define TO_CLOSE_DISTANCE				100		// [mm]
+#define TO_FAR_DISTANCE					3500	// [mm]
+#define FIELD_SIZE_Y 					3000	// [mm]
+#define FIELD_SIZE_X 					2000	// [mm]
+#define HOKUYO_MARGIN_FIELD_SIDE_IGNORE 50 		// [mm] Marge sur les bords du terrain
+#define MARGIN_BEACON					100		// [mm] Marge pour les balises extérieures
+
+#define DEG100_TO_PI4096(x)				((((Sint32)(x))*183)>>8)
+
+typedef struct{
+	Sint32 dist;
+	int teta;
+	Sint32 coordX;
+	Sint32 coordY;
+	Sint32 coordX_prec;
+	Sint32 coordY_prec;
+	Uint8 accuracy;
+}HOKUYO_adversary_position;
 
 void HOKUYO_init(void);
 void HOKUYO_processMain(void);
