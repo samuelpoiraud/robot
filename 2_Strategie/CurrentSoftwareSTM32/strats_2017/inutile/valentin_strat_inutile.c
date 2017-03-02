acb288606110b31cf2c1a0d9d8fb843208cb2811
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2017-03-02 18:24:07 +0000

    Test de la remonté de config actionneur et des warners => OK Résolution du problème du wait_time, double appel d'une fonction non réentrante

--- 2_Strategie/CurrentSoftwareSTM32/strats_2017/inutile/valentin_strat_inutile.c
+++ 2_Strategie/CurrentSoftwareSTM32/strats_2017/inutile/valentin_strat_inutile.c
@@ -4,6 +4,7 @@
 #include "../../actuator/act_functions.h"
 #include "../../actuator/queue.h"
 #include "../../QS/QS_stateMachineHelper.h"
+#include "../../QS/QS_outputlog.h"
 #include "../../utils/generic_functions.h"
 #include "../../utils/actionChecker.h"
 #include "../../elements.h"
@@ -23,28 +24,48 @@ void valentin_strat_inutile_big(){
 
 		case INIT:
 			if(entrance){
+
+				ACT_set_warner(ACT_CYLINDER_ELEVATOR_LEFT, ACT_CYLINDER_ELEVATOR_LEFT_WARNER);
+				ACT_push_order(ACT_CYLINDER_ELEVATOR_LEFT, ACT_CYLINDER_ELEVATOR_LEFT_BOTTOM);
+			}
+			/*if(entrance){
 				ELEMENTS_set_flag(FLAG_HARRY_DISABLE_MODULE_LEFT, TRUE);
 			}
 			state = try_going(1100, COLOR_Y(2100), state , ACTION, DONE, FAST, ANY_WAY, DODGE_AND_WAIT, END_AT_LAST_POINT);
+			*/
+			state = wait_time(2000, state, ACTION);
+			if(ACT_get_warner(ACT_CYLINDER_ELEVATOR_LEFT)){
+				debug_printf("\n\nWARNER RECEIVED\n\n");
+			}
 			break;
 
 		case ACTION:
-			state = check_sub_action_result(sub_harry_prise_module_unicolor_north(LEFT), state, DONE, ERROR);
+			if(ACT_get_warner(ACT_CYLINDER_ELEVATOR_LEFT)){
+				debug_printf("\n\nWARNER RECEIVED\n\n");
+			}
+			state = check_sub_action_result(ACT_check_position_config(ACT_CYLINDER_ELEVATOR_LEFT, ACT_CYLINDER_ELEVATOR_LEFT_BOTTOM), state, DONE, ERROR);
+			//state = check_sub_action_result(sub_harry_prise_module_unicolor_north(LEFT), state, DONE, ERROR);
 			break;
 
 		case ERROR:
-			if(entrance){
+			/*if(entrance){
 				//ROCKETS_print(MODULE_ROCKET_MULTI_OUR_SIDE);
 				STOCKS_print(MODULE_STOCK_LEFT);
 				STOCKS_print(MODULE_STOCK_RIGHT);
+			}*/
+			if(entrance){
+				debug_printf("ERROR\n");
 			}
 			break;
 
 		case DONE:
-			if(entrance){
+			/*if(entrance){
 				//ROCKETS_print(MODULE_ROCKET_MULTI_OUR_SIDE);
 				STOCKS_print(MODULE_STOCK_LEFT);
 				STOCKS_print(MODULE_STOCK_RIGHT);
+			}*/
+			if(entrance){
+				debug_printf("DONE\n");
 			}
 			break;
 	}
