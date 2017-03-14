144c7b5ce286e7aecf6530f5613863e33a94b691
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2017-03-14 22:36:23 +0000

    Quelques modif du nouveau selftest actionneur pour que ca compile (Il reste encore des choses a modfifier)

--- 2_Strategie/CurrentSoftwareSTM32/Supervision/SelftestActionneur.h
+++ 2_Strategie/CurrentSoftwareSTM32/Supervision/SelftestActionneur.h
@@ -9,14 +9,15 @@
 #ifndef SUPERVISION_SELFTESTACTIONNEUR_H_
 	#define SUPERVISION_SELFTESTACTIONNEUR_H_
 
+	#include "../QS/QS_types.h"
+	#include "../actuator/queue.h"
+
 	typedef struct{
 			Uint8 numero_etape;
 			ACT_sid_e actionneur;
 			ACT_order_e position;
+			queue_id_e queue_id;
 	}struct_selftest_t;
-
-
-	#define LAST_ETAPE = 4
 
 
 #endif /* SUPERVISION_SELFTESTACTIONNEUR_H_ */
