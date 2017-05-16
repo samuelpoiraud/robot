aab7e156f14f7dc7f62cab8740bb46b940e88cf7
cmalgogne <cmalgogne@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2017-05-16 16:42:18 +0000

    asservissement du gros robot

--- 1_Propulsion/CurrentSoftware/_Propulsion_config.h
+++ 1_Propulsion/CurrentSoftware/_Propulsion_config.h
@@ -60,7 +60,7 @@
 
 	//COEF_ODOMETRIE_CENTRIFUGE permet de régler la variation d'odométrie en courbe
 	#define SMALL_ODOMETRY_COEF_CENTRIFUGAL_DEFAULT (0)
-	#define BIG_ODOMETRY_COEF_CENTRIFUGAL_DEFAULT 0xFFFFFC00 //128/(rad*4096*64) si on considère que les "speed" sont en fait des distances sinon c'est 128*ms*ms/(rad*4096*64)
+	#define BIG_ODOMETRY_COEF_CENTRIFUGAL_DEFAULT (0) //128/(rad*4096*64) si on considère que les "speed" sont en fait des distances sinon c'est 128*ms*ms/(rad*4096*64)
 
 
 
@@ -161,15 +161,15 @@
 	#define BIG_KD_TRANSLATION		(0x20)//(0x1C) //0x80
 
 	#define SMALL_KP_TRANSLATION 	(0x20)
-	#define BIG_KP_TRANSLATION		(0x2A)//(0x30)//(0x28)
+	#define BIG_KP_TRANSLATION		(0x28)//(0x30)//(0x28)
 	// Sur archi'tech (2009) il s'est avéré meilleur de scinder les deux coeffs selon le sens de rotation...(POSITIF, NEGATIF)
 	//Etaient alors définis deux coeffs pour le D et de pour le P : KD_ROTATION_POSITIF, KD_ROTATION_NEGATIF.....
 	//en pratique, si le robot est équilibré, les coeffs sont les mêmes !
 	#define SMALL_KD_ROTATION		(0x150) //0x800
-	#define BIG_KD_ROTATION			(0x100)//(0x08) //0x800
+	#define BIG_KD_ROTATION			(0x280)//(0x08) //0x800
 
 	#define SMALL_KP_ROTATION		(0x60)//40 //0x80
-	#define BIG_KP_ROTATION			(0x80)//(0x70) //0x80
+	#define BIG_KP_ROTATION			(0x48)//(0x70) //0x80
 
 	#define SMALL_KI_ROTATION		(0x0)
 	#define BIG_KI_ROTATION			(0x05)
