aa9d8422f2a28d07fa27c95cd644cad80772aea1
cmalgogne <cmalgogne@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2017-05-17 19:48:08 +0000

    add poussix

---
+++ 3_Actionneur/CurrentSoftwareSTM32/Anne/small_cylinder/small_cylinder_poussix/small_cylinder_poussix.h
@@ -0,0 +1,59 @@
+/*  Club Robot ESEO 2016 - 2017
+ *
+ *	Fichier : exemple_ax12.h
+ *	Package : Carte actionneur
+ *	Description : Gestion de l'actionneur AX12 EXEMPLE
+ *  Auteur :
+ *  Version 2017
+ *  Robot : BIG
+ */
+
+
+#ifndef SMALL_CYLINDER_POUSSIX_H
+	#define	SMALL_CYLINDER_POUSSIX_H
+
+	#include "../QS/QS_all.h"
+	#include "../QS/QS_CANmsgList.h"
+
+	#include "../queue.h"
+
+	/** Initialisation du gestionnaire de SMALL_CYLINDER_POUSSIX.
+	 *
+	 * Configure l'asservissement de l'AX12
+	 */
+	void SMALL_CYLINDER_POUSSIX_init();
+
+	/** Initialisation en position du gestionnaire de SMALL_CYLINDER_POUSSIX.
+	 *
+	 * Initialise la position de l'AX12
+	 */
+	void SMALL_CYLINDER_POUSSIX_init_pos();
+
+	/** Stoppe l'actionneur.
+	 *
+	 * Cette fonction est appelée en fin de match
+	 */
+	void SMALL_CYLINDER_POUSSIX_stop();
+
+	/** Gère les messages CAN liés à la SMALL_CYLINDER_POUSSIX.
+	 *
+	 * Cette fonction s'occupe toute seule de gérer la queue et d'agir en conséquence.
+	 * @param msg le message CAN
+	 * @return TRUE si le message CAN a été géré par cet actionneur, FALSE sinon
+	 */
+	bool_e SMALL_CYLINDER_POUSSIX_CAN_process_msg(CAN_msg_t* msg);
+
+	/** Gère les commandes demandées.
+	 *
+	 * Fonction à mettre sur la file pour demander une action.
+	 */
+	void SMALL_CYLINDER_POUSSIX_run_command(queue_id_t queueId, bool_e init);
+
+
+	/**
+	 * Réinitialise la configuration de l'AX12
+	 */
+	void SMALL_CYLINDER_POUSSIX_reset_config();
+
+#endif	/* EXEMPLE_H */
+
