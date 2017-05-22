b40b7351ce5cd215d4688e6b5ee850b2e4399677
ehlingth <ehlingth@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2017-05-22 20:32:17 +0000

    maj thomas_strat inutile

--- 2_Strategie/CurrentSoftwareSTM32/strats_2017/inutile/thomas_strat_inutile.c
+++ 2_Strategie/CurrentSoftwareSTM32/strats_2017/inutile/thomas_strat_inutile.c
@@ -56,17 +56,21 @@ void thomas_strat_inutile_small(){
 				GET_IN_DIRECT,
 				GO_TO_START_POINT,
 				ACTION,
+				PRISE,
+				FLEEEE,
+				DEPOSE,
+				DEPOSE_2,
 				ERROR,
 				DONE
 			);
 
 	switch(state){
 		case INIT:
-			state = try_going(global.pos.x+200, global.pos.y, state, GO_POSITION, ERROR, FAST, BACKWARD, DODGE_AND_WAIT, END_AT_BRAKE);
+			state = try_going(global.pos.x+200, global.pos.y, state, GO_POSITION, ERROR, FAST, ANY_WAY, DODGE_AND_WAIT, END_AT_BRAKE);
 			break;
 
 		case GO_POSITION:
-			state = try_going(800, COLOR_Y(1000), state, ARM_OUT, ERROR, FAST, BACKWARD, DODGE_AND_WAIT, END_AT_BRAKE);
+			state = try_going(800, COLOR_Y(1000), state, PRISE, ERROR, FAST, ANY_WAY, DODGE_AND_WAIT, END_AT_BRAKE);
 			break;
 
 		case ARM_OUT:
@@ -81,11 +85,33 @@ void thomas_strat_inutile_small(){
 			break;
 
 		case GO_TO_START_POINT: //ajuster la distance fusée
-			state = try_going(265, COLOR_Y(1150), state, ACTION, ERROR, FAST, FORWARD, NO_DODGE_AND_WAIT, END_AT_LAST_POINT);
+			if(global.color == BLUE){
+				state = try_going(265, COLOR_Y(1145), state, ACTION, ERROR, FAST, FORWARD, NO_DODGE_AND_WAIT, END_AT_LAST_POINT);
+			}else{
+				state = try_going(265, COLOR_Y(1155), state, ACTION, ERROR, FAST, FORWARD, NO_DODGE_AND_WAIT, END_AT_LAST_POINT);
+			}
 			break;
 
 		case ACTION:
 			state = check_sub_action_result(sub_act_anne_take_rocket_down_to_top( MODULE_ROCKET_MULTI_OUR_SIDE, TRUE, TRUE, TRUE, TRUE)
+, state, FLEEEE, ERROR);
+			break;
+
+		case PRISE:
+			state = check_sub_action_result(sub_anne_fusee_color(), state, FLEEEE, ERROR);
+			break;
+
+		case FLEEEE:
+			state = try_going(600, COLOR_Y(1150), state, DEPOSE, DEPOSE, FAST, ANY_WAY, NO_DODGE_AND_WAIT, END_AT_LAST_POINT);
+			break;
+
+		case DEPOSE:
+			state = check_sub_action_result(sub_act_anne_mae_dispose_modules(ARG_DISPOSE_ONE_CYLINDER_FOLLOW_BY_ANOTHER)
+, state, DEPOSE_2, ERROR);
+			break;
+
+		case DEPOSE_2:
+			state = check_sub_action_result(sub_act_anne_mae_dispose_modules(ARG_DISPOSE_ONE_CYLINDER_AND_FINISH)
 , state, DONE, ERROR);
 			break;
 
