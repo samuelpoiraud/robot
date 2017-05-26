185e6576f2a7b52ad319ff2bb7b3ac5b87439eb1
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2017-05-26 11:59:50 +0000

    Revert speed actionneur Harry

--- 3_Actionneur/CurrentSoftwareSTM32/Harry/cylinder/cylinder_arm/cylinder_arm_right_config.h
+++ 3_Actionneur/CurrentSoftwareSTM32/Harry/cylinder/cylinder_arm/cylinder_arm_right_config.h
@@ -15,7 +15,7 @@
     #define CYLINDER_ARM_RIGHT_RX24_ASSER_TIMEOUT				2000	// Si le RX24 n'atteint pas la position demandée avant ce temps, on considère qu'il y a un problème. Temps en ms.
     #define CYLINDER_ARM_RIGHT_RX24_ASSER_POS_EPSILON			6		// Lorsque le RX24 atteint sa position avec +/- POS_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
     #define CYLINDER_ARM_RIGHT_RX24_ASSER_POS_LARGE_EPSILON		28		// Si le déplacement de le RX24 timeout mais que sa position est à +/- POS_LARGE_EPSILON degré d'écart max, on considère qu'il a atteint sa position. Angle en degré.
-    #define CYLINDER_ARM_RIGHT_RX24_MAX_TORQUE_PERCENT		    90		// Couple maximum en pourcentage de le RX24. Utilisé pour limiter le courant dans le moteur. A mettre a une valeur correcte pour pincer assez fort sans risquer d'endommager le RX24.
+    #define CYLINDER_ARM_RIGHT_RX24_MAX_TORQUE_PERCENT		    50		// Couple maximum en pourcentage de le RX24. Utilisé pour limiter le courant dans le moteur. A mettre a une valeur correcte pour pincer assez fort sans risquer d'endommager le RX24.
     #define CYLINDER_ARM_RIGHT_RX24_MAX_TEMPERATURE				60		// Température maximale en degré
 	#define CYLINDER_ARM_RIGHT_RX24_SPEED						15		// Vitesse par défaut du RX24
 
