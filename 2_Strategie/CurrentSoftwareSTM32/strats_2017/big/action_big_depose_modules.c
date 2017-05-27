f0841104fb0679d6ca7c4a9395e3f711ca7b9d21
vbesnard <vbesnard@3ed2a8e1-f0f3-4f7f-89ff-c33dc6e3fa8f>
2017-05-27 05:15:59 +0000

    Problème prise de fusée Ajout argument pour les Moonbases

--- 2_Strategie/CurrentSoftwareSTM32/strats_2017/big/action_big_depose_modules.c
+++ 2_Strategie/CurrentSoftwareSTM32/strats_2017/big/action_big_depose_modules.c
@@ -391,20 +391,20 @@ error_e sub_harry_depose_modules_centre(Uint8 drop_place, moduleStockLocation_e 
 				// pos 1
 				if(way == FORWARD){
 					if(global.color == BLUE){
-						state = try_rush((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(1715-(MOONBASES_getNbModules()-3)*7):(1715),\
-								COLOR_Y((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(865-(MOONBASES_getNbModules()-3)*7):(865)), state, UP_PUSHER_LEFT, ERROR, FORWARD, DODGE_AND_WAIT, FALSE);
+						state = try_rush((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(1715-(MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)-3)*7):(1715),\
+								COLOR_Y((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(865-(MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)-3)*7):(865)), state, UP_PUSHER_LEFT, ERROR, FORWARD, DODGE_AND_WAIT, FALSE);
 					}else{
-						state = try_rush((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(1715-(MOONBASES_getNbModules()-3)*7):(1715),\
-									COLOR_Y((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(865-(MOONBASES_getNbModules()-3)*7):(865)), state, UP_PUSHER_RIGHT, ERROR, FORWARD, DODGE_AND_WAIT, FALSE);
+						state = try_rush((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(1715-(MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)-3)*7):(1715),\
+									COLOR_Y((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(865-(MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)-3)*7):(865)), state, UP_PUSHER_RIGHT, ERROR, FORWARD, DODGE_AND_WAIT, FALSE);
 					}
 				}
 				else{
 					if(global.color == BLUE){
-						state = try_rush((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(1690-(MOONBASES_getNbModules()-3)*7):(1690),\
-								COLOR_Y((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(830-(MOONBASES_getNbModules()-3)*7):(830)), state, UP_PUSHER_LEFT, ERROR, FORWARD, DODGE_AND_WAIT, FALSE);
+						state = try_rush((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(1690-(MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)-3)*7):(1690),\
+								COLOR_Y((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(830-(MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)-3)*7):(830)), state, UP_PUSHER_LEFT, ERROR, FORWARD, DODGE_AND_WAIT, FALSE);
 					}else{
-						state = try_rush((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(1690-(MOONBASES_getNbModules()-3)*7):(1690),\
-									COLOR_Y((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(830-(MOONBASES_getNbModules()-3)*7):(830)), state, UP_PUSHER_RIGHT, ERROR, FORWARD, DODGE_AND_WAIT, FALSE);
+						state = try_rush((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(1690-(MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)-3)*7):(1690),\
+									COLOR_Y((MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)>3)?(830-(MOONBASES_getNbModules(MODULE_MOONBASE_OUR_CENTER)-3)*7):(830)), state, UP_PUSHER_RIGHT, ERROR, FORWARD, DODGE_AND_WAIT, FALSE);
 					}
 				}
 			}else if(drop_place == POS_2){
